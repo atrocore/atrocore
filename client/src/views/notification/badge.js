81f31de7633df38a28811e5d79e8ded8a9c84a39
r.ratsun <r.ratsun@gmail.com>
2021-02-22 12:04:07 +0200

    refactoring of queue manager

--- client/src/views/notification/badge.js
+++ client/src/views/notification/badge.js
@@ -36,7 +36,7 @@ Espo.define('views/notification/badge', 'view', function (Dep) {
 
         template: 'notification/badge',
 
-        notificationsCheckInterval: 2,
+        notificationsCheckInterval: 1,
 
         timeout: null,
 
