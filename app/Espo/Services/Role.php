676d0fed1d19254c04dd79a92a8578cb0bfa1119
r.ratsun <r.ratsun@gmail.com>
2021-04-01 17:29:53 +0300

    .

--- app/Espo/Services/Role.php
+++ app/Espo/Services/Role.php
@@ -35,31 +35,19 @@ namespace Espo\Services;
 
 use \Espo\ORM\Entity;
 
+/**
+ * Class Role
+ */
 class Role extends Record
 {
-    protected function init()
-    {
-        parent::init();
-        $this->addDependency('fileManager');
-    }
-
     protected $forceSelectAllAttributes = true;
 
-    public function afterCreate(Entity $entity, array $data = array())
+    /**
+     * @inheritDoc
+     */
+    protected function getFieldsThatConflict(Entity $entity, \stdClass $data): array
     {
-        parent::afterCreate($entity, $data);
-        $this->clearRolesCache();
-    }
-
-    public function afterUpdate(Entity $entity, array $data = array())
-    {
-        parent::afterUpdate($entity, $data);
-        $this->clearRolesCache();
-    }
-
-    protected function clearRolesCache()
-    {
-        $this->getInjection('fileManager')->removeInDir('data/cache/application/acl');
+        return [];
     }
 }
 
