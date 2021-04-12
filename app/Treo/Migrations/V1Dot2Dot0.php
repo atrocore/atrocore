f9b4b2570660ddbc87783be475d8bd2ac5e3fec0
r.ratsun <r.ratsun@gmail.com>
2021-04-12 17:49:17 +0300

    .

--- app/Treo/Migrations/V1Dot2Dot0.php
+++ app/Treo/Migrations/V1Dot2Dot0.php
@@ -63,6 +63,7 @@ class V1Dot2Dot0 extends V1Dot1Dot23
         $this->execute("DROP TABLE external_account");
         $this->execute("DROP TABLE lead_capture");
         $this->execute("DROP TABLE lead_capture_log_record");
+        $this->execute("DROP TABLE reminder");
     }
 
     /**
