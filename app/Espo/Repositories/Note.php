4f8aaa19cd536e38a72dce12a6dee58d8f1ac20f
r.ratsun <r.ratsun@gmail.com>
2021-04-13 10:03:13 +0300

    .

---
+++ app/Espo/Repositories/Note.php
@@ -0,0 +1,423 @@
+<?php
+/*
+ * This file is part of EspoCRM and/or AtroCore.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2019 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * AtroCore is EspoCRM-based Open Source application.
+ * Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
+ *
+ * AtroCore as well as EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AtroCore as well as EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
+ * and "AtroCore" word.
+ */
+
+declare(strict_types=1);
+
+namespace Espo\Repositories;
+
+use Espo\Core\Acl;
+use Espo\Core\AclManager;
+use Espo\Core\ORM\Repositories\RDB;
+use Espo\ORM\Entity;
+use Espo\ORM\EntityCollection;
+
+/**
+ * Class Note
+ */
+class Note extends RDB
+{
+    /**
+     * @param Entity $entity
+     * @param array  $options
+     */
+    protected function beforeSave(Entity $entity, array $options = [])
+    {
+        if ($entity->get('type') == 'Post') {
+            $this->addMentionData($entity);
+        }
+
+        parent::beforeSave($entity, $options);
+    }
+
+    /**
+     * @param Entity $entity
+     * @param array  $options
+     */
+    protected function afterSave(Entity $entity, array $options = [])
+    {
+        $this->sendNotifications($entity);
+
+        parent::afterSave($entity, $options);
+    }
+
+    protected function addMentionData(Entity $entity): void
+    {
+        $post = $entity->get('post');
+
+        $mentionData = new \stdClass();
+
+        $previousMentionList = array();
+        if (!$entity->isNew()) {
+            $data = $entity->get('data');
+            if (!empty($data) && !empty($data->mentions)) {
+                $previousMentionList = array_keys(get_object_vars($data->mentions));
+            }
+        }
+
+        preg_match_all('/(@[\w@.-]+)/', $post, $matches);
+
+        $mentionCount = 0;
+
+        if (is_array($matches) && !empty($matches[0]) && is_array($matches[0])) {
+            $parent = null;
+            if ($entity->get('parentId') && $entity->get('parentType')) {
+                $parent = $this->getEntityManager()->getEntity($entity->get('parentType'), $entity->get('parentId'));
+            }
+            foreach ($matches[0] as $item) {
+                $userName = substr($item, 1);
+                $user = $this->getEntityManager()->getRepository('User')->where(array('userName' => $userName))->findOne();
+                if ($user) {
+                    if (!$this->getAcl()->checkUser('assignmentPermission', $user)) {
+                        continue;
+                    }
+                    $m = array(
+                        'id'       => $user->id,
+                        'name'     => $user->get('name'),
+                        'userName' => $user->get('userName'),
+                        '_scope'   => $user->getEntityName()
+                    );
+                    $mentionData->$item = (object)$m;
+                    $mentionCount++;
+                    if (!in_array($item, $previousMentionList)) {
+                        if ($user->id == $this->getUser()->id) {
+                            continue;
+                        }
+                        $this->notifyAboutMention($entity, $user, $parent);
+                        $entity->addNotifiedUserId($user->id);
+                    }
+                }
+            }
+        }
+
+        $data = $entity->get('data');
+        if (empty($data)) {
+            $data = new \stdClass();
+        }
+        if ($mentionCount) {
+            $data->mentions = $mentionData;
+        } else {
+            unset($data->mentions);
+        }
+
+        $entity->set('data', $data);
+    }
+
+    protected function notifyAboutMention(Entity $entity, \Espo\Entities\User $user, Entity $parent = null): void
+    {
+        if ($user->get('isPortalUser')) {
+            return;
+        }
+        if ($parent) {
+            if (!$this->getAclManager()->check($user, $parent, 'stream')) {
+                return;
+            }
+        }
+        $this->getNotificationService()->notifyAboutMentionInPost($user->id, $entity->id);
+    }
+
+    protected function sendNotifications(Entity $entity): void
+    {
+        if ($entity->isNew()) {
+            $parentType = $entity->get('parentType');
+            $parentId = $entity->get('parentId');
+            $superParentType = $entity->get('superParentType');
+            $superParentId = $entity->get('superParentId');
+
+            $notifyUserIdList = [];
+
+            if ($parentType && $parentId) {
+                $userList = $this->getSubscriberList($parentType, $parentId, $entity->get('isInternal'));
+                $userIdMetList = [];
+                foreach ($userList as $user) {
+                    $userIdMetList[] = $user->id;
+                }
+                if ($superParentType && $superParentId) {
+                    $additionalUserList = $this->getSubscriberList($superParentType, $superParentId, $entity->get('isInternal'));
+                    foreach ($additionalUserList as $user) {
+                        if ($user->isPortal()) {
+                            continue;
+                        }
+                        if (in_array($user->id, $userIdMetList)) {
+                            continue;
+                        }
+                        $userIdMetList[] = $user->id;
+                        $userList[] = $user;
+                    }
+                }
+
+                if ($entity->get('relatedType')) {
+                    $targetType = $entity->get('relatedType');
+                } else {
+                    $targetType = $parentType;
+                }
+
+                $skipAclCheck = false;
+                if (!$entity->isAclProcessed()) {
+                    $skipAclCheck = true;
+                } else {
+                    $teamIdList = $entity->getLinkMultipleIdList('teams');
+                    $userIdList = $entity->getLinkMultipleIdList('users');
+                }
+
+                foreach ($userList as $user) {
+                    if ($skipAclCheck) {
+                        $notifyUserIdList[] = $user->id;
+                        continue;
+                    }
+                    if ($user->isAdmin()) {
+                        $notifyUserIdList[] = $user->id;
+                        continue;
+                    }
+
+                    if ($user->isPortal()) {
+                        if ($entity->get('relatedType')) {
+                            continue;
+                        } else {
+                            $notifyUserIdList[] = $user->id;
+                        }
+                        continue;
+                    }
+
+                    $level = $this->getInternalAclManager()->getLevel($user, $targetType, 'read');
+
+                    if ($level === 'all') {
+                        $notifyUserIdList[] = $user->id;
+                        continue;
+                    } else {
+                        if ($level === 'team') {
+                            if (in_array($user->id, $userIdList)) {
+                                $notifyUserIdList[] = $user->id;
+                                continue;
+                            }
+
+                            if (!empty($teamIdList)) {
+                                $userTeamIdList = $user->getLinkMultipleIdList('teams');
+                                foreach ($teamIdList as $teamId) {
+                                    if (in_array($teamId, $userTeamIdList)) {
+                                        $notifyUserIdList[] = $user->id;
+                                        break;
+                                    }
+                                }
+                            }
+                            continue;
+                        } else {
+                            if ($level === 'own') {
+                                if (in_array($user->id, $userIdList)) {
+                                    $notifyUserIdList[] = $user->id;
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                }
+
+            } else {
+                $targetType = $entity->get('targetType');
+                if ($targetType === 'users') {
+                    $targetUserIdList = $entity->get('usersIds');
+                    if (is_array($targetUserIdList)) {
+                        foreach ($targetUserIdList as $userId) {
+                            if ($userId === $this->getUser()->id) {
+                                continue;
+                            }
+                            if (in_array($userId, $notifyUserIdList)) {
+                                continue;
+                            }
+                            $notifyUserIdList[] = $userId;
+                        }
+                    }
+                } else {
+                    if ($targetType === 'teams') {
+                        $targetTeamIdList = $entity->get('teamsIds');
+                        if (is_array($targetTeamIdList)) {
+                            foreach ($targetTeamIdList as $teamId) {
+                                $team = $this->getEntityManager()->getEntity('Team', $teamId);
+                                if (!$team) {
+                                    continue;
+                                }
+                                $targetUserList = $this->getEntityManager()->getRepository('Team')->findRelated(
+                                    $team, 'users', array(
+                                        'whereClause' => array(
+                                            'isActive' => true
+                                        )
+                                    )
+                                );
+                                foreach ($targetUserList as $user) {
+                                    if ($user->id === $this->getUser()->id) {
+                                        continue;
+                                    }
+                                    if (in_array($user->id, $notifyUserIdList)) {
+                                        continue;
+                                    }
+                                    $notifyUserIdList[] = $user->id;
+                                }
+                            }
+                        }
+                    } else {
+                        if ($targetType === 'portals') {
+                            $targetPortalIdList = $entity->get('portalsIds');
+                            if (is_array($targetPortalIdList)) {
+                                foreach ($targetPortalIdList as $portalId) {
+                                    $portal = $this->getEntityManager()->getEntity('Portal', $portalId);
+                                    if (!$portal) {
+                                        continue;
+                                    }
+                                    $targetUserList = $this->getEntityManager()->getRepository('Portal')->findRelated(
+                                        $portal, 'users', array(
+                                            'whereClause' => array(
+                                                'isActive' => true
+                                            )
+                                        )
+                                    );
+                                    foreach ($targetUserList as $user) {
+                                        if ($user->id === $this->getUser()->id) {
+                                            continue;
+                                        }
+                                        if (in_array($user->id, $notifyUserIdList)) {
+                                            continue;
+                                        }
+                                        $notifyUserIdList[] = $user->id;
+                                    }
+                                }
+                            }
+                        } else {
+                            if ($targetType === 'all') {
+                                $targetUserList = $this->getEntityManager()->getRepository('User')->find(
+                                    array(
+                                        'whereClause' => array(
+                                            'isActive'     => true,
+                                            'isPortalUser' => false
+                                        )
+                                    )
+                                );
+                                foreach ($targetUserList as $user) {
+                                    if ($user->id === $this->getUser()->id) {
+                                        continue;
+                                    }
+                                    $notifyUserIdList[] = $user->id;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            $notifyUserIdList = array_unique($notifyUserIdList);
+
+            foreach ($notifyUserIdList as $i => $userId) {
+                if ($entity->isUserIdNotified($userId)) {
+                    unset($notifyUserIdList[$i]);
+                }
+            }
+            $notifyUserIdList = array_values($notifyUserIdList);
+
+            if (!empty($notifyUserIdList)) {
+                $this->getNotificationService()->notifyAboutNote($notifyUserIdList, $entity);
+            }
+        }
+    }
+
+    protected function getSubscriberList(string $parentType, string $parentId, bool $isInternal = false): EntityCollection
+    {
+        $pdo = $this->getEntityManager()->getPDO();
+
+        if (!$isInternal) {
+            $sql = "
+                SELECT user_id AS userId
+                FROM subscription
+                WHERE entity_id = " . $pdo->quote($parentId) . " AND entity_type = " . $pdo->quote($parentType) . "
+            ";
+        } else {
+            $sql = "
+                SELECT subscription.user_id AS userId
+                FROM subscription
+                JOIN user ON user.id = subscription.user_id
+                WHERE
+                    entity_id = " . $pdo->quote($parentId) . " AND entity_type = " . $pdo->quote($parentType) . " AND
+                    user.is_portal_user = 0
+            ";
+        }
+
+        $userList = $this->getEntityManager()->getRepository('User')->where(
+            [
+                'isActive' => true
+            ]
+        )->select(['id', 'isPortalUser', 'isAdmin'])->find(
+            [
+                'customWhere' => "AND user.id IN (" . $sql . ")"
+            ]
+        );
+
+        return $userList;
+    }
+
+    protected function getInternalAclManager(): AclManager
+    {
+        return $this->getInjection('internalAclManager');
+    }
+
+    protected function getUser(): \Espo\Entities\User
+    {
+        return $this->getInjection('user');
+    }
+
+    protected function getAcl(): Acl
+    {
+        return $this->getInjection('acl');
+    }
+
+    protected function getAclManager(): AclManager
+    {
+        return $this->getInjection('aclManager');
+    }
+
+    protected function getNotificationService(): \Espo\Services\Notification
+    {
+        return $this->getInjection('serviceFactory')->create('Notification');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function init()
+    {
+        parent::init();
+
+        $this->addDependency('acl');
+        $this->addDependency('aclManager');
+        $this->addDependency('user');
+        $this->addDependency('serviceFactory');
+        $this->addDependency('internalAclManager');
+    }
+}
+
--- app/Espo/Repositories/Notification.php
+++ app/Espo/Repositories/Notification.php
@@ -33,7 +33,10 @@
 
 namespace Espo\Repositories;
 
+use Espo\Core\Htmlizer\Htmlizer;
+use Espo\Core\Mail\Sender;
 use Espo\Core\ORM\Repositories\RDB;
+use Espo\Core\Utils\TemplateFileManager;
 use Espo\ORM\Entity;
 
 /**
@@ -44,6 +47,16 @@ class Notification extends RDB
     const UPDATE_COUNT_PATH = 'data/notifications-count';
 
     const NOT_READ_COUNT_FILE = 'data/notReadCount.json';
+
+    /**
+     * @var Htmlizer|null
+     */
+    protected $htmlizer = null;
+
+    /**
+     * @var array
+     */
+    protected $userIdPortalCacheMap = [];
 
     public static function refreshNotReadCount(): void
     {
@@ -60,9 +73,174 @@ class Notification extends RDB
      */
     protected function afterSave(Entity $entity, array $options = [])
     {
-        parent::afterSave($entity, $options);
-
         self::refreshNotReadCount();
+
+        if ($entity->isNew()) {
+            $this->sendEmail($entity);
+        }
+
+        parent::afterSave($entity, $options);
+    }
+
+    protected function sendEmail(Entity $notification): void
+    {
+//        $this->emailMentionInPost($notification);
+
+//        processNotificationMentionInPost
+//        processNotificationNote
+//        processNotificationNotePost
+//        processNotificationNoteStatus
+//        processNotificationNoteEmailReceived
+
+    }
+
+    protected function emailMentionInPost(Entity $notification): void
+    {
+        if ($notification->get('type') != 'MentionInPost') {
+            return;
+        }
+
+        if (!$this->getConfig()->get('mentionEmailNotifications', false)) {
+            return;
+        }
+
+        if (empty($userId = $notification->get('userId'))) {
+            return;
+        }
+
+        if (empty($user = $this->getEntityManager()->getEntity('User', $userId))) {
+            return;
+        }
+
+        if (empty($emailAddress = $user->get('emailAddress'))) {
+            return;
+        }
+
+        if (empty($preferences = $this->getEntityManager()->getEntity('Preferences', $userId))) {
+            return;
+        }
+
+        if (!$preferences->get('receiveMentionEmailNotifications')) {
+            return;
+        }
+
+        if ($notification->get('relatedType') !== 'Note' || !$notification->get('relatedId')) {
+            return;
+        }
+
+        if (empty($note = $this->getEntityManager()->getEntity('Note', $notification->get('relatedId')))) {
+            return;
+        }
+
+        $parentId = $note->get('parentId');
+        $parentType = $note->get('parentType');
+
+        $data = [];
+
+        if ($parentId && $parentType) {
+            $parent = $this->getEntityManager()->getEntity($parentType, $parentId);
+            if (!$parent) {
+                return;
+            }
+
+            $data['url'] = $this->getSiteUrl($user) . '/#' . $parentType . '/view/' . $parentId;
+            $data['parentName'] = $parent->get('name');
+            $data['parentType'] = $parentType;
+            $data['parentId'] = $parentId;
+        } else {
+            $data['url'] = $this->getSiteUrl($user) . '/#Notification';
+        }
+
+        $data['userName'] = $note->get('createdByName');
+
+        $data['post'] = nl2br($note->get('post'));
+
+        $subjectTpl = $this->getTemplateFileManager()->getTemplate('mention', 'subject');
+        $subjectTpl = str_replace(["\n", "\r"], '', $subjectTpl);
+
+        $bodyTpl = $this->getTemplateFileManager()->getTemplate('mention', 'body');
+
+        $subject = $this->getHtmlizer()->render($note, $subjectTpl, 'mention-email-subject', $data, true);
+        $body = $this->getHtmlizer()->render($note, $bodyTpl, 'mention-email-body', $data, true);
+
+        try {
+            $this->getMailSender()->send(
+                [
+                    'subject' => $subject,
+                    'body'    => $body,
+                    'isHtml'  => true,
+                    'to'      => $emailAddress
+                ]
+            );
+        } catch (\Exception $e) {
+            $GLOBALS['log']->error('emailMentionInPost: [' . $e->getCode() . '] ' . $e->getMessage());
+        }
+    }
+
+    protected function getSiteUrl(\Espo\Entities\User $user): string
+    {
+        if ($user->get('isPortalUser')) {
+            if (!array_key_exists($user->id, $this->userIdPortalCacheMap)) {
+                $this->userIdPortalCacheMap[$user->id] = null;
+
+                $portalIdList = $user->getLinkMultipleIdList('portals');
+                $defaultPortalId = $this->getConfig()->get('defaultPortalId');
+
+                $portalId = null;
+
+                if (in_array($defaultPortalId, $portalIdList)) {
+                    $portalId = $defaultPortalId;
+                } else {
+                    if (count($portalIdList)) {
+                        $portalId = $portalIdList[0];
+                    }
+                }
+
+                if ($portalId) {
+                    $portal = $this->getEntityManager()->getEntity('Portal', $portalId);
+                    $this->getEntityManager()->getRepository('Portal')->loadUrlField($portal);
+                    $this->userIdPortalCacheMap[$user->id] = $portal;
+                }
+            } else {
+                $portal = $this->userIdPortalCacheMap[$user->id];
+            }
+
+            if ($portal) {
+                return rtrim($portal->get('url'), '/');
+            }
+        }
+
+        return $this->getConfig()->getSiteUrl();
+    }
+
+    protected function getTemplateFileManager(): TemplateFileManager
+    {
+        return $this->getInjection('templateFileManager');
+    }
+
+    protected function getHtmlizer(): Htmlizer
+    {
+        if (is_null($this->htmlizer)) {
+            $this->htmlizer = new Htmlizer($this->getInjection('fileManager'), $this->getInjection('dateTime'), $this->getInjection('number'), null);
+        }
+
+        return $this->htmlizer;
+    }
+
+    protected function getMailSender(): Sender
+    {
+        return $this->getInjection('mailSender');
+    }
+
+    protected function init()
+    {
+        parent::init();
+
+        $this->addDependency('templateFileManager');
+        $this->addDependency('fileManager');
+        $this->addDependency('dateTime');
+        $this->addDependency('number');
+        $this->addDependency('mailSender');
     }
 }
 
