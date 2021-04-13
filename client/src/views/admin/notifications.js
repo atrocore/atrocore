55fb8b8691305cace23ccd9ab53c99b6dc8af6a1
r.ratsun <r.ratsun@gmail.com>
2021-04-13 19:27:44 +0300

    .

--- client/src/views/admin/notifications.js
+++ client/src/views/admin/notifications.js
@@ -36,62 +36,6 @@ Espo.define('views/admin/notifications', 'views/settings/record/edit', function 
 
         layoutName: 'notifications',
 
-        dependencyDefs: {
-            'assignmentEmailNotifications': {
-                map: {
-                    true: [
-                        {
-                            action: 'show',
-                            fields: ['assignmentEmailNotificationsEntityList']
-                        }
-                    ]
-                },
-                default: [
-                    {
-                        action: 'hide',
-                        fields: ['assignmentEmailNotificationsEntityList']
-                    }
-                ]
-            },
-            'adminNotifications': {
-                map: {
-                    true: [
-                        {
-                            action: 'show',
-                            fields: ['adminNotificationsNewVersion', 'adminNotificationsNewExtensionVersion']
-                        }
-                    ]
-                },
-                default: [
-                    {
-                        action: 'hide',
-                        fields: ['adminNotificationsNewVersion', 'adminNotificationsNewExtensionVersion']
-                    }
-                ]
-            }
-        },
-
-        setup: function () {
-            Dep.prototype.setup.call(this);
-
-            this.controlStreamEmailNotificationsEntityList();
-            this.listenTo(this.model, 'change', function (model) {
-                if (model.hasChanged('streamEmailNotifications') || model.hasChanged('portalStreamEmailNotifications')) {
-                    this.controlStreamEmailNotificationsEntityList();
-                }
-            }, this);
-        },
-
-        controlStreamEmailNotificationsEntityList: function () {
-            if (this.model.get('streamEmailNotifications') || this.model.get('portalStreamEmailNotifications')) {
-                this.showField('streamEmailNotificationsEntityList');
-                this.showField('streamEmailNotificationsTypeList');
-            } else {
-                this.hideField('streamEmailNotificationsEntityList');
-                this.hideField('streamEmailNotificationsTypeList');
-            }
-        }
-
     });
 
 });
