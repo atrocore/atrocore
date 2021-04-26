9a8691d05bf6e463ca2245445a5af746856478ab
r.ratsun <r.ratsun@gmail.com>
2021-04-26 17:06:15 +0300

    .

--- app/Espo/Repositories/User.php
+++ app/Espo/Repositories/User.php
@@ -42,6 +42,26 @@ use \Espo\Core\Exceptions\Conflict;
 
 class User extends \Espo\Core\ORM\Repositories\RDB
 {
+    /**
+     * Get admin users
+     *
+     * @return array
+     */
+    public function getAdminUsers(): array
+    {
+        $sql
+            = 'SELECT 
+                 u.id AS id, p.data AS data
+               FROM user AS u
+               LEFT JOIN preferences AS p ON u.id = p.id
+               WHERE u.deleted = 0 AND u.is_admin = 1 AND u.is_active = 1';
+
+        $sth = $this->getEntityManager()->getPDO()->prepare($sql);
+        $sth->execute();
+
+        return $sth->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
     protected function init()
     {
         parent::init();
