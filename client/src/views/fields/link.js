41c1b679f3a2361ebf3df1c39aa7745187313b52
r.ratsun <r.ratsun@gmail.com>
2021-04-30 15:17:30 +0300

    .

--- client/src/views/fields/link.js
+++ client/src/views/fields/link.js
@@ -114,6 +114,11 @@ Espo.define('views/fields/link', 'views/fields/base', function (Dep) {
                 }.bind(this));
             }
 
+            if (this.foreignScope === 'User' && this.model.getFieldParam(this.name, 'currentUserAsDefault') && this.mode === 'edit' && !this.model.get('id')) {
+                this.model.set(this.idName, this.getUser().get('id'));
+                this.model.set(this.nameName, this.getUser().get('name'));
+            }
+
             if ('createDisabled' in this.options) {
                 this.createDisabled = this.options.createDisabled;
             }
