9248e5320e15d56f14122854350886e31ad11be1
r.ratsun <r.ratsun@gmail.com>
2021-06-08 11:59:54 +0300

    ability to disable cors

--- app/Treo/Core/Application.php
+++ app/Treo/Core/Application.php
@@ -224,6 +224,13 @@ class Application
             $this->logoutAll();
         }
 
+        if ($this->getConfig()->get('disableCorsPolicy', false) && !empty($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "OPTIONS") {
+            header('Access-Control-Allow-Origin: *');
+            header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization, Authorization-Token");
+            header("HTTP/1.1 200 OK");
+            exit;
+        }
+
         // prepare base route
         $baseRoute = '/api/v1';
 
