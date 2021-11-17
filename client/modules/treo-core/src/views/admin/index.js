7fb2febeff206dc9c1a3b50512237589649b5be0
Roman Zablodskyi <r.zablodskyi@gmail.com>
2021-11-17 10:26:33 +0200

    Added twitter feed on admin page

--- client/modules/treo-core/src/views/admin/index.js
+++ client/modules/treo-core/src/views/admin/index.js
@@ -38,6 +38,66 @@ Espo.define('treo-core:views/admin/index', 'class-replace!treo-core:views/admin/
         afterRender() {
             Dep.prototype.afterRender.call(this);
 
+            let navbar = $('.navbar-nav.navbar-right');
+            let pageHeader = $('.page-header');
+            let container = $('.admin-tables-container');
+            let rightColumn = $('.admin-right-column');
+
+            if (container.length) {
+                let prevScrollTop = 0;
+                let primaryContainerPosition = container.get(0).getBoundingClientRect();
+
+                $(window).on('scroll', () => {
+                    if ($(window).width() >= 992 && container.height() < rightColumn.height()) {
+                        let currentScrollTop = $(window).scrollTop();
+                        let containerPosition = container.get(0).getBoundingClientRect();
+
+                        if (prevScrollTop < currentScrollTop) {
+                            if (containerPosition.bottom < $(window).height()) {
+                                container.removeClass('scrolled');
+                                container.addClass('fixed-bottom');
+
+                                container.css({width: container.parent().width() + 'px', top: ''});
+                            } else if (container.hasClass('fixed-top')){
+                                container.addClass('scrolled');
+                                container.removeClass('fixed-top');
+
+                                container.css({top: currentScrollTop + 'px'});
+                            }
+                        } else {
+                            if (container.hasClass('fixed-bottom')) {
+                                let offset = container.offset().top;
+
+                                container.removeClass('fixed-bottom');
+                                container.addClass('scrolled');
+
+                                container.css({top: offset - primaryContainerPosition.top + 'px'});
+                            } else if (containerPosition.top > navbar.height()) {
+                                container.removeClass('scrolled');
+                                container.css({top: ''});
+
+                                container.addClass('fixed-top');
+                                container.css({width: container.parent().width() + 'px'});
+                            }
+
+                            if (currentScrollTop < navbar.height() + pageHeader.height()) {
+                                container.removeClass('fixed-top');
+                                container.css({top: ''});
+                            }
+                        }
+
+                        prevScrollTop = currentScrollTop;
+                    }
+                });
+
+                $(window).on('resize', () => {
+                    if ($(window).width() < 992) {
+                        container.removeClass('fixed-top fixed-bottom scrolled');
+                        container.css({top: '', width: ''});
+                    }
+                });
+            }
+
             $('footer').removeClass('is-collapsed not-collapsed');
         }
     })
