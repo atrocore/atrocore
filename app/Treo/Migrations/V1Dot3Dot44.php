773167070718e59f93a0f901b28b924756b579da
r.ratsun <r.ratsun@gmail.com>
2021-12-17 15:26:46 +0200

    .

---
+++ app/Treo/Migrations/V1Dot3Dot44.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * This file is part of EspoCRM and/or AtroCore.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2019 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * AtroCore is EspoCRM-based Open Source application.
+ * Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
+ *
+ * AtroCore as well as EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AtroCore as well as EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
+ * and "AtroCore" word.
+ */
+
+declare(strict_types=1);
+
+namespace Treo\Migrations;
+
+use Treo\Core\Migration\Base;
+
+class V1Dot3Dot44 extends Base
+{
+    public function up(): void
+    {
+        $this->execute("ALTER TABLE `user` ADD portal_id VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci");
+        $this->execute("CREATE INDEX IDX_PORTAL_ID ON `user` (portal_id)");
+
+        $data = $this
+            ->getPDO()
+            ->query("SELECT * FROM `portal_user` WHERE deleted=0")
+            ->fetchAll(\PDO::FETCH_ASSOC);
+
+        foreach ($data as $v) {
+            $this->execute("UPDATE `user` SET portal_id='{$v['portal_id']}' WHERE id='{$v['user_id']}'");
+        }
+
+        $this->execute("DROP TABLE portal_user");
+    }
+
+    public function down(): void
+    {
+//        $this->execute("");
+    }
+
+    protected function execute(string $sql): void
+    {
+        try {
+            $this->getPDO()->exec($sql);
+        } catch (\Throwable $e) {
+            // ignore all
+        }
+    }
+}
