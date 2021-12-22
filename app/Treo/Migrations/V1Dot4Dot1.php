f271aa0a0f7581520d89371812da3b9766a163f6
r.ratsun <r.ratsun@gmail.com>
2021-12-22 12:09:03 +0200

    .

--- app/Treo/Migrations/V1Dot4Dot1.php
+++ app/Treo/Migrations/V1Dot4Dot1.php
@@ -35,6 +35,7 @@ declare(strict_types=1);
 
 namespace Treo\Migrations;
 
+use Treo\Console\Cron;
 use Treo\Core\Migration\Base;
 
 class V1Dot4Dot1 extends Base
@@ -45,6 +46,7 @@ class V1Dot4Dot1 extends Base
         $this->execute("ALTER TABLE `queue_item` CHANGE `sort_order` sort_order INT AUTO_INCREMENT NOT NULL UNIQUE COLLATE utf8mb4_unicode_ci");
         $this->execute("CREATE UNIQUE INDEX UNIQ_BA4B6DE845AFA4EA ON `queue_item` (sort_order)");
         $this->execute("ALTER TABLE `queue_item` ADD priority VARCHAR(255) DEFAULT 'Normal' COLLATE utf8mb4_unicode_ci");
+        file_put_contents(Cron::DAEMON_KILLER, '1');
     }
 
     public function down(): void
@@ -52,6 +54,7 @@ class V1Dot4Dot1 extends Base
         $this->execute("DROP INDEX sort_order ON `queue_item`");
         $this->execute("ALTER TABLE `queue_item` CHANGE `sort_order` sort_order INT DEFAULT '0' COLLATE utf8mb4_unicode_ci");
         $this->execute("ALTER TABLE `queue_item` DROP priority");
+        file_put_contents(Cron::DAEMON_KILLER, '1');
     }
 
     protected function execute(string $sql): void
