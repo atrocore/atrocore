e28002b1f7aed9dce21ac9a2a297fefa890e0954
r.ratsun <r.ratsun@gmail.com>
2022-01-28 18:18:14 +0200

    .

---
+++ app/Treo/Migrations/V1Dot4Dot14.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * This file is part of EspoCRM and/or AtroCore.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2019 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * AtroCore is EspoCRM-based Open Source application.
+ * Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
+ *
+ * AtroCore as well as EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AtroCore as well as EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
+ * and "AtroCore" word.
+ */
+
+declare(strict_types=1);
+
+namespace Treo\Migrations;
+
+use Treo\Console\Cron;
+use Treo\Core\Migration\Base;
+
+class V1Dot4Dot14 extends Base
+{
+    public function up(): void
+    {
+        $this->execute("CREATE TABLE `pseudo_transaction` (`id` VARCHAR(24) NOT NULL COLLATE utf8mb4_unicode_ci, `deleted` TINYINT(1) DEFAULT '0' COLLATE utf8mb4_unicode_ci, `sort_order` INT AUTO_INCREMENT NOT NULL UNIQUE COLLATE utf8mb4_unicode_ci, `entity_type` VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `entity_id` VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `action` VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `input_data` MEDIUMTEXT DEFAULT NULL COLLATE utf8mb4_unicode_ci, `created_by_id` VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `parent_id` VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci, INDEX `IDX_CREATED_BY_ID` (created_by_id), INDEX `IDX_PARENT_ID` (parent_id), INDEX `IDX_ENTITY_TYPE` (entity_type, deleted), INDEX `IDX_ENTITY_ID` (entity_id, deleted), UNIQUE INDEX `UNIQ_433845D45AFA4EA` (sort_order), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB");
+    }
+
+    public function down(): void
+    {
+        $this->execute("DROP table `pseudo_transaction`");
+    }
+
+    protected function execute(string $sql): void
+    {
+        try {
+            $this->getPDO()->exec($sql);
+        } catch (\Throwable $e) {
+            // ignore all
+        }
+    }
+}
