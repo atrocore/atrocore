9d853a762d3ef91ef17a1d4c391b39cf80854877
r.ratsun <r.ratsun@gmail.com>
2022-03-15 17:52:56 +0200

    .

---
+++ client/src/views/record/detail-tree.js
@@ -0,0 +1,80 @@
+/*
+ * This file is part of EspoCRM and/or AtroCore.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2019 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * AtroCore is EspoCRM-based Open Source application.
+ * Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
+ *
+ * AtroCore as well as EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AtroCore as well as EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
+ * and "AtroCore" word.
+ *
+ * This software is not allowed to be used in Russia and Belarus.
+ */
+
+Espo.define('views/record/detail-tree', 'views/record/detail',
+    Dep => Dep.extend({
+
+        template: 'record/detail-tree',
+
+        setup() {
+            Dep.prototype.setup.call(this);
+
+            if (!this.isWide && this.type !== 'editSmall' && this.type !== 'detailSmall') {
+                this.isTreePanel = true;
+                this.setupTreePanel();
+            }
+        },
+
+        data() {
+            return _.extend({isTreePanel: this.isTreePanel}, Dep.prototype.data.call(this))
+        },
+
+        setupTreePanel() {
+            this.createView('treePanel', 'views/record/panels/tree-panel', {
+                el: `${this.options.el} .catalog-tree-panel`,
+                scope: this.scope,
+                model: this.model
+            }, view => {
+                view.listenTo(view, 'select-node', data => {
+                    this.selectNode(data);
+                });
+                view.listenTo(view, 'tree-init', () => {
+                    this.treeInit(view);
+                });
+            });
+        },
+
+        selectNode(data) {
+            window.location.href = `/#${this.scope}/view/${data.id}`;
+        },
+
+        treeInit(view) {
+            if (view.model && view.model.get('id')) {
+                view.selectTreeNode(view.parseRoute(view.model.get('categoryRoute')), view.model.get('id'));
+            }
+        },
+
+    })
+);
+
