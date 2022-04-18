f388405972bd9ea2dd6c60438b16f0e4683c4b70
r.ratsun <r.ratsun@gmail.com>
2022-04-18 12:19:06 +0300

    .

--- client/src/views/header.js
+++ client/src/views/header.js
@@ -38,15 +38,38 @@ Espo.define('views/header', 'view', function (Dep) {
 
         template: 'header',
 
+        updatedOverviewFilters: [],
+
+        baseOverviewFilters: [
+            {
+                name: 'fieldsFilter',
+                view: 'treo-core:views/fields/overview-fields-filter'
+            },
+            {
+                name: 'localesFilter',
+                view: 'treo-core:views/fields/overview-locales-filter'
+            }
+        ],
+
+        events: _.extend({
+            'click a:not([data-action])': function (e) {
+                let path = e.currentTarget.getAttribute("href");
+                e.preventDefault();
+                this.getRouter().checkConfirmLeaveOut(function () {
+                    this.getRouter().navigate(path, {trigger: true});
+                }.bind(this), this, false);
+            }
+        }, Dep.prototype.events),
+
         data: function () {
-            var data = {};
+            let data = {};
             if ('getHeader' in this.getParentView()) {
                 data.header = this.getParentView().getHeader();
             }
             data.scope = this.scope || this.getParentView().scope;
             data.items = this.getItems();
-
             data.isXsSingleRow = this.options.isXsSingleRow;
+            data.overviewFilters = this.updatedOverviewFilters.map(filter => filter.name);
 
             if ((data.items.buttons || []).length < 2) {
                 data.isHeaderAdditionalSpace = true;
@@ -64,6 +87,9 @@ Espo.define('views/header', 'view', function (Dep) {
                     }
                 }, this);
             }
+            if (this.model && !this.model.isNew() && this.getMetadata().get(['scopes', this.scope, 'advancedFilters'])) {
+                this.createOverviewFilters();
+            }
         },
 
         afterRender: function () {
@@ -74,7 +100,31 @@ Espo.define('views/header', 'view', function (Dep) {
             var items = this.getParentView().getMenu() || {};
 
             return items;
+        },
+
+        createOverviewFilters() {
+            this.updatedOverviewFilters = this.filterOverviewFilters();
+
+            (this.updatedOverviewFilters || []).forEach(filter => {
+                this.createView(filter.name, filter.view, {
+                    el: `${this.options.el} .field[data-name="${filter.name}"]`,
+                    model: this.model,
+                    name: filter.name,
+                    storageKey: 'overview-filters',
+                    modelKey: 'advancedEntityView'
+                }, view => view.render());
+            });
+        },
+
+        filterOverviewFilters() {
+            return (this.baseOverviewFilters || []).filter(filter => {
+                if (filter.name === 'localesFilter') {
+                    return this.getConfig().get('isMultilangActive') && (this.getConfig().get('inputLanguageList') || []).length
+                }
+                return true;
+            });
         }
+
     });
 });
 
