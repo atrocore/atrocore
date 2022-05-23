5a181b42fe6aa34aee12110e3f61b134debfcbca
r.ratsun <r.ratsun@gmail.com>
2022-05-23 09:27:53 +0300

    .

--- app/Espo/Controllers/Settings.php
+++ app/Espo/Controllers/Settings.php
@@ -59,6 +59,7 @@ class Settings extends \Espo\Core\Controllers\Base
 
         $data['jsLibs'] = $this->getMetadata()->get('app.jsLibs');
         $data['themes'] = $this->getMetadata()->get('themes');
+        $data['coreVersion'] = \Espo\Services\Composer::getCoreVersion();
 
         $data = $this->prepareTabList($data);
 
