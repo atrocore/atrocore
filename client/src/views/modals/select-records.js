5c2a79fae3b84af087a3257d78ed68a90d6512ec
r.ratsun <r.ratsun@gmail.com>
2022-06-15 15:45:39 +0300

    .

--- client/src/views/modals/select-records.js
+++ client/src/views/modals/select-records.js
@@ -276,7 +276,6 @@ Espo.define('views/modals/select-records', ['views/modal', 'search-manager', 'li
                     }, this);
 
                     this.listenTo(this, 'change-view', e => {
-                        view.bool = {};
                         view.resetFilters();
                     });
 
