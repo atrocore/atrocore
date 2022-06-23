5d3453e0ecb7fa01f7ac3bb11937bc53cb4abf9d
r.ratsun <r.ratsun@gmail.com>
2022-06-23 11:11:39 +0300

    .

--- client/src/views/record/list.js
+++ client/src/views/record/list.js
@@ -1383,7 +1383,7 @@ Espo.define('views/record/list', 'view', function (Dep) {
                     if (relatingEntityField.includes(entityType) && entityId) {
                         filteredListLayout.push(item);
                     }
-                } else if (this.getMetadata().get(`entityDefs.${this.entityType}.links.${item.name}.type`) === 'belongsTo') {
+                } else if (this.layoutName === 'listSmall' && this.getMetadata().get(`entityDefs.${this.entityType}.links.${item.name}.type`) === 'belongsTo') {
                     if (this.getMetadata().get(`entityDefs.${this.entityType}.links.${item.name}.entity`) !== entityType) {
                         filteredListLayout.push(item);
                     }
