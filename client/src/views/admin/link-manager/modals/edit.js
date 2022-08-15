d58213336e78c4e798fb4f2a12b11077b2baba44
r.ratsun <r.ratsun@gmail.com>
2022-08-15 11:18:58 +0300

    .

--- client/src/views/admin/link-manager/modals/edit.js
+++ client/src/views/admin/link-manager/modals/edit.js
@@ -163,7 +163,7 @@ Espo.define('views/admin/link-manager/modals/edit', ['views/modal', 'views/admin
                 },
                 readOnly: !isNew
             });
-            this.createView('linkType', 'views/admin/link-manager/fields/link-type', {
+            this.createView('linkType', this.getMetadata().get('app.viewsMap.adminLinkManagerFieldsLinkType') || 'views/fields/enum', {
                 model: model,
                 mode: 'edit',
                 el: this.options.el + ' .field[data-name="linkType"]',
