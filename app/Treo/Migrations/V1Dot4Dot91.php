fb990d5a4601f0846daa37736ac7675bd758e435
r.ratsun <r.ratsun@gmail.com>
2022-08-31 10:02:34 +0300

    .

--- app/Treo/Migrations/V1Dot4Dot91.php
+++ app/Treo/Migrations/V1Dot4Dot91.php
@@ -45,12 +45,18 @@ class V1Dot4Dot91 extends Base
     {
         $this->execute("DROP INDEX IDX_SOURCE_ID ON attachment");
         $this->execute("ALTER TABLE attachment DROP source_id");
+
+        $this->execute("DROP INDEX IDX_PARENT ON attachment");
+        $this->execute("ALTER TABLE attachment DROP parent_id, DROP parent_type");
     }
 
     public function down(): void
     {
         $this->execute("ALTER TABLE attachment ADD source_id VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
         $this->execute("CREATE INDEX IDX_SOURCE_ID ON attachment (source_id)");
+
+        $this->execute("ALTER TABLE attachment ADD parent_id VARCHAR(24) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, ADD parent_type VARCHAR(100) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
+        $this->execute("CREATE INDEX IDX_PARENT ON attachment (parent_type, parent_id)");
     }
 
     protected function execute(string $query): void
