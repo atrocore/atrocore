d2dbf38df7016d23766e1f5ba1395d901c3a4617
r.ratsun <r.ratsun@gmail.com>
2022-08-31 12:56:08 +0300

    .

---
+++ client/src/views/record/panels/for-relationship-type.js
@@ -0,0 +1,111 @@
+/*
+ * This file is part of EspoCRM and/or AtroCore.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2019 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * AtroCore is EspoCRM-based Open Source application.
+ * Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
+ *
+ * AtroCore as well as EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AtroCore as well as EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
+ * and "AtroCore" word.
+ *
+ * This software is not allowed to be used in Russia and Belarus.
+ */
+
+Espo.define('views/record/panels/for-relationship-type', 'views/record/panels/relationship', Dep => {
+
+    return Dep.extend({
+
+        rowActionsView: 'views/record/row-actions/relationship-no-unlink',
+
+        setup() {
+            Dep.prototype.setup.call(this);
+
+            const relationshipScope = this.defs.relationshipScope;
+            const relationshipEntities = Espo.Utils.clone(this.defs.relationshipEntities);
+
+            if (relationshipEntities.length === 2) {
+                this.actionList.unshift({
+                    label: 'Select',
+                    action: 'selectRelatedEntity',
+                    data: {
+                        link: this.panelName,
+                        scope: relationshipEntities.filter(entity => entity !== this.model.urlRoot).shift(),
+                        afterSelectCallback: "createRelationshipEntitiesViaIds"
+                    },
+                    acl: 'create',
+                    aclScope: relationshipScope
+                });
+            }
+
+            this.actionList.push({
+                label: 'deleteAll',
+                action: 'deleteAllRelationshipEntities',
+                data: {
+                    "relationshipScope": relationshipScope
+                },
+                acl: 'delete',
+                aclScope: relationshipScope
+            });
+        },
+
+        createRelationshipEntitiesViaIds(selectObj) {
+            const relationshipScope = this.defs.relationshipScope;
+            const relationshipEntities = Espo.Utils.clone(this.defs.relationshipEntities);
+
+            let data = {
+                entityTypeFrom: this.model.name,
+                entityIdFrom: this.model.id,
+                entityTypeTo: relationshipEntities.filter(entity => entity !== this.model.urlRoot).shift(),
+                entityIdsTo: selectObj.map(item => item.id)
+            };
+
+            this.ajaxPostRequest(`${relationshipScope}/action/createRelationshipEntitiesViaIds`, data).then(() => {
+                this.notify('Created', 'success');
+                this.actionRefresh();
+                this.model.trigger('after:relate', this.panelName);
+            });
+        },
+
+        actionDeleteAllRelationshipEntities(data) {
+            this.confirm(this.translate('deleteAllConfirmation', 'messages'), () => {
+                this.notify('Please wait...');
+                $.ajax({
+                    url: data.relationshipScope + '/action/deleteAll',
+                    type: 'POST',
+                    data: JSON.stringify({
+                        entityType: this.model.name,
+                        entityId: this.model.id
+                    }),
+                }).done(() => {
+                    this.notify(false);
+                    this.notify('Removed', 'success');
+                    this.collection.fetch();
+                    this.model.trigger('after:unrelate');
+                });
+            });
+        },
+
+    });
+});
+
