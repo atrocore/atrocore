14350f41a4f179f3a8b8ad6ae775e3ca9423bf68
r.ratsun <r.ratsun@gmail.com>
2023-02-21 11:19:58 +0200

    .

--- app/Treo/Migrations/V1Dot5Dot28.php
+++ app/Treo/Migrations/V1Dot5Dot28.php
@@ -43,11 +43,19 @@ class V1Dot5Dot28 extends Base
     {
         $this->getPDO()->exec("CREATE INDEX IDX_STATUS ON queue_item (status, deleted)");
         $this->getPDO()->exec("CREATE INDEX IDX_SORT_ORDER ON queue_item (sort_order, deleted)");
+
+        $this->getPDO()->exec("DROP INDEX IDX_NUMBER ON notification");
+        $this->getPDO()->exec("CREATE INDEX IDX_READ ON notification (`read`, deleted)");
+        $this->getPDO()->exec("CREATE INDEX IDX_NUMBER ON notification (number, deleted)");
     }
 
     public function down(): void
     {
         $this->getPDO()->exec("DROP INDEX IDX_STATUS ON queue_item");
         $this->getPDO()->exec("DROP INDEX IDX_SORT_ORDER ON queue_item");
+
+        $this->getPDO()->exec("DROP INDEX IDX_READ ON notification");
+        $this->getPDO()->exec("DROP INDEX IDX_NUMBER ON notification");
+        $this->getPDO()->exec("CREATE INDEX IDX_NUMBER ON notification (number)");
     }
 }
