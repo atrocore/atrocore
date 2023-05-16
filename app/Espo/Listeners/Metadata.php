a7f09b29dc741a14a266e07b6cb0fd33d04df42c
Raoul Dzoukou <rd@atrocore.com>
2023-05-16 12:07:58 +0100

    Range Float and range int filters

--- app/Espo/Listeners/Metadata.php
+++ app/Espo/Listeners/Metadata.php
@@ -90,7 +90,7 @@ class Metadata extends AbstractListener
                     ];
                 }
 
-                $data['entityDefs'][$entity]['fields'][$field]['filterDisabled'] = true;
+                // $data['entityDefs'][$entity]['fields'][$field]['filterDisabled'] = true;
 
                 $fieldFrom = $field . 'From';
                 $fieldTo = $field . 'To';
