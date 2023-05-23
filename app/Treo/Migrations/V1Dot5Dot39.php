5825bc5f05b3f8aea6462990dec1f2e3e04841a1
r.ratsun <r.ratsun@gmail.com>
2023-05-23 14:22:37 +0300

    .

--- app/Treo/Migrations/V1Dot5Dot39.php
+++ app/Treo/Migrations/V1Dot5Dot39.php
@@ -39,11 +39,19 @@ class V1Dot5Dot39 extends Base
 {
     public function up(): void
     {
-        $this->getPDO()->exec("DROP TABLE array_value");
+        $this->exec("DROP TABLE array_value");
     }
 
     public function down(): void
     {
         $this->getPDO()->exec("CREATE TABLE array_value (id VARCHAR(24) NOT NULL COLLATE `utf8mb4_unicode_ci`, deleted TINYINT(1) DEFAULT '0' COLLATE `utf8mb4_unicode_ci`, value VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, attribute VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, entity_id VARCHAR(24) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, entity_type VARCHAR(100) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, INDEX IDX_ENTITY (entity_id, entity_type), INDEX IDX_ENTITY_TYPE_VALUE (entity_type, value), INDEX IDX_ENTITY_VALUE (entity_type, entity_id, value), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB");
+    }
+
+    protected function exec(string $query): void
+    {
+        try {
+            $this->getPDO()->exec($query);
+        } catch (\Throwable $e) {
+        }
     }
 }
