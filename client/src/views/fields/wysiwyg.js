cb763e7e6c8d9efbb9bfdc3fdc834af007f2865b
DouglasWafo <dw@atrocore.com>
2023-05-31 11:20:06 +0100

    Remove bad syntax

--- client/src/views/fields/wysiwyg.js
+++ client/src/views/fields/wysiwyg.js
@@ -433,8 +433,12 @@ Espo.define('views/fields/wysiwyg', ['views/fields/text', 'lib!Summernote'], fun
             var data = {};
             if (!this.model.has('isHtml') || this.model.get('isHtml')) {
                 data[this.name] = this.$summernote.summernote('code');
+                //check if empty
+                if (data[this.name] === '') data[this.name] = null;
             } else {
-                data[this.name] = this.$element.val();
+                data[this.name] = this.$element ? this.$element.val() : null;
+                //check if empty
+                if (data[this.name]==='') data[this.name] = null;
             }
 
             if (this.model.has('isHtml')) {
