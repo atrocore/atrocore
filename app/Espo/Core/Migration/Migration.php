1092cb442ce517b6f73edfe20c8dec508b8549c7
r.ratsun <r.ratsun@gmail.com>
2023-07-05 11:25:30 +0300

    .

--- app/Espo/Core/Migration/Migration.php
+++ app/Espo/Core/Migration/Migration.php
@@ -100,7 +100,7 @@ class Migration extends Injectable
                     && $from != $className
                     && in_array($className, $migrations)
                     && !empty($migration = $this->createMigration($module, $className))) {
-                    self::renderLine("Run migration '" . str_replace(['V', 'Dot'], ['', '.'], $className) . "'");
+                    self::renderLine("Run migration " . str_replace(['V', 'Dot'], ['', '.'], $className));
                     $migration->up();
                 }
             }
@@ -112,7 +112,7 @@ class Migration extends Injectable
                     && $to != $className
                     && in_array($className, $migrations)
                     && !empty($migration = $this->createMigration($module, $className))) {
-                    self::renderLine("Run migration '" . str_replace(['V', 'Dot'], ['', '.'], $className) . "'");
+                    self::renderLine("Run migration " . str_replace(['V', 'Dot'], ['', '.'], $className));
                     $migration->down();
                 }
             }
