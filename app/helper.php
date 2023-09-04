fe7800573bf2be82dcc6393451fd0794bdc30617
Roman Ratsun <rr@atrocore.com>
2023-09-04 14:13:27 +0000

    .

---
+++ app/helper.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.md, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+class_alias("\\Atro\\Composer\\PostUpdate", "\\Treo\\Composer\\PostUpdate");
