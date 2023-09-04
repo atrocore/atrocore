a9b4b0f5306b2acbbcae0462ae16572d3353547e
Roman Ratsun <rr@atrocore.com>
2023-09-04 15:00:25 +0000

    .

--- app/Atro/Core/FileStorage/Storages/Base.php
+++ app/Atro/Core/FileStorage/Storages/Base.php
@@ -1,45 +1,19 @@
 <?php
-/*
- * This file is part of EspoCRM and/or AtroCore.
+/**
+ * AtroCore Software
  *
- * EspoCRM - Open Source CRM application.
- * Copyright (C) 2014-2019 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
- * Website: http://www.espocrm.com
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.md, located in the root directory.
  *
- * AtroCore is EspoCRM-based Open Source application.
- * Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
- *
- * AtroCore as well as EspoCRM is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * AtroCore as well as EspoCRM is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
- *
- * The interactive user interfaces in modified source and object code versions
- * of this program must display Appropriate Legal Notices, as required under
- * Section 5 of the GNU General Public License version 3.
- *
- * In accordance with Section 7(b) of the GNU General Public License version 3,
- * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
- * and "AtroCore" word.
+ * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
  */
 
-namespace Treo\Core\FileStorage\Storages;
+namespace Atro\Core\FileStorage\Storages;
 
 use \Espo\Core\Interfaces\Injectable;
 use Espo\Entities\Attachment;
 
-/**
- * Class Base
- * @package Treo\Core\FileStorage\Storages
- */
 abstract class Base implements Injectable
 {
     /**
@@ -78,6 +52,7 @@ abstract class Base implements Injectable
 
     /**
      * @param $name
+     *
      * @return mixed
      */
     protected function getInjection($name)
@@ -120,31 +95,36 @@ abstract class Base implements Injectable
 
     /**
      * @param Attachment $attachment
+     *
      * @return mixed
      */
     abstract public function unlink(Attachment $attachment);
 
     /**
      * @param Attachment $attachment
+     *
      * @return mixed
      */
     abstract public function getContents(Attachment $attachment);
 
     /**
      * @param Attachment $attachment
+     *
      * @return mixed
      */
     abstract public function isFile(Attachment $attachment);
 
     /**
      * @param Attachment $attachment
-     * @param $contents
+     * @param            $contents
+     *
      * @return mixed
      */
     abstract public function putContents(Attachment $attachment, $contents);
 
     /**
      * @param Attachment $attachment
+     *
      * @return mixed
      */
     abstract public function getLocalFilePath(Attachment $attachment);
