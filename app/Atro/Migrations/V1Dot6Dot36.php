d6aedfeb68c2b7e4a6a7fdcc68cd07bb87fcafe3
Raoul Dzoukou <rd@atrocore.com>
2023-09-05 13:37:11 +0100

    .

--- app/Atro/Migrations/V1Dot6Dot36.php
+++ app/Atro/Migrations/V1Dot6Dot36.php
@@ -19,17 +19,11 @@ class V1Dot6Dot36 extends Base
     {
         copy('vendor/atrocore/core/copy/.htaccess', '.htaccess');
         copy('vendor/atrocore/core/copy/index.php', 'index.php');
-
-        $this->getPDO()->exec("UPDATE queue_item set priority='High' where priority='Crucial'");
     }
 
     public function down(): void
     {
         copy('vendor/atrocore/core/copy/.htaccess', '.htaccess');
         copy('vendor/atrocore/core/copy/index.php', 'index.php');
-
-        $this->getPDO()->exec("UPDATE queue_item set priority='Crucial' where priority='High'");
-        $this->getPDO()->exec("UPDATE queue_item set priority='High' where priority='Highest'");
-        $this->getPDO()->exec("UPDATE queue_item set priority='Low' where priority='Lowest'");
     }
 }
