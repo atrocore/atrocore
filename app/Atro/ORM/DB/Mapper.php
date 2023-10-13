8321b2349f743a814c8667df9b65f7c4b15012ed
Roman Ratsun <rr@atrocore.com>
2023-10-13 14:24:42 +0000

    .

--- app/Atro/ORM/DB/Mapper.php
+++ app/Atro/ORM/DB/Mapper.php
@@ -670,22 +670,22 @@ class Mapper implements IMapper
                             echo '<pre>';
                             print_r('333');
                             die();
-
-                            $valArr = $value;
-                            foreach ($valArr as $k => $v) {
-                                $valArr[$k] = $this->pdo->quote($valArr[$k]);
-                            }
-                            $oppose = '';
-                            $emptyValue = '0';
-                            if ($operator == '<>') {
-                                $oppose = 'NOT ';
-                                $emptyValue = '1';
-                            }
-                            if (!empty($valArr)) {
-                                $whereParts[] = $leftPart . " {$oppose}IN " . "(" . implode(',', $valArr) . ")";
-                            } else {
-                                $whereParts[] = "" . $emptyValue;
-                            }
+//
+//                            $valArr = $value;
+//                            foreach ($valArr as $k => $v) {
+//                                $valArr[$k] = $this->pdo->quote($valArr[$k]);
+//                            }
+//                            $oppose = '';
+//                            $emptyValue = '0';
+//                            if ($operator == '<>') {
+//                                $oppose = 'NOT ';
+//                                $emptyValue = '1';
+//                            }
+//                            if (!empty($valArr)) {
+//                                $whereParts[] = $leftPart . " {$oppose}IN " . "(" . implode(',', $valArr) . ")";
+//                            } else {
+//                                $whereParts[] = "" . $emptyValue;
+//                            }
                         }
                     }
                 }
