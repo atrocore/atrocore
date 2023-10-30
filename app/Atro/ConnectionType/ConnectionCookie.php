952db32695d57a8eb34b222f05774861189623e8
Raoul Dzoukou <rd@atrocore.com>
2023-10-30 12:00:30 +0100

    .

---
+++ app/Atro/ConnectionType/ConnectionCookie.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\ConnectionType;
+
+use Espo\Core\Exceptions\BadRequest;
+use Espo\ORM\Entity;
+
+class ConnectionCookie extends AbstractConnection
+{
+    public function connect(Entity $connection)
+    {
+        $body = $connection->get('payload');
+        $url = $connection->get('loginUrl');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if (!empty($response) && $httpCode == 200) {
+            $cookies = $this->extractCookiesFromResponse($response);
+            if (!empty($cookies)) {
+                return [
+                    'cookie' => $cookies
+                ];
+            }
+        }
+
+        throw new BadRequest(sprintf($this->exception('connectionFailed'), 'Connection failed.'));
+    }
+
+    public function extractCookiesFromResponse(string $result): string
+    {
+        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
+        $cookies = array();
+        foreach ($matches[1] as $item) {
+            $cookies[] = $item;
+        }
+
+        return join("; ", $cookies);
+    }
+}
