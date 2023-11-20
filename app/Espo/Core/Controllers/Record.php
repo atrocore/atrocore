5e02c9076276a3480c6d1587c7f897ec576408a9
r.ratsun <33658723+rratsun@users.noreply.github.com>
2023-11-20 09:10:41 +0200

    Update Record.php

--- app/Espo/Core/Controllers/Record.php
+++ app/Espo/Core/Controllers/Record.php
@@ -561,9 +561,10 @@ class Record extends Base
             throw new BadRequest();
         }
 
-        if (!$this->getAcl()->check($this->name, 'edit')) {
+        if (!$this->getAcl()->check($this->name, 'read')) {
             throw new Forbidden();
         }
+        
         return $this->getEntityManager()->getRepository($this->name)->get()->getValueMap();
     }
 
