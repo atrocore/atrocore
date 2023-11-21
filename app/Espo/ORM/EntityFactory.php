f62d350fe7617cd31874c94942ef53b1366da65b
Roman Ratsun <rr@atrocore.com>
2023-11-21 14:22:02 +0000

    .

--- app/Espo/ORM/EntityFactory.php
+++ app/Espo/ORM/EntityFactory.php
@@ -55,7 +55,7 @@ class EntityFactory
             return null;
         }
 
-        return new $className($defs, $this->entityManager);
+        return new $className($defs, $this->entityManager, $name);
     }
 
     public function getEntityManager(): EntityManager
