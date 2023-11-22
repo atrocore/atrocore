ea2e7327734699c6f387b645ca954c0a4407c916
Roman Ratsun <rr@atrocore.com>
2023-11-22 12:34:07 +0000

    .

---
+++ app/Atro/Migrations/V1Dot7Dot14.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Atro\Migrations;
+
+use Atro\Core\Migration\Base;
+
+class V1Dot7Dot14 extends Base
+{
+    public function up(): void
+    {
+        $this->rebuild();
+    }
+
+    public function down(): void
+    {
+        $this->rebuild();
+    }
+}
