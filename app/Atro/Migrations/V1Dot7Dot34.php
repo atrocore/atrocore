bf5b04d90c3cc2b2fb6fc0c28a1989e3b8f26cfb
Raoul Dzoukou <rd@atrocore.com>
2023-12-14 11:18:28 +0100

    .

---
+++ app/Atro/Migrations/V1Dot7Dot34.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Atro\Migrations;
+
+use Atro\Console\Cron;
+use Atro\Core\Migration\Base;
+
+class V1Dot7Dot34 extends Base
+{
+    public function up(): void
+    {
+        $fromSchema = $this->getCurrentSchema();
+        $toSchema = clone $fromSchema;
+
+        $this->addColumn($toSchema, 'account', 'role', ['type' => 'varchar']);
+
+        foreach ($this->schemasDiffToSql($fromSchema, $toSchema) as $sql) {
+            $this->getPDO()->exec($sql);
+        }
+
+    }
+
+    public function down(): void
+    {
+        $fromSchema = $this->getCurrentSchema();
+        $toSchema = clone $fromSchema;
+
+        $this->dropColumn($toSchema, 'account', 'role');
+
+        foreach ($this->schemasDiffToSql($fromSchema, $toSchema) as $sql) {
+            $this->getPDO()->exec($sql);
+        }
+    }
+}
