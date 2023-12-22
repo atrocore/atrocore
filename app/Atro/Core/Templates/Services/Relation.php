0e24ffbee8f0eb5f2ed0f84adc841ee03b3c1b7d
Roman Ratsun <rr@atrocore.com>
2023-12-22 10:08:49 +0000

    .

--- app/Atro/Core/Templates/Services/Relation.php
+++ app/Atro/Core/Templates/Services/Relation.php
@@ -13,6 +13,9 @@ declare(strict_types=1);
 
 namespace Atro\Core\Templates\Services;
 
+use Espo\Core\Exceptions\BadRequest;
+use Espo\Core\Exceptions\Forbidden;
+use Espo\Core\Exceptions\NotFound;
 use Espo\ORM\Entity;
 use Espo\Services\Record;
 
@@ -20,9 +23,9 @@ class Relation extends Record
 {
     protected function afterCreateEntity(Entity $entity, $data)
     {
-        parent::afterCreateEntity($entity, $data);
-
         $this->createHierarchical($entity);
+
+        parent::afterCreateEntity($entity, $data);
     }
 
     public function createHierarchical(Entity $entity): void
@@ -54,6 +57,57 @@ class Relation extends Record
             }
             $parentId = $this->getPseudoTransactionManager()->pushCreateEntityJob($entity->getEntityType(), $input);
             $this->getPseudoTransactionManager()->pushUpdateEntityJob($hierarchicalEntity->getEntityType(), $hierarchicalEntity->get('id'), null, $parentId);
+        }
+    }
+
+    protected function afterDeleteEntity(Entity $entity)
+    {
+        $this->deleteHierarchical($entity);
+
+        parent::afterDeleteEntity($entity);
+    }
+
+    public function deleteHierarchical(Entity $entity): void
+    {
+        $link = $this->getRepository()->getHierarchicalRelation();
+        if (empty($link)) {
+            return;
+        }
+
+        $hierarchicalEntity = $entity->get($link);
+        if (empty($hierarchicalEntity)) {
+            return;
+        }
+
+        $childrenIds = $hierarchicalEntity->getLinkMultipleIdList('children');
+        if (empty($childrenIds[0])) {
+            return;
+        }
+
+        $additionalFields = $this->getRepository()->getAdditionalFieldsNames();
+
+        $where = [];
+        foreach ($childrenIds as $childId) {
+            foreach ($this->getRepository()->getRelationFields() as $relField) {
+                if ($relField === $link) {
+                    $where["{$relField}Id"][] = $childId;
+                } else {
+                    $where["{$relField}Id"] = $entity->get("{$relField}Id");
+                }
+            }
+            foreach ($additionalFields as $additionalField) {
+                $where[$additionalField] = $entity->get($additionalField);
+            }
+        }
+
+        $childrenRecords = $this->getRepository()->select(['id'])->where($where)->find();
+        foreach ($childrenRecords as $childrenRecord) {
+            try {
+                $this->deleteEntity($childrenRecord->get('id'));
+            } catch (Forbidden $e) {
+            } catch (NotFound $e) {
+            } catch (BadRequest $e) {
+            }
         }
     }
 }
