149a528d5cc68878058742415e1b0db5882e75fd
Roman Ratsun <rr@atrocore.com>
2024-01-16 08:18:53 +0000

    .

---
+++ app/Atro/Migrations/V1Dot8Dot12.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Atro\Migrations;
+
+use Atro\Core\Migration\Base;
+
+class V1Dot8Dot12 extends Base
+{
+    public function up(): void
+    {
+        $fromSchema = $this->getCurrentSchema();
+        $toSchema = clone $fromSchema;
+
+        $this->addColumn($toSchema, 'action', 'in_background', ['type' => 'bool']);
+
+        foreach ($this->schemasDiffToSql($fromSchema, $toSchema) as $sql) {
+            $this->exec($sql);
+        }
+    }
+
+    public function down(): void
+    {
+    }
+
+    protected function exec(string $query): void
+    {
+        try {
+            $this->getPDO()->exec($query);
+        } catch (\Throwable $e) {
+        }
+    }
+}
