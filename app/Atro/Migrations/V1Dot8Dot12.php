e1f152184a182d783cd34e0905fb4152afbf846b
Roman Ratsun <rr@atrocore.com>
2024-01-19 07:32:06 +0000

    .

--- app/Atro/Migrations/V1Dot8Dot12.php
+++ app/Atro/Migrations/V1Dot8Dot12.php
@@ -20,7 +20,7 @@ class V1Dot8Dot12 extends Base
         $fromSchema = $this->getCurrentSchema();
         $toSchema = clone $fromSchema;
 
-        $this->addColumn($toSchema, 'action', 'in_background', ['type' => 'bool']);
+        $this->addColumn($toSchema, 'action', 'in_background', ['type' => 'bool', 'default' => true]);
         $this->addColumn($toSchema, 'action', 'mass_action', ['type' => 'bool', 'default' => true]);
         $this->addColumn($toSchema, 'action', 'usage', ['type' => 'enum']);
         $this->addColumn($toSchema, 'action', 'display', ['type' => 'enum']);
