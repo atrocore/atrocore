212efd3a1ab0fd3a32122bfc2dbf47b180b17431
Roman Ratsun <rr@atrocore.com>
2024-01-24 12:50:28 +0000

    .

--- client/src/views/admin/auth-token/list.js
+++ client/src/views/admin/auth-token/list.js
@@ -34,12 +34,6 @@ Espo.define('views/admin/auth-token/list', 'views/list', function (Dep) {
 
     return Dep.extend({
 
-        setup: function () {
-            Dep.prototype.setup.call(this);
-
-            this.menu.buttons = [];
-        },
-
         getHeader: function () {
             return '<a href="#Admin">' + this.translate('Administration') + "</a> » " + this.getLanguage().translate('Auth Tokens', 'labels', 'Admin');
         },
