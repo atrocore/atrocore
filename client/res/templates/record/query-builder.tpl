16cedd5e30cfa155056527bd0c2a203ace3f923f
Roman Ratsun <rr@atrocore.com>
2024-02-15 08:34:38 +0000

    .

--- client/res/templates/record/query-builder.tpl
+++ client/res/templates/record/query-builder.tpl
@@ -1,3 +1,6 @@
+{{#if hasAttributeButton}}
+<button class="btn-link btn" data-action="add-attribute-filter"><div>{{translate 'Add Attribute'}}</div></button>
+{{/if}}
 <div class="query-builder"></div>
 <div class="row filter-actions">
     <button class="btn-link btn" data-action="search"><div>{{translate 'Apply filter'}}</div></button>
--- client/src/views/record/query-builder.js
+++ client/src/views/record/query-builder.js
@@ -14,12 +14,20 @@ Espo.define('views/record/query-builder', ['view', 'lib!Interact', 'lib!QueryBui
 
         template: 'record/query-builder',
 
+        filters: [],
+
         events: {
             'click button[data-action="search"]': function (e) {
                 this.search();
             },
             'click button[data-action="reset-filter"]': function (e) {
                 this.resetFilters();
+            },
+            'click button[data-action="add-attribute-filter"]': function (e) {
+                e.stopPropagation();
+                e.preventDefault();
+
+                this.addAttributeFilter();
             },
         },
 
@@ -29,33 +37,61 @@ Espo.define('views/record/query-builder', ['view', 'lib!Interact', 'lib!QueryBui
 
             this.model = new this.collection.model();
             this.model.clear();
+
+            // set translates
+            $.fn.queryBuilder.regional['main'] = this.getLanguage().data.Global.queryBuilderFilter;
+            $.fn.queryBuilder.defaults({lang_code: 'main'});
         },
 
         data() {
-            return {};
+            return {
+                hasAttributeButton: this.model.urlRoot === 'Foo'
+            };
         },
 
-        search() {
-            this.collection.where = [];
+        initQueryBuilderFilter(rules = []) {
+            this.$el.find('.query-builder').queryBuilder({
+                allow_empty: true,
+                operators: [
+                    {type: 'contains'},
+                    {type: 'not_contains'},
+                    {type: 'equal'},
+                    {type: 'not_equal'},
+                    {type: 'less'},
+                    {type: 'less_or_equal'},
+                    {type: 'greater'},
+                    {type: 'greater_or_equal'},
+                    {type: 'between'},
+                    {type: 'in'},
+                    {type: 'not_in'},
+                    {type: 'is_null'},
+                    {type: 'is_not_null'},
+                    {type: 'linked_with', nb_inputs: 1, multiple: true, apply_to: ['string']},
+                    {type: 'not_linked_with', nb_inputs: 1, multiple: true, apply_to: ['string']},
+                    {type: 'array_any_of', nb_inputs: 1, multiple: true, apply_to: ['string']},
+                    {type: 'array_none_of', nb_inputs: 1, multiple: true, apply_to: ['string']},
+                    {type: 'is_linked', nb_inputs: 0, apply_to: ['string']},
+                    {type: 'is_not_linked', nb_inputs: 0, apply_to: ['string']},
+                    {type: 'query_in', nb_inputs: 1, apply_to: ['string']},
+                    {type: 'query_linked_with', nb_inputs: 1, apply_to: ['string']},
+                ],
+                rules: rules,
+                filters: this.filters,
+                plugins: {
+                    sortable: {
+                        icon: 'fas fa-sort'
+                    }
+                }
+            });
 
-            const qbRules = this.$el.find('.query-builder').queryBuilder('getRules');
-
-            if (qbRules && qbRules.rules && qbRules.rules.length > 0) {
-                this.collection.where = [qbRules];
-            }
-
-            this.collection.fetch().then(() => Backbone.trigger('after:search', this.collection));
-
-            Backbone.Events.trigger('search', this);
+            this.$el.find('.query-builder').on('afterUpdateRuleOperator.queryBuilder', (e, rule) => {
+                this.model.trigger('afterUpdateRuleOperator', rule);
+            });
         },
 
-        resetFilters() {
-            this.$el.find('.query-builder').queryBuilder('setRules', []);
-            this.search();
-        },
+        prepareFilters(callback) {
+            this.filters = [];
 
-        afterRender() {
-            let filters = [];
             let promiseList = [];
             $.each(this.getMetadata().get(['entityDefs', this.scope, 'fields']), (field, fieldDefs) => {
                 if (fieldDefs.filterDisabled) {
@@ -69,7 +105,7 @@ Espo.define('views/record/query-builder', ['view', 'lib!Interact', 'lib!QueryBui
                     this.createView(field, view, {name: field, model: this.model}, view => {
                         let filter = view.createQueryBuilderFilter();
                         if (filter) {
-                            filters.push(filter);
+                            this.filters.push(filter);
                         }
                         resolve();
                     });
@@ -77,50 +113,79 @@ Espo.define('views/record/query-builder', ['view', 'lib!Interact', 'lib!QueryBui
             });
 
             Promise.all(promiseList).then(() => {
-                if (filters.length > 0) {
-                    // set translates
-                    $.fn.queryBuilder.regional['main'] = this.getLanguage().data.Global.queryBuilderFilter;
-                    $.fn.queryBuilder.defaults({lang_code: 'main'});
+                callback();
+            });
+        },
 
-                    // init
-                    this.$el.find('.query-builder').queryBuilder({
-                        allow_empty: true,
-                        operators: [
-                            {type: 'contains'},
-                            {type: 'not_contains'},
-                            {type: 'equal'},
-                            {type: 'not_equal'},
-                            {type: 'less'},
-                            {type: 'less_or_equal'},
-                            {type: 'greater'},
-                            {type: 'greater_or_equal'},
-                            {type: 'between'},
-                            {type: 'in'},
-                            {type: 'not_in'},
-                            {type: 'is_null'},
-                            {type: 'is_not_null'},
-                            {type: 'linked_with', nb_inputs: 1, multiple: true, apply_to: ['string']},
-                            {type: 'not_linked_with', nb_inputs: 1, multiple: true, apply_to: ['string']},
-                            {type: 'array_any_of', nb_inputs: 1, multiple: true, apply_to: ['string']},
-                            {type: 'array_none_of', nb_inputs: 1, multiple: true, apply_to: ['string']},
-                            {type: 'is_linked', nb_inputs: 0, apply_to: ['string']},
-                            {type: 'is_not_linked', nb_inputs: 0, apply_to: ['string']},
-                            {type: 'query_in', nb_inputs: 1, apply_to: ['string']},
-                            {type: 'query_linked_with', nb_inputs: 1, apply_to: ['string']},
-                        ],
-                        rules: [],
-                        filters: filters,
-                        plugins: {
-                            sortable: {
-                                icon: 'fas fa-sort'
+        search() {
+            this.collection.where = [];
+
+            const rules = this.$el.find('.query-builder').queryBuilder('getRules');
+
+            if (rules && rules.rules && rules.rules.length > 0) {
+                this.collection.where = [rules];
+            }
+
+            this.collection.fetch().then(() => Backbone.trigger('after:search', this.collection));
+
+            Backbone.Events.trigger('search', this);
+        },
+
+        resetFilters() {
+            this.$el.find('.query-builder').queryBuilder('setRules', []);
+            this.search();
+        },
+
+        addAttributeFilter() {
+            const scope = 'Attribute';
+            if (this.getAcl().check(scope, 'read')) {
+                const viewName = this.getMetadata().get(['clientDefs', scope, 'modalViews', 'select']) || 'views/modals/select-records';
+                this.notify('Loading...');
+                this.createView('dialog', viewName, {
+                    scope: scope,
+                    multiple: false,
+                    createButton: false,
+                    massRelateEnabled: false
+                }, dialog => {
+                    dialog.render();
+                    this.notify(false);
+                    dialog.once('select', attribute => {
+                        let rules = this.$el.find('.query-builder').queryBuilder('getRules');
+
+                        const fieldType = Espo.Utils.camelCaseToHyphen(attribute.get('type'));
+                        const view = this.getMetadata().get(['fields', fieldType, 'view'], `views/fields/${fieldType}`);
+
+                        const name = `attr_${attribute.get('id')}`;
+
+                        this.createView(name, view, {
+                            name: name,
+                            model: this.model
+                        }, view => {
+                            let filter = view.createQueryBuilderFilter();
+                            if (filter) {
+                                filter.label = attribute.get('name');
+
+                                let ids = this.filters.map(item => {
+                                    return item.id
+                                });
+
+                                if (!ids.includes(name)) {
+                                    this.filters.push(filter);
+                                    const $qb = this.$el.find('.query-builder');
+                                    $qb.queryBuilder('destroy');
+                                    $qb.addClass('query-builder');
+                                    this.initQueryBuilderFilter(rules);
+                                }
                             }
-                        }
+                        });
                     });
+                });
+            }
+        },
 
-                    this.$el.find('.query-builder').on('afterUpdateRuleOperator.queryBuilder', (e, rule) => {
-                        this.model.trigger('afterUpdateRuleOperator', rule);
-                    });
-                }
+        afterRender() {
+            this.prepareFilters(() => {
+                this.initQueryBuilderFilter();
             });
         },
 
