a096ad758b732314ac8b81eed1797e1dfb4cf192
yurdm <yd@atrocore.com>
2024-02-21 11:17:47 +0200

    Remove minimum_age field of ScheduledJob entity

---
+++ app/Atro/Migrations/V1Dot8Dot39.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Atro\Migrations;
+
+use Atro\Core\Migration\Base;
+
+class V1Dot8Dot39 extends Base
+{
+    public function up(): void
+    {
+        $this->execute("ALTER TABLE scheduled_job DROP minimum_age");
+    }
+
+    public function down(): void
+    {
+
+    }
+
+    protected function execute(string $sql)
+    {
+        try {
+            $this->getPDO()->exec($sql);
+        } catch (\Throwable $e) {
+            // ignore all
+        }
+    }
+}
