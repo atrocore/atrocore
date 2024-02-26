1bb2afc3cb9e5840f43de0177706b3d120fc4dc2
Roman Ratsun <rr@atrocore.com>
2024-02-26 15:51:09 +0000

    .

--- app/Atro/Listeners/Metadata.php
+++ app/Atro/Listeners/Metadata.php
@@ -16,6 +16,7 @@ namespace Atro\Listeners;
 use Atro\Core\EventManager\Event;
 use Atro\Core\KeyValueStorages\StorageInterface;
 use Atro\Core\Templates\Repositories\Relation;
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\ParameterType;
 use Espo\Core\Utils\Database\Orm\RelationManager;
 use Espo\Core\Utils\Util;
@@ -25,6 +26,10 @@ class Metadata extends AbstractListener
     public function modify(Event $event): void
     {
         $data = $event->getArgument('data');
+
+        $this->updateAssetRelationshipPanels($data);
+
+        $this->setAssetTypes($data);
 
         // add owner
         $data = $this->addOwner($data);
@@ -80,6 +85,74 @@ class Metadata extends AbstractListener
         }
 
         $event->setArgument('data', $data);
+    }
+
+    protected function updateAssetRelationshipPanels(array &$data): void
+    {
+        foreach ($data['entityDefs'] as $scope => $defs) {
+            if (empty($defs['links'])) {
+                continue 1;
+            }
+            foreach ($defs['links'] as $link => $linkData) {
+                if (!empty($linkData['entity']) && $linkData['entity'] == 'Asset' && !isset($data['clientDefs'][$scope]['relationshipPanels'][$link]['view'])) {
+                    $data['clientDefs'][$scope]['relationshipPanels'][$link]['view'] = "dam:views/record/panels/assets";
+                }
+            }
+        }
+    }
+
+    protected function setAssetTypes(array &$data): void
+    {
+        if (!$this->getConfig()->get('isInstalled', false)) {
+            return;
+        }
+
+        $typesData = $this->getAssetTypes();
+
+        $data['fields']['asset']['types'] = array_column($typesData, 'name');
+        $data['fields']['asset']['hasPreviewExtensions'][] = 'pdf';
+        $data['entityDefs']['Asset']['fields']['type']['options'] = $data['fields']['asset']['types'];
+        $data['entityDefs']['Asset']['fields']['type']['optionsIds'] = array_column($typesData, 'id');
+        $data['entityDefs']['Asset']['fields']['type']['assignAutomatically'] = [];
+        foreach ($typesData as $item) {
+            if (!empty($item['assignAutomatically'])) {
+                $data['entityDefs']['Asset']['fields']['type']['assignAutomatically'][] = $item['name'];
+            }
+            if (!empty($item['typesToExclude'])) {
+                $data['entityDefs']['Asset']['fields']['type']['typesToExclude'][$item['name']] = $item['typesToExclude'];
+            }
+        }
+    }
+
+    protected function getAssetTypes(): array
+    {
+        $assetTypes = $this->getContainer()->get('dataManager')->getCacheData('assetTypes');
+        if (!is_array($assetTypes)) {
+            /** @var Connection $connection */
+            $connection = $this->getContainer()->get('connection');
+            try {
+                $res = $connection->createQueryBuilder()
+                    ->select('at.id, at.name, at.assign_automatically, at.types_to_exclude')
+                    ->from($connection->quoteIdentifier('asset_type'), 'at')
+                    ->where('at.deleted = :false')
+                    ->setParameter('false', false, ParameterType::BOOLEAN)
+                    ->orderBy('at.sort_order', 'ASC')
+                    ->fetchAllAssociative();
+
+                $assetTypes = [];
+                foreach ($res as $k => $row) {
+                    $assetTypes[$k]['id'] = $row['id'];
+                    $assetTypes[$k]['name'] = $row['name'];
+                    $assetTypes[$k]['assignAutomatically'] = !empty($row['assign_automatically']);
+                    $assetTypes[$k]['typesToExclude'] = !empty($row['types_to_exclude']) ? @json_decode((string)$row['types_to_exclude'], true) : [];
+                }
+            } catch (\Throwable $e) {
+                $assetTypes = [];
+            }
+            $this->getContainer()->get('dataManager')->setCacheData('assetTypes', $assetTypes);
+        }
+
+        return $assetTypes;
     }
 
     protected function prepareExtensibleEnum(array &$data): void
