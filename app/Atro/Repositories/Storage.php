586891842af3f30a8abaad6f31f3557afec82681
Roman Ratsun <rr@atrocore.com>
2024-03-05 14:05:03 +0000

    .

---
+++ app/Atro/Repositories/Storage.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\Repositories;
+
+use Atro\Core\Templates\Repositories\Base;
+use Espo\ORM\Entity;
+
+class Storage extends Base
+{
+    protected function beforeSave(Entity $entity, array $options = [])
+    {
+        if ($entity->get('type') !== 'fileSystem') {
+            $entity->set('path', null);
+        }
+
+        parent::beforeSave($entity, $options);
+    }
+}
