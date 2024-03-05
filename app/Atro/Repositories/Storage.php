868b10e04ed21bfe49a084a648b2f1ebed904ebe
Roman Ratsun <rr@atrocore.com>
2024-03-05 14:41:36 +0000

    .

--- app/Atro/Repositories/Storage.php
+++ app/Atro/Repositories/Storage.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Atro\Repositories;
 
+use Atro\Core\Exceptions\BadRequest;
 use Atro\Core\Templates\Repositories\Base;
 use Espo\ORM\Entity;
 
@@ -24,6 +25,35 @@ class Storage extends Base
             $entity->set('path', null);
         }
 
+        if ($entity->isAttributeChanged('isDefault') && !$entity->get('isDefault')) {
+            if (empty($this->where(['isDefault' => true, 'id!=' => $entity->get('id')])->findOne())) {
+                throw new BadRequest('Please mark another Storage as the default.');
+            }
+        }
+
         parent::beforeSave($entity, $options);
+    }
+
+    protected function afterSave(Entity $entity, array $options = [])
+    {
+        parent::afterSave($entity, $options);
+
+        if ($entity->isAttributeChanged('isDefault') && $entity->get('isDefault')) {
+            foreach ($this->where(['isDefault' => true, 'id!=' => $entity->get('id')])->find() as $e) {
+                $e->set('isDefault', false);
+                $this->getEntityManager()->saveEntity($e);
+            }
+        }
+    }
+
+    protected function beforeRemove(Entity $entity, array $options = [])
+    {
+        if ($entity->get('isDefault')) {
+            throw new BadRequest('Default storage can not be deleted.');
+        }
+
+        //@todo check if files exists
+
+        parent::beforeRemove($entity, $options);
     }
 }
