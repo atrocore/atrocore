8da73e01a067178683e04281becef7e164e0a6c0
Roman Ratsun <rr@atrocore.com>
2024-03-18 13:09:26 +0000

    .

--- app/Atro/Core/FileValidation/Items/Mime.php
+++ app/Atro/Core/FileValidation/Items/Mime.php
@@ -16,31 +16,21 @@ namespace Atro\Core\FileValidation\Items;
 use Atro\Core\FileValidation\Base;
 use Atro\Core\Exceptions\BadRequest;
 
-/**
- * Class Mime
- */
 class Mime extends Base
 {
-    /**
-     * @return bool
-     */
     public function validate(): bool
     {
-        // get mime type
-        $mimeType = (string)mime_content_type($this->getFilePath());
+        $mimeType = (string)mime_content_type($this->file->getFilePath());
 
-        if (isset($this->params['list'])) {
-            return in_array($mimeType, $this->params['list']);
-        } elseif (isset($this->params['pattern'])) {
-            return stripos($mimeType, $this->params['pattern']) === false ? false : true;
+        if ($this->validationRule->get('validateBy') == 'List') {
+            return in_array($mimeType, $this->validationRule->get('mimeList'));
+        } elseif ($this->validationRule->get('validateBy') == 'Pattern') {
+            return stripos($mimeType, $this->validationRule->get('pattern')) === false ? false : true;
         }
 
         return true;
     }
 
-    /**
-     * @throws BadRequest
-     */
     public function onValidateFail()
     {
         throw new BadRequest($this->exception('mimeTypeValidationFailed'));
