faf25557abf6559560adde1e9ac75cbbf810855c
Roman Ratsun <rr@atrocore.com>
2024-04-23 13:26:20 +0000

    .

---
+++ app/Atro/Services/Storage.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\Services;
+
+use Atro\Core\Templates\Services\Base;
+use Espo\ORM\Entity;
+
+class Storage extends Base
+{
+    protected function getFieldsThatConflict(Entity $entity, \stdClass $data): array
+    {
+        return [];
+    }
+}
