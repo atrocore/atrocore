10e8c06d104062e0669cb969bb42be0f0f0f04d8
Roman Zablodskyi <rz@atrocore.com>
2024-04-30 10:19:29 +0300

    .

--- app/Atro/Migrations/V1Dot10Dot7.php
+++ app/Atro/Migrations/V1Dot10Dot7.php
@@ -16,25 +16,43 @@ class V1Dot10Dot7 extends Base
         $fromSchema = $this->getCurrentSchema();
         $toSchema = clone $fromSchema;
 
-        $table = $toSchema->createTable('action_set_job');
-        $table->addColumn('id', 'string', ['length' => 24]);
-        $table->addColumn('name', 'string', ['length' => 255, 'notnull' => false]);
-        $table->addColumn('deleted', 'boolean', ['default' => false, 'notnull' => false]);
-        $table->addColumn('state', 'string', ['default' => 'Pending', 'length' => 255, 'notnull' => false]);
-        $table->addColumn('state_message', 'text', ['notnull' => false]);
-        $table->addColumn('start', 'datetime', ['notnull' => false]);
-        $table->addColumn('end', 'datetime', ['notnull' => false]);
-        $table->addColumn('sort_order', 'integer', ['autoincrement' => true, 'notnull' => true]);
-        $table->addColumn('set_id', 'string', ['length' => 24, 'notnull' => false]);
-        $table->addColumn('action_id', 'string', ['length' => 24, 'notnull' => false]);
-        $table->addColumn('parent_id', 'string', ['length' => 24, 'notnull' => false]);
+        if (!$toSchema->hasTable('action_set_job')) {
+            $table = $toSchema->createTable('action_set_job');
 
-        $table->addUniqueIndex(['sort_order'], 'UNIQ_59D263D145AFA4EA');
-        $table->addIndex(['set_id', 'deleted'], 'IDX_ACTION_SET_JOB_SET_ID');
-        $table->addIndex(['action_id', 'deleted'], 'IDX_ACTION_SET_JOB_ACTION_ID');
-        $table->addIndex(['parent_id', 'deleted'], 'IDX_ACTION_SET_JOB_PARENT_ID');
+            $table->addColumn('id', 'string', ['length' => 24]);
+            $table->addColumn('name', 'string', ['length' => 255, 'notnull' => false]);
+            $table->addColumn('deleted', 'boolean', ['default' => false, 'notnull' => false]);
+            $table->addColumn('state', 'string', ['default' => 'Pending', 'length' => 255, 'notnull' => false]);
+            $table->addColumn('state_message', 'text', ['notnull' => false]);
+            $table->addColumn('start', 'datetime', ['notnull' => false]);
+            $table->addColumn('end', 'datetime', ['notnull' => false]);
+            $table->addColumn('sort_order', 'integer', ['autoincrement' => true, 'notnull' => true]);
+            $table->addColumn('set_id', 'string', ['length' => 24, 'notnull' => false]);
+            $table->addColumn('action_id', 'string', ['length' => 24, 'notnull' => false]);
+            $table->addColumn('parent_id', 'string', ['length' => 24, 'notnull' => false]);
+        } else {
+            $table = $toSchema->getTable('action_set_job');
+        }
 
-        $table->setPrimaryKey(['id']);
+        if (!$table->hasIndex('UNIQ_59D263D145AFA4EA')) {
+            $table->addUniqueIndex(['sort_order'], 'UNIQ_59D263D145AFA4EA');
+        }
+
+        if (!$table->hasIndex('IDX_ACTION_SET_JOB_SET_ID')) {
+            $table->addIndex(['set_id', 'deleted'], 'IDX_ACTION_SET_JOB_SET_ID');
+        }
+
+        if (!$table->hasIndex('IDX_ACTION_SET_JOB_ACTION_ID')) {
+            $table->addIndex(['action_id', 'deleted'], 'IDX_ACTION_SET_JOB_ACTION_ID');
+        }
+
+        if (!$table->hasIndex('IDX_ACTION_SET_JOB_PARENT_ID')) {
+            $table->addIndex(['parent_id', 'deleted'], 'IDX_ACTION_SET_JOB_PARENT_ID');
+        }
+
+        if (!$table->hasPrimaryKey()) {
+            $table->setPrimaryKey(['id']);
+        }
 
         foreach ($this->schemasDiffToSql($fromSchema, $toSchema) as $sql) {
             $this->exec($sql);
@@ -49,5 +67,10 @@ class V1Dot10Dot7 extends Base
             $this->getPDO()->exec($query);
         } catch (\Throwable $e) {
         }
+    }
+
+    public function down(): void
+    {
+        throw new \Error("Downgrade is prohibited.");
     }
 }
