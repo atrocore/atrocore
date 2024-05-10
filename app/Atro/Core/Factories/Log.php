1f749b9f87ff62bba8b0e7ec1650f9e15466f477
Roman Ratsun <roman.ratsun@atrocore.com>
2024-05-10 12:55:27 +0000

    .

---
+++ app/Atro/Core/Factories/Log.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\Core\Factories;
+
+use Atro\Core\Container;
+use Atro\Core\Factories\FactoryInterface as Factory;
+use Espo\Core\Utils\Config;
+use Espo\Core\Utils\Log\Monolog\Handler\RotatingFileHandler;
+use Espo\Core\Utils\Log\Monolog\Handler\StreamHandler;
+use Monolog\ErrorHandler;
+use Monolog\Logger;
+
+class Log implements Factory
+{
+    public function create(Container $container)
+    {
+        /** @var Config $config */
+        $config = $container->get('config');
+
+        switch ($config->get('logger.level', 'WARNING')) {
+            case 'INFO':
+                $levelCode = Logger::INFO;
+                break;
+            case 'NOTICE':
+                $levelCode = Logger::NOTICE;
+                break;
+            case 'ERROR':
+                $levelCode = Logger::ERROR;
+                break;
+            case 'CRITICAL':
+                $levelCode = Logger::CRITICAL;
+                break;
+            case 'ALERT':
+                $levelCode = Logger::ALERT;
+                break;
+            case 'EMERGENCY':
+                $levelCode = Logger::EMERGENCY;
+                break;
+            default:
+                $levelCode = Logger::DEBUG;
+        }
+
+        $log = new Logger('Log');
+
+        $path = $config->get('logger.path', 'data/logs/log.log');
+        if ($config->get('logger.rotation', true)) {
+            $handler = new RotatingFileHandler($path, $config->get('logger.maxFileNumber', 30), $levelCode);
+        } else {
+            $handler = new StreamHandler($path, $levelCode);
+        }
+
+        $log->pushHandler($handler);
+
+        $errorHandler = new ErrorHandler($log);
+        $errorHandler->registerExceptionHandler([], false);
+        $errorHandler->registerErrorHandler([], false);
+
+        return $log;
+    }
+}
