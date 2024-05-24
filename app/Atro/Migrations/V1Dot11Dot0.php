20fe033035297e0578c99cd310a3dd654ab84e5c
Roman Ratsun <roman.ratsun@atrocore.com>
2024-05-24 07:47:11 +0000

    .

--- app/Atro/Migrations/V1Dot11Dot0.php
+++ app/Atro/Migrations/V1Dot11Dot0.php
@@ -13,9 +13,12 @@ namespace Atro\Migrations;
 
 use Atro\Core\Exceptions\Error;
 use Atro\Core\Migration\Base;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\DBAL\ParameterType;
+use Espo\Core\Utils\Util;
 
-class V1Dot10Dot17 extends Base
+class V1Dot11Dot0 extends Base
 {
     public function getMigrationDateTime(): ?\DateTime
     {
@@ -26,6 +29,11 @@ class V1Dot10Dot17 extends Base
     {
         $this->exec("ALTER TABLE storage ADD folder_id VARCHAR(24) DEFAULT NULL");
         $this->exec("CREATE INDEX IDX_STORAGE_FOLDER_ID ON storage (folder_id, deleted)");
+
+        $this->exec("ALTER TABLE folder ADD hash VARCHAR(255) DEFAULT NULL");
+        $this->exec("CREATE UNIQUE INDEX UNIQ_ECA209CDD1B862B8EB3B4E33 ON folder (hash, deleted)");
+
+        self::updateFoldersHash($this->getConnection());
 
         if ($this->isPgSQL()) {
             $this->exec("ALTER TABLE storage ADD sync_folders BOOLEAN DEFAULT 'false' NOT NULL");
@@ -56,12 +64,48 @@ class V1Dot10Dot17 extends Base
 
         $this->exec("DROP TABLE folder_storage");
 
-        $this->updateComposer('atrocore/core', '^1.10.17');
+        $this->updateComposer('atrocore/core', '^1.11.0');
     }
 
     public function down(): void
     {
         throw new Error('Downgrade is prohibited.');
+    }
+
+    public static function updateFoldersHash(Connection $conn): void
+    {
+        $records = $conn->createQueryBuilder()
+            ->select('f.*, h.parent_id')
+            ->from('folder', 'f')
+            ->leftJoin('f', 'folder_hierarchy', 'h', 'f.id=h.entity_id')
+            ->where('f.deleted=:false')
+            ->andWhere('f.deleted=:false')
+            ->setParameter('false', false, ParameterType::BOOLEAN)
+            ->fetchAllAssociative();
+
+        foreach ($records as $record) {
+            try {
+                self::updateFolderHash((string)$record['id'], (string)$record['name'], (string)$record['parent_id'], $conn);
+            } catch (UniqueConstraintViolationException $e) {
+                $record['name'] .= ' ' . Util::generateId();
+                self::updateFolderHash((string)$record['id'], (string)$record['name'], (string)$record['parent_id'], $conn);
+            } catch (\Throwable $e) {
+            }
+        }
+    }
+
+    public static function updateFolderHash(string $id, string $name, string $parentId, Connection $conn): void
+    {
+        $hash = md5("{$name}_{$parentId}");
+        $conn->createQueryBuilder()
+            ->update('folder')
+            ->set('hash', ':hash')
+            ->set('name', ':name')
+            ->where('id=:id')
+            ->setParameter('id', $id)
+            ->setParameter('name', $name)
+            ->setParameter('hash', $hash)
+            ->executeQuery();
     }
 
     protected function exec(string $sql): void
--- app/Atro/Repositories/Folder.php
+++ app/Atro/Repositories/Folder.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Atro\Repositories;
 
+use Doctrine\DBAL\ParameterType;
 use Espo\Core\Templates\Repositories\Hierarchy;
 use Espo\ORM\Entity;
 
@@ -24,6 +25,30 @@ class Folder extends Hierarchy
             $entity->set('code', null);
         }
 
+        if ($entity->isAttributeChanged('name')) {
+            $entity->set('hash', $this->prepareFolderHash($entity->get('id'), $entity->get('name')));
+        }
+
         parent::beforeSave($entity, $options);
+    }
+
+    public function prepareFolderHash(string $folderId, string $folderName = null): string
+    {
+        $record = $this->getConnection()->createQueryBuilder()
+            ->select('f.*, h.parent_id')
+            ->from('folder', 'f')
+            ->leftJoin('f', 'folder_hierarchy', 'h', 'f.id=h.entity_id')
+            ->where('f.deleted=:false')
+            ->andWhere('f.deleted=:false')
+            ->andWhere('f.id=:id')
+            ->setParameter('false', false, ParameterType::BOOLEAN)
+            ->setParameter('id', $folderId)
+            ->fetchAssociative();
+
+        if (!$folderName) {
+            $folderName = $record['name'];
+        }
+
+        return md5("{$folderName}_{$record['parent_id']}");
     }
 }
