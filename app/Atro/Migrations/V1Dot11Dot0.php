3caba485cb29d3084e928feb18f9d8bd59a64486
Roman Ratsun <roman.ratsun@atrocore.com>
2024-05-24 09:45:29 +0000

    .

--- app/Atro/Migrations/V1Dot11Dot0.php
+++ app/Atro/Migrations/V1Dot11Dot0.php
@@ -32,7 +32,9 @@ class V1Dot11Dot0 extends Base
 
         if ($this->isPgSQL()) {
             $this->exec("ALTER TABLE storage ADD sync_folders BOOLEAN DEFAULT 'false' NOT NULL");
-            $this->exec("CREATE TABLE file_folder_linker (id VARCHAR(24) NOT NULL, name VARCHAR(255) NOT NULL, deleted BOOLEAN DEFAULT 'false', parent_id VARCHAR(255) NOT NULL, folder_id VARCHAR(255) DEFAULT NULL, file_id VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))");
+            $this->exec(
+                "CREATE TABLE file_folder_linker (id VARCHAR(24) NOT NULL, name VARCHAR(255) NOT NULL, deleted BOOLEAN DEFAULT 'false', parent_id VARCHAR(255) NOT NULL, folder_id VARCHAR(255) DEFAULT NULL, file_id VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))"
+            );
             $this->exec("CREATE UNIQUE INDEX IDX_FILE_FOLDER_LINKER_UNIQUE_ITEM ON file_folder_linker (deleted, parent_id, name)");
             $this->exec("CREATE INDEX IDX_FILE_FOLDER_LINKER_FOLDER_IDX ON file_folder_linker (parent_id, folder_id)");
             $this->exec("CREATE INDEX IDX_FILE_FOLDER_LINKER_FILE_IDX ON file_folder_linker (parent_id, file_id)");
@@ -40,12 +42,15 @@ class V1Dot11Dot0 extends Base
             $this->exec("CREATE UNIQUE INDEX UNIQ_8B5ADE4E93CB796CEB3B4E33 ON file_folder_linker (file_id, deleted)");
         } else {
             $this->exec("ALTER TABLE storage ADD sync_folders TINYINT(1) DEFAULT '0' NOT NULL");
-            $this->exec("CREATE TABLE file_folder_linker (id VARCHAR(24) NOT NULL, name VARCHAR(255) NOT NULL, deleted TINYINT(1) DEFAULT '0', parent_id VARCHAR(255) NOT NULL, folder_id VARCHAR(255) DEFAULT NULL, file_id VARCHAR(255) DEFAULT NULL, UNIQUE INDEX IDX_FILE_FOLDER_LINKER_UNIQUE_ITEM (deleted, parent_id, name), INDEX IDX_FILE_FOLDER_LINKER_FOLDER_IDX (parent_id, folder_id), INDEX IDX_FILE_FOLDER_LINKER_FILE_IDX (parent_id, file_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB");
+            $this->exec(
+                "CREATE TABLE file_folder_linker (id VARCHAR(24) NOT NULL, name VARCHAR(255) NOT NULL, deleted TINYINT(1) DEFAULT '0', parent_id VARCHAR(255) NOT NULL, folder_id VARCHAR(255) DEFAULT NULL, file_id VARCHAR(255) DEFAULT NULL, UNIQUE INDEX IDX_FILE_FOLDER_LINKER_UNIQUE_ITEM (deleted, parent_id, name), INDEX IDX_FILE_FOLDER_LINKER_FOLDER_IDX (parent_id, folder_id), INDEX IDX_FILE_FOLDER_LINKER_FILE_IDX (parent_id, file_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB"
+            );
             $this->exec("CREATE UNIQUE INDEX UNIQ_8B5ADE4E162CB942EB3B4E33 ON file_folder_linker (folder_id, deleted)");
             $this->exec("CREATE UNIQUE INDEX UNIQ_8B5ADE4E93CB796CEB3B4E33 ON file_folder_linker (file_id, deleted)");
         }
 
-//        self::updateFoldersHash($this->getConnection());
+        $this->createFoldersItems();
+        $this->createFilesItems();
 
         try {
             $records = $this->getConnection()->createQueryBuilder()
@@ -78,41 +83,105 @@ class V1Dot11Dot0 extends Base
         throw new Error('Downgrade is prohibited.');
     }
 
-//    public static function updateFoldersHash(Connection $conn): void
-//    {
-//        $records = $conn->createQueryBuilder()
-//            ->select('f.*, h.parent_id')
-//            ->from('folder', 'f')
-//            ->leftJoin('f', 'folder_hierarchy', 'h', 'f.id=h.entity_id')
-//            ->where('f.deleted=:false')
-//            ->andWhere('f.deleted=:false')
-//            ->setParameter('false', false, ParameterType::BOOLEAN)
-//            ->fetchAllAssociative();
-//
-//        foreach ($records as $record) {
-//            try {
-//                self::updateFolderHash((string)$record['id'], (string)$record['name'], (string)$record['parent_id'], $conn);
-//            } catch (UniqueConstraintViolationException $e) {
-//                $record['name'] .= ' ' . Util::generateId();
-//                self::updateFolderHash((string)$record['id'], (string)$record['name'], (string)$record['parent_id'], $conn);
-//            } catch (\Throwable $e) {
-//            }
-//        }
-//    }
-//
-//    public static function updateFolderHash(string $id, string $name, string $parentId, Connection $conn): void
-//    {
-//        $hash = md5("{$name}_{$parentId}");
-//        $conn->createQueryBuilder()
-//            ->update('folder')
-//            ->set('hash', ':hash')
-//            ->set('name', ':name')
-//            ->where('id=:id')
-//            ->setParameter('id', $id)
-//            ->setParameter('name', $name)
-//            ->setParameter('hash', $hash)
-//            ->executeQuery();
-//    }
+    public function createFoldersItems(): void
+    {
+        $records = $this->getConnection()->createQueryBuilder()
+            ->select('f.*, h.parent_id')
+            ->from('folder', 'f')
+            ->leftJoin('f', 'folder_hierarchy', 'h', 'f.id=h.entity_id')
+            ->where('f.deleted=:false')
+            ->andWhere('f.deleted=:false')
+            ->setParameter('false', false, ParameterType::BOOLEAN)
+            ->fetchAllAssociative();
+
+        foreach ($records as $record) {
+            try {
+                $this->getConnection()->createQueryBuilder()
+                    ->insert('file_folder_linker')
+                    ->setValue('id', ':id')
+                    ->setValue('name', ':name')
+                    ->setValue('parent_id', ':parentId')
+                    ->setValue('folder_id', ':folderId')
+                    ->setParameter('id', Util::generateId())
+                    ->setParameter('name', (string)$record['name'])
+                    ->setParameter('parentId', (string)$record['parent_id'])
+                    ->setParameter('folderId', (string)$record['id'])
+                    ->executeQuery();
+            } catch (UniqueConstraintViolationException $e) {
+                $newName = $record['name'] . ' (' . Util::generateId() . ')';
+
+                $this->getConnection()->createQueryBuilder()
+                    ->update('folder')
+                    ->set('name', ':name')
+                    ->where('id=:id')
+                    ->setParameter('id', $record['id'])
+                    ->setParameter('name', $newName)
+                    ->executeQuery();
+
+                $this->getConnection()->createQueryBuilder()
+                    ->insert('file_folder_linker')
+                    ->setValue('id', ':id')
+                    ->setValue('name', ':name')
+                    ->setValue('parent_id', ':parentId')
+                    ->setValue('folder_id', ':folderId')
+                    ->setParameter('id', Util::generateId())
+                    ->setParameter('name', $newName)
+                    ->setParameter('parentId', (string)$record['parent_id'])
+                    ->setParameter('folderId', (string)$record['id'])
+                    ->executeQuery();
+            }
+        }
+    }
+
+    public function createFilesItems(): void
+    {
+        $records = $this->getConnection()->createQueryBuilder()
+            ->select('*')
+            ->from('file')
+            ->where('deleted=:false')
+            ->setParameter('false', false, ParameterType::BOOLEAN)
+            ->fetchAllAssociative();
+
+        foreach ($records as $record) {
+            try {
+                $this->getConnection()->createQueryBuilder()
+                    ->insert('file_folder_linker')
+                    ->setValue('id', ':id')
+                    ->setValue('name', ':name')
+                    ->setValue('parent_id', ':parentId')
+                    ->setValue('file_id', ':fileId')
+                    ->setParameter('id', Util::generateId())
+                    ->setParameter('name', (string)$record['name'])
+                    ->setParameter('parentId', (string)$record['folder_id'])
+                    ->setParameter('fileId', (string)$record['id'])
+                    ->executeQuery();
+            } catch (UniqueConstraintViolationException $e) {
+                $parts = explode('.', $record['name']);
+                $ext = array_pop($parts);
+                $newName = implode('.', $parts) . ' (' . Util::generateId() . ').' . $ext;
+
+                $this->getConnection()->createQueryBuilder()
+                    ->update('file')
+                    ->set('name', ':name')
+                    ->where('id=:id')
+                    ->setParameter('id', $record['id'])
+                    ->setParameter('name', $newName)
+                    ->executeQuery();
+
+                $this->getConnection()->createQueryBuilder()
+                    ->insert('file_folder_linker')
+                    ->setValue('id', ':id')
+                    ->setValue('name', ':name')
+                    ->setValue('parent_id', ':parentId')
+                    ->setValue('file_id', ':fileId')
+                    ->setParameter('id', Util::generateId())
+                    ->setParameter('name', $newName)
+                    ->setParameter('parentId', (string)$record['folder_id'])
+                    ->setParameter('fileId', (string)$record['id'])
+                    ->executeQuery();
+            }
+        }
+    }
 
     protected function exec(string $sql): void
     {
