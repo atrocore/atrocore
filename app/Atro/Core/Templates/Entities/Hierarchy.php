c49a634653ec1225a54fcfca58776ddeb4f8377e
Roman Ratsun <roman.ratsun@atrocore.com>
2024-05-29 06:53:24 +0000

    .

--- app/Atro/Core/Templates/Entities/Hierarchy.php
+++ app/Atro/Core/Templates/Entities/Hierarchy.php
@@ -17,4 +17,27 @@ use Espo\Core\ORM\Entity;
 
 class Hierarchy extends Entity
 {
+    public function getParentId(): ?string
+    {
+        $parentsIds = $this->get('parentsIds');
+        if (is_array($parentsIds)) {
+            return $parentsIds[0] ?? null;
+        }
+
+        $parents = $this->get('parents');
+
+        return !empty($parents[0]) ? $parents[0]->get('id') : null;
+    }
+
+    public function getParent(): ?Hierarchy
+    {
+        $parentsIds = $this->get('parentsIds');
+        if (is_array($parentsIds)) {
+            return !empty($parentsIds[0]) ? $this->getEntityManager()->getRepository($this->entityType)->get($parentsIds[0]) : null;
+        }
+
+        $parents = $this->get('parents');
+
+        return !empty($parents[0]) ? $parents[0] : null;
+    }
 }
