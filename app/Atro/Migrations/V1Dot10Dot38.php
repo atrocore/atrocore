252d0117c5ee3463af7a23da4fcc91b68c70449a
Roman Ratsun <roman.ratsun@atrocore.com>
2024-07-03 08:53:57 +0000

    .

--- app/Atro/Migrations/V1Dot10Dot38.php
+++ app/Atro/Migrations/V1Dot10Dot38.php
@@ -11,9 +11,7 @@
 
 namespace Atro\Migrations;
 
-use Atro\Core\Exceptions\Error;
 use Atro\Core\Migration\Base;
-use Doctrine\DBAL\ParameterType;
 
 class V1Dot10Dot38 extends Base
 {
@@ -26,14 +24,15 @@ class V1Dot10Dot38 extends Base
     {
         if($this->isPgSQL()){
             $this->exec("ALTER TABLE html_template RENAME TO preview_template;");
-            $this->exec("CREATE TABLE preview_template (id VARCHAR(24) NOT NULL, name VARCHAR(255) DEFAULT NULL, deleted BOOLEAN DEFAULT 'false', template TEXT DEFAULT NULL, entity_type VARCHAR(255) DEFAULT 'Product', data TEXT DEFAULT NULL, is_active BOOLEAN DEFAULT 'true' NOT NULL, PRIMARY KEY(id));
-COMMENT ON COLUMN preview_template.data IS '(DC2Type:jsonObject)';");
+            $this->exec("CREATE TABLE preview_template (id VARCHAR(24) NOT NULL, name VARCHAR(255) DEFAULT NULL, deleted BOOLEAN DEFAULT 'false', template TEXT DEFAULT NULL, entity_type VARCHAR(255) DEFAULT 'Product', data TEXT DEFAULT NULL, is_active BOOLEAN DEFAULT 'true' NOT NULL, PRIMARY KEY(id))");
+            $this->exec("COMMENT ON COLUMN preview_template.data IS '(DC2Type:jsonObject)';");
         }else{
             $this->exec("RENAME table html_template TO preview_template;");
-            $this->exec("CREATE TABLE preview_template (id VARCHAR(24) NOT NULL, name VARCHAR(255) DEFAULT NULL, delete
-d TINYINT(1) DEFAULT '0', template LONGTEXT DEFAULT NULL, entity_type VARCHAR(255) DEFAULT 'Pr
-oduct', data LONGTEXT DEFAULT NULL COMMENT '(DC2Type:jsonObject)', is_active TINYINT(1) DEFAULT '1' NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB;");
+            $this->exec("CREATE TABLE preview_template (id VARCHAR(24) NOT NULL, name VARCHAR(255) DEFAULT NULL, deleted TINYINT(1) DEFAULT '0', template LONGTEXT DEFAULT NULL, entity_type VARCHAR(255) DEFAULT 'Product', data LONGTEXT DEFAULT NULL COMMENT '(DC2Type:jsonObject)', is_active TINYINT(1) DEFAULT '1' NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB");
         }
+
+        self::removeDir('custom');
+        self::removeDir('dump/custom');
     }
 
     public function down(): void
@@ -45,6 +44,36 @@ oduct', data LONGTEXT DEFAULT NULL COMMENT '(DC2Type:jsonObject)', is_active TIN
         try {
             $this->getPDO()->exec($query);
         } catch (\Throwable $e) {
+        }
+    }
+
+    protected static function scanDir(string $dir): array
+    {
+        // prepare result
+        $result = [];
+
+        if (file_exists($dir) && is_dir($dir)) {
+            foreach (scandir($dir) as $item) {
+                if (!in_array($item, ['.', '..'])) {
+                    $result[] = $item;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    protected static function removeDir(string $dir): void
+    {
+        if (file_exists($dir) && is_dir($dir)) {
+            foreach (self::scanDir($dir) as $object) {
+                if (is_dir($dir . "/" . $object)) {
+                    self::removeDir($dir . "/" . $object);
+                } else {
+                    unlink($dir . "/" . $object);
+                }
+            }
+            rmdir($dir);
         }
     }
 }
