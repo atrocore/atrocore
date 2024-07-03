b816206d7966226d1187f989f1b31fc49b0ad918
Roman Ratsun <roman.ratsun@atrocore.com>
2024-07-03 08:55:31 +0000

    .

--- app/Atro/Migrations/V1Dot10Dot38.php
+++ app/Atro/Migrations/V1Dot10Dot38.php
@@ -17,7 +17,7 @@ class V1Dot10Dot38 extends Base
 {
     public function getMigrationDateTime(): ?\DateTime
     {
-        return new \DateTime('2024-07-02 17:00:00');
+        return new \DateTime('2024-07-03 12:00:00');
     }
 
     public function up(): void
