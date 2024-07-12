ca68347054f0772bb7999c7ae60ded2fe0e95743
Roman Ratsun <roman.ratsun@atrocore.com>
2024-07-12 06:45:05 +0000

    .

---
+++ svelte/src/fields/Text.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let name = 'world';
+
+    function changeName() {
+        console.log('13333')
+        name = name + '_';
+    }
+</script>
+
+<button class="ss" on:click={changeName}>Hello {name}!</button>
+
+<style>
+    .ss {
+        color: purple;
+    }
+</style>
--- svelte/src/main.js
+++ svelte/src/main.js
@@ -1,4 +1,5 @@
 import MyComponent from './MyComponent.svelte';
 import Counter from './Counter.svelte';
+import TextField from './fields/Text.svelte';
 
-export {MyComponent, Counter};
+export {MyComponent, Counter, TextField};
