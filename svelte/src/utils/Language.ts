58b33f209b4452aac1656941174c9e7534fd86d1
Roman Ratsun <roman.ratsun@atrocore.com>
2024-07-16 11:44:45 +0000

    .

---
+++ svelte/src/utils/Language.ts
@@ -0,0 +1,79 @@
+import {writable} from 'svelte/store';
+
+const data = writable({});
+
+export const Language = {
+
+    setTranslations(newTranslations: any): void {
+        data.set(newTranslations);
+    },
+
+    has(name: string, category: string, scope: string): boolean {
+        let res = false;
+
+        data.subscribe((current) => {
+            if (scope in current) {
+                if (category in current[scope]) {
+                    if (name in current[scope][category]) {
+                        res = true;
+                        return;
+                    }
+                }
+            }
+        })();
+
+        return res;
+    },
+
+    get(scope: string, category: string, name: string): string | null {
+        let translatedText: string = name;
+
+        data.subscribe((current) => {
+            if (scope in current) {
+                if (category in current[scope]) {
+                    if (name in current[scope][category]) {
+                        translatedText = current[scope][category][name];
+                        return;
+                    }
+                }
+            }
+
+            if (scope in current) {
+                if (category in current[scope]) {
+                    if (name in current[scope][category]) {
+                        translatedText = current[scope][category][name];
+                        return;
+                    }
+                }
+            }
+            if (scope == 'Global') {
+                translatedText = name;
+                return;
+            }
+
+            translatedText = null;
+        })();
+
+        return translatedText;
+    },
+
+    translate(name: string, category: null | string = null, scope: null | string = null): string | object {
+        scope = scope || 'Global';
+        category = category || 'labels';
+        var res = Language.get(scope, category, name);
+        if (res === null && scope != 'Global') {
+            res = Language.get('Global', category, name);
+        }
+
+        return res;
+    },
+
+    translateOption(value: string, field: string, scope: null | string = null): string {
+        let translation = Language.translate(field, 'options', scope);
+        if (typeof translation != 'object') {
+            translation = {};
+        }
+
+        return translation[value] || value;
+    },
+};
