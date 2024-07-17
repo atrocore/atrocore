9a02d2a47dcdf7af39a61189710f4307a4ba6e70
dzoukou <dzoukouraoul@gmail.com>
2024-07-17 08:09:22 +0100

    .

--- app/Atro/Migrations/V1Dot10Dot41.php
+++ app/Atro/Migrations/V1Dot10Dot41.php
@@ -26,45 +26,33 @@ class V1Dot10Dot41 extends Base
     public function up(): void
     {
         $this->exec('DROP TABLE if exists email_template;');
-        $fromSchema = $this->getCurrentSchema();
-        $toSchema = clone $fromSchema;
 
+        if ($this->isPgSQL()) {
+            $this->exec("CREATE TABLE email_template (id VARCHAR(24) NOT NULL, name VARCHAR(255) DEFAULT NULL, deleted BOOLEAN DEFAULT 'false', code VARCHAR(255) DEFAULT NULL, description TEXT DEFAULT NULL, email_to TEXT DEFAULT NULL, email_cc TEXT DEFAULT NULL, subject TEXT DEFAULT NULL, body TEXT DEFAULT NULL, allow_attachments BOOLEAN DEFAULT 'false' NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, modified_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, connection_id VARCHAR(24) DEFAULT NULL, created_by_id VARCHAR(24) DEFAULT NULL, modified_by_id VARCHAR(24) DEFAULT NULL, PRIMARY KEY(id));");
+            $this->exec("CREATE UNIQUE INDEX UNIQ_9C0600CA77153098EB3B4E33 ON email_template (code, deleted);");
+            $this->exec("CREATE INDEX IDX_EMAIL_TEMPLATE_CONNECTION_ID ON email_template (connection_id, deleted);");
+            $this->exec("CREATE INDEX IDX_EMAIL_TEMPLATE_CREATED_BY_ID ON email_template (created_by_id, deleted);");
+            $this->exec("CREATE INDEX IDX_EMAIL_TEMPLATE_MODIFIED_BY_ID ON email_template (modified_by_id, deleted);");
+            $this->exec("CREATE INDEX IDX_EMAIL_TEMPLATE_NAME ON email_template (name, deleted);");
+            $this->exec("COMMENT ON COLUMN email_template.email_to IS '(DC2Type:jsonArray)';");
+            $this->exec("COMMENT ON COLUMN email_template.email_cc IS '(DC2Type:jsonArray)';");
+        } else {
+            $this->exec("CREATE TABLE email_template (id VARCHAR(24) NOT NULL, name VARCHAR(255) DEFAULT NULL, deleted TINYINT(1) DEFAULT '0', code VARCHAR(255) DEFAULT NULL, description LONGTEXT DEFAULT NULL, email_to LONGTEXT DEFAULT NULL COMMENT '(DC2Type:jsonArray)', email_cc LONGTEXT DEFAULT NULL COMMENT '(DC2Type:jsonArray)', subject LONGTEXT DEFAULT NULL, body LONGTEXT DEFAULT NULL, allow_attachments TINYINT(1) DEFAULT '0' NOT NULL, created_at DATETIME DEFAULT NULL, modified_at DATETIME DEFAULT NULL, connection_id VARCHAR(24) DEFAULT NULL, created_by_id VARCHAR(24) DEFAULT NULL, modified_by_id VARCHAR(24) DEFAULT NULL, UNIQUE INDEX UNIQ_9C0600CA77153098EB3B4E33 (code, deleted), INDEX IDX_EMAIL_TEMPLATE_CONNECTION_ID (connection_id, deleted), INDEX IDX_EMAIL_TEMPLATE_CREATED_BY_ID (created_by_id, deleted), INDEX IDX_EMAIL_TEMPLATE_MODIFIED_BY_ID (modified_by_id, deleted), INDEX IDX_EMAIL_TEMPLATE_NAME (name, deleted), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB;");
+        }
 
-        $table = $toSchema->createTable('email_template');
-        $table->addColumn('id', 'string', ['length' => 24]);
-        $table->addColumn('name', 'string', ['notnull' => false]);
-        $table->addColumn('code', 'string', ['notnull' => false]);
-        $table->addColumn('email_cc', 'jsonArray', ['notnull' => false]);
-        $table->addColumn('email_to', 'jsonArray', ['notnull' => false]);
-        $table->addColumn('description', 'text', ['notnull' => false]);
-        $table->addColumn('subject', 'text', ['notnull' => false]);
-        $table->addColumn('body', 'text', ['notnull' => false]);
         foreach ($this->getConfig()->get('locales', []) as $locale) {
             if ($locale['language'] === 'en_US') {
                 continue;
             }
             $locale = strtolower($locale['language']);
+            if ($this->isPgSQL()) {
+                $this->exec("ALTER TABLE email_template ADD subject_$locale TEXT DEFAULT NULL");
+                $this->exec("ALTER TABLE email_template ADD body_$locale TEXT DEFAULT NULL");
+            } else {
+                $this->exec("ALTER TABLE email_template ADD subject_$locale LONGTEXT DEFAULT NULL");
+                $this->exec("ALTER TABLE email_template ADD body_$locale LONGTEXT DEFAULT NULL");
+            }
 
-            $table->addColumn("subject_$locale", 'text', ['notnull' => false]);
-            $table->addColumn("body_$locale", 'text', ['notnull' => false]);
-        }
-        $table->addColumn('created_at', 'datetime', ['notnull' => false]);
-        $table->addColumn('modified_at', 'datetime', ['notnull' => false]);
-        $table->addColumn('modified_by_id', 'string', ['notnull' => false, 'length' => 24]);
-        $table->addColumn('created_by_id', 'string', ['notnull' => false, 'length' => 24]);
-        $table->addColumn('allow_attachments', 'boolean', ['default' => false]);
-        $table->addColumn('connection_id', 'string', ['length' => 24, 'notnull' => false]);
-        $table->addColumn('deleted', 'boolean', ['default' => false, 'notnull' => false]);
-        $table->setPrimaryKey(['id']);
-        $table->addUniqueIndex(['code', 'deleted'], 'UNIQ_9C0600CA77153098EB3B4E33');
-        $table->addIndex(['connection_id', 'deleted'], 'IDX_EMAIL_TEMPLATE_CONNECTION_ID');
-        $table->addIndex(['created_by_id', 'deleted'], 'IDX_EMAIL_TEMPLATE_CREATED_BY_ID');
-        $table->addIndex(['modified_by_id', 'deleted'], 'IDX_EMAIL_TEMPLATE_MODIFIED_BY_ID');
-        $table->addIndex(['name', 'deleted'], 'IDX_EMAIL_TEMPLATE_NAME');
-
-
-        foreach ($this->schemasDiffToSql($fromSchema, $toSchema) as $sql) {
-            $this->exec($sql);
         }
 
         self::createNotificationEmailTemplates($this->getConnection(), $this->getConfig());
