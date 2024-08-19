1cc9e03afc9c0e5ef723163cb7325bf7144dfb17
yurdm <yd@atrocore.com>
2024-08-20 00:56:34 +0300

    Use PIM files in the editor

--- client/src/views/fields/markdown.js
+++ client/src/views/fields/markdown.js
@@ -21,6 +21,10 @@ Espo.define('views/fields/markdown', ['views/fields/text', 'lib!EasyMDE'], funct
 
         maxHeight: 400,
 
+        getMaxUploadSize: function () {
+            return (this.getConfig().get('chunkFileSize') || 2) * 1024 * 1024;
+        },
+
         setup() {
             Dep.prototype.setup.call(this);
 
@@ -49,14 +53,128 @@ Espo.define('views/fields/markdown', ['views/fields/text', 'lib!EasyMDE'], funct
                         drawImage: null,
                         toggleSideBySide: null
                     },
+                    previewRender: (plainText) => marked(plainText),
                     previewClass: ['editor-preview', 'complex-text'],
-                    toolbar: ['undo', 'redo', '|', 'heading-1', 'heading-2', 'heading-3', '|', 'bold', 'italic', 'strikethrough', '|', 'unordered-list', 'ordered-list', 'code', 'quote', 'horizontal-rule', '|', 'link', 'image', '|', 'preview', 'guide']
+                    toolbar: [
+                        'undo', 'redo', '|',
+                        'heading-1', 'heading-2', 'heading-3', '|',
+                        'bold', 'italic', 'strikethrough', '|',
+                        'unordered-list', 'ordered-list', 'code', 'quote', 'link', 'horizontal-rule', '|',
+                        {
+                            name: "selectImage",
+                            action: (editor) => {
+                                this.notify('Loading...');
+                                this.createView('selectFileDialog', this.getMetadata().get('clientDefs.File.modalViews.select'), {
+                                    scope: 'File',
+                                    filters: {
+                                        fileType: {
+                                            type: 'in',
+                                            field: 'typeId',
+                                            attribute: 'typeId',
+                                            value: ['a_image', 'a_favicon']
+                                        }
+                                    },
+                                }, view => {
+                                    view.render();
+                                    this.notify(false);
+                                    this.listenTo(view, 'select', model => {
+                                        const file = new File([], model.get('name'));
+                                        file.url = model.get('downloadUrl');
+                                        this.editor.uploadImageUsingCustomFunction(this.uploadImage.bind(this), file)
+                                    });
+                                });
+                            },
+                            className: "fa fa-file-image-o",
+                            title: "Select"
+                        },
+                        {
+                            name: "uploadImage",
+                            action: (editor) => {
+                                this.notify('Loading...')
+                                this.createView('upload', 'views/file/modals/upload', {
+                                    scope: 'File',
+                                    fullFormDisabled: true,
+                                    layoutName: 'upload',
+                                    multiUpload: false,
+                                    attributes: _.extend(this.model.attributes, {share: true}),
+                                }, view => {
+                                    view.render();
+                                    this.notify(false);
+                                    this.listenTo(view.model, 'after:file-upload', entity => {
+                                        const file = new File([], entity.name);
+                                        file.url = entity.sharedUrl;
+                                        this.editor.uploadImageUsingCustomFunction(this.uploadImage.bind(this), file)
+                                    });
+                                    this.listenToOnce(view, 'close', () => {
+                                        this.clearView('upload');
+                                    });
+                                });
+                            },
+                            className: "fa fa-download",
+                            title: "Upload from a local filesystem"
+                        }, '|',
+                        'preview', 'guide'
+                    ],
+                    uploadImage: true,
+                    imageUploadFunction: (file, onSuccess, onError) => {
+                        if (file.size >= this.getMaxUploadSize()) {
+                            Espo.ui.notify(`Your file exceeded size limit of ${this.getMaxUploadSize()} MB`);
+                            return;
+                        }
+
+                        const extensions = this.getMetadata().get(['app', 'file', 'image', 'extensions']);
+                        if (!file) {
+                            return;
+                        }
+
+                        let isImage = false;
+                        for (const ext of extensions) {
+                            if (file.name.endsWith(ext)) {
+                                isImage = true;
+                                break;
+                            }
+                        }
+
+                        if (!isImage) {
+                            Espo.ui.error('Your file is not an image.');
+                            return;
+                        }
+
+                        const reader = new FileReader();
+                        reader.onload = e => {
+                            $.ajax({
+                                type: 'POST',
+                                url: 'File?silent=true',
+                                contentType: "application/json",
+                                data: JSON.stringify({
+                                    name: file.name,
+                                    fileSize: file.size,
+                                    fileContents: e.target.result,
+                                    share: true
+                                }),
+                            }).done(response => {
+                                file.url = response.sharedUrl;
+                                this.uploadImage(file, onSuccess, onError);
+                            }).error(response => {
+                                Espo.ui.error('Error while uploading file');
+                            })
+                        };
+                        reader.readAsDataURL(file);
+                    }
                 });
 
                 const scroller = this.editor.codemirror.getScrollerElement();
                 if (scroller) {
                     scroller.style.maxHeight = `${this.maxHeight}px`;
                 }
+            }
+        },
+
+        uploadImage(file, onSuccess, onError) {
+            if (file.url) {
+                onSuccess(file.url);
+            } else {
+                Espo.ui.error('Cannot upload your image')
             }
         }
     });
