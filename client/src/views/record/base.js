eff8c058f61934284e0ecbe485e28cf6a3a34a2f
Kevin Teguia <kt@atrocore.com>
2024-09-13 19:30:31 +0100

    .

--- client/src/views/record/base.js
+++ client/src/views/record/base.js
@@ -352,7 +352,6 @@ Espo.define('views/record/base', ['view', 'view-record-helper', 'ui-handler', 'l
             }
 
             this.setupBeforeFinal();
-            this.setupTour();
         },
 
         setupBeforeFinal: function () {
@@ -814,8 +813,7 @@ Espo.define('views/record/base', ['view', 'view-record-helper', 'ui-handler', 'l
 
         exit: function (after) {
         }
+
     });
-
-
 
 });
