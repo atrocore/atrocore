74317229d9d8fe57a1d06f572202d2d7d5b958f8
dzoukou <dzoukouraoul@gmail.com>
2024-09-18 08:35:55 +0100

    .

---
+++ app/Atro/Repositories/LayoutProfile.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\Repositories;
+
+use Atro\Core\Templates\Repositories\Base;
+use Espo\ORM\Entity;
+
+class LayoutProfile extends Base
+{
+    /**
+     * @inheritDoc
+     */
+    public function beforeSave(Entity $entity, array $options = array())
+    {
+        if ($entity->isAttributeChanged('isDefault') && !empty($entity->get('isDefault'))) {
+
+        }
+
+
+        parent::beforeSave($entity, $options);
+    }
+
+    protected function init()
+    {
+        parent::init();
+
+        $this->addDependency('language');
+    }
+}
