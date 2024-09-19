8e8a097a42d405855e5e5111f23bda4c29074acc
Roman Ratsun <roman.ratsun@atrocore.com>
2024-09-19 07:55:38 +0000

    .

--- app/Atro/Migrations/V1Dot10Dot73.php
+++ app/Atro/Migrations/V1Dot10Dot73.php
@@ -13,7 +13,7 @@ namespace Atro\Migrations;
 
 use Atro\Core\Migration\Base;
 
-class V1Dot10Dot72 extends Base
+class V1Dot10Dot73 extends Base
 {
     public function getMigrationDateTime(): ?\DateTime
     {
