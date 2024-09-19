9947c6bf34ba42d110dd6afa2abf0b8ca0153162
Roman Ratsun <roman.ratsun@atrocore.com>
2024-09-19 07:15:34 +0000

    .

--- app/Atro/Migrations/V1Dot10Dot72.php
+++ app/Atro/Migrations/V1Dot10Dot72.php
@@ -29,7 +29,26 @@ class V1Dot10Dot72 extends Base
         $this->exec("ALTER TABLE user_followed_record ALTER user_id TYPE VARCHAR(36)");
         $this->exec("ALTER TABLE entity_team ALTER entity_id TYPE VARCHAR(36)");
 
-        $this->rebuild();
+        if ($this->isPgSQL()) {
+            $res = $this->getConnection()->createQueryBuilder()
+                ->select('table_name, column_name')
+                ->from('information_schema.columns')
+                ->where("data_type='character varying' AND character_maximum_length=24")
+                ->fetchAllAssociative();
+        } else {
+            $dbName = $this->getConfig()->get('database')['dbname'];
+            $res = $this->getConnection()->createQueryBuilder()
+                ->select('table_name, column_name')
+                ->from('nformation_schema.columns')
+                ->where("data_type='varchar' AND character_maximum_length=24 AND table_schema='$dbName'")
+                ->fetchAllAssociative();
+        }
+
+        foreach ($res as $row) {
+            $this->exec("ALTER TABLE " . $this->getConnection()->quoteIdentifier($row['table_name']) . " ALTER {$row['column_name']} TYPE VARCHAR(36)");
+        }
+
+        $this->updateComposer('atrocore/core', '^1.10.72');
     }
 
     public function down(): void
