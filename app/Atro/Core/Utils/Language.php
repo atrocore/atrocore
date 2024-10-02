423592beed9c6dfd99dc1314448c8ae11616ccd7
Roman Ratsun <roman.ratsun@atrocore.com>
2024-10-02 09:50:14 +0000

    .

---
+++ app/Atro/Core/Utils/Language.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Atro\Core\Utils;
+
+use Atro\Core\Container;
+
+class Language extends \Espo\Core\Utils\Language
+{
+    public function __construct(Container $container)
+    {
+        $currentLanguage = self::detectLanguage($container->get('config'), $container->get('preferences'));
+
+        parent::__construct($container, $currentLanguage);
+    }
+
+    protected function getData()
+    {
+        $currentLanguage = $this->getLanguage();
+        if (empty($this->data)) {
+            $this->init();
+        }
+
+        if ($currentLanguage === self::DEFAULT_LANGUAGE) {
+            return $this->data[$currentLanguage];
+        }
+
+        if (empty($data = $this->getDataManager()->getCacheData($currentLanguage))) {
+            $data = $this->data[self::DEFAULT_LANGUAGE];
+
+            $fallbackLanguage = $this->getMetadata()->get(['multilang', 'fallbackLanguage', $currentLanguage]);
+            if ($fallbackLanguage) {
+                $data = \Espo\Core\Utils\Util::merge($data, $this->data[$fallbackLanguage] ?? []);
+            }
+
+            $data = \Espo\Core\Utils\Util::merge($data, $this->data[$currentLanguage] ?? []);
+
+            $this->getDataManager()->setCacheData($currentLanguage, $data);
+        }
+
+        return $data;
+    }
+}
