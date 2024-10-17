559bece3bee5e8012251ee5f44bfe23d44ee2c92
Roman Ratsun <roman.ratsun@atrocore.com>
2024-10-17 08:16:48 +0000

    .

---
+++ app/Atro/Services/Translation.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Atro\Services;
+
+use Atro\Core\Templates\Services\ReferenceData;
+use Espo\ORM\Entity;
+
+class Translation extends ReferenceData
+{
+
+    public function prepareEntityForOutput(Entity $entity)
+    {
+        parent::prepareEntityForOutput($entity);
+
+        $entity->set('name', $entity->get('code'));
+    }
+}
