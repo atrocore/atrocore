17cca93d325e48da068dab431cc0d88190c4691b
Roman Ratsun <roman.ratsun@atrocore.com>
2024-10-17 11:00:37 +0000

    .

--- app/Atro/Controllers/Translation.php
+++ app/Atro/Controllers/Translation.php
@@ -14,77 +14,14 @@ declare(strict_types=1);
 namespace Atro\Controllers;
 
 use Atro\Console\AbstractConsole;
-use Atro\Core\Exceptions\BadRequest;
-use Atro\Core\Exceptions\Forbidden;
-use Atro\Core\Exceptions\NotFound;
 use Atro\Core\Templates\Controllers\ReferenceData;
-use Atro\Core\Utils\Language;
-use Atro\Repositories\Translation as TranslationRepository;
 
 class Translation extends ReferenceData
 {
-    /**
-     * @param mixed $params
-     * @param mixed $data
-     * @param mixed $request
-     *
-     * @return array
-     * @throws BadRequest
-     * @throws NotFound
-     */
-    public function getActionGetDefaults($params, $data, $request): array
-    {
-        if (empty($request->get('key'))) {
-            throw new BadRequest();
-        }
-
-        $records = TranslationRepository::getSimplifiedTranslates((new Language($this->getContainer()))->getModulesData());
-
-        if (empty($records[$request->get('key')])) {
-            throw new NotFound();
-        }
-
-        return $records[$request->get('key')];
-    }
-
     public function postActionReset(): bool
     {
         exec(AbstractConsole::getPhpBinPath($this->getConfig()) . " index.php refresh translations >/dev/null");
 
         return true;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function actionCreateLink($params, $data, $request)
-    {
-        throw new Forbidden();
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function actionMassDelete($params, $data, $request)
-    {
-        throw new Forbidden();
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function actionRemoveLink($params, $data, $request)
-    {
-        throw new Forbidden();
-    }
-
-    /**
-     * @inheritDoc
-     */
-    protected function checkControllerAccess()
-    {
-        if (!$this->getUser()->isAdmin()) {
-            throw new Forbidden();
-        }
     }
 }
