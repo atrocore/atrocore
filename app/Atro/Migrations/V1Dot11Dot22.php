55145b694ec2a967669caa4c96bbf98392d1f0c3
Kevin Teguia <kt@atrocore.com>
2024-10-17 15:25:30 +0100

    add migration

---
+++ app/Atro/Migrations/V1Dot11Dot22.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Atro\Migrations;
+
+use Atro\Core\Migration\Base;
+use Atro\Core\Utils\Util;
+use Doctrine\DBAL\ParameterType;
+
+class V1Dot11Dot22 extends Base
+{
+    public function getMigrationDateTime(): ?\DateTime
+    {
+        return new \DateTime('2024-10-17 18:00:00');
+    }
+
+    public function up(): void
+    {
+        if($this->isPgSQL()){
+            $this->exec('ALTER TABLE "user" DROP is_super_admin');
+        }else{
+            $this->exec('ALTER TABLE user DROP is_super_admin');
+        }
+    }
+
+    public function down(): void
+    {
+    }
+
+    protected function exec(string $query): void
+    {
+        try {
+            $this->getPDO()->exec($query);
+        } catch (\Throwable $e) {
+        }
+    }
+}
