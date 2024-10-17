921eb7fabfd2aa13193e1f01184727e38f475523
Roman Ratsun <roman.ratsun@atrocore.com>
2024-10-17 12:05:28 +0000

    .

--- app/Atro/Repositories/UiHandler.php
+++ app/Atro/Repositories/UiHandler.php
@@ -13,70 +13,70 @@ declare(strict_types=1);
 
 namespace Atro\Repositories;
 
-use Atro\Core\Templates\Repositories\Base;
+use Atro\Core\Templates\Repositories\ReferenceData;
 use Espo\Core\DataManager;
 use Atro\Core\Exceptions\BadRequest;
 use Espo\ORM\Entity;
 
-class UiHandler extends Base
+class UiHandler extends ReferenceData
 {
     public const CACHE_FILE = DataManager::CACHE_DIR_PATH . '/ui_handler.json';
 
-    protected function beforeSave(Entity $entity, array $options = [])
-    {
-        if (!$entity->isNew()) {
-            foreach (['type', 'isActive', 'entityType', 'fields', 'conditionsType', 'conditions'] as $field) {
-                if ($entity->isAttributeChanged($field)) {
-                    $this->validateSystemHandler($entity);
-                }
-            }
-        }
-
-        parent::beforeSave($entity, $options);
-    }
-
-    protected function afterSave(Entity $entity, array $options = [])
-    {
-        $this->deleteCacheFile();
-
-        parent::afterSave($entity, $options);
-    }
-
-    protected function beforeRemove(Entity $entity, array $options = [])
-    {
-        $this->validateSystemHandler($entity);
-
-        parent::beforeRemove($entity, $options);
-    }
-
-    protected function afterRemove(Entity $entity, array $options = [])
-    {
-        $this->deleteCacheFile();
-
-        parent::afterRemove($entity, $options);
-    }
-
-    public function validateSystemHandler(Entity $entity): void
-    {
-        if (!empty($entity->get('hash'))) {
-            throw new BadRequest(
-                sprintf($this->getLanguage()->translate('systemHandler', 'exceptions', 'UiHandler'), $entity->get('name'))
-            );
-        }
-    }
-
-    public function deleteCacheFile(): void
-    {
-        if (empty($this->getMemoryStorage()->get('importJobId'))) {
-            $file = self::CACHE_FILE;
-            if (file_exists($file)) {
-                unlink($file);
-            }
-
-            $this->getConfig()->remove('cacheTimestamp');
-            $this->getConfig()->save();
-
-            DataManager::pushPublicData('dataTimestamp', (new \DateTime())->getTimestamp());
-        }
-    }
+//    protected function beforeSave(Entity $entity, array $options = [])
+//    {
+//        if (!$entity->isNew()) {
+//            foreach (['type', 'isActive', 'entityType', 'fields', 'conditionsType', 'conditions'] as $field) {
+//                if ($entity->isAttributeChanged($field)) {
+//                    $this->validateSystemHandler($entity);
+//                }
+//            }
+//        }
+//
+//        parent::beforeSave($entity, $options);
+//    }
+//
+//    protected function afterSave(Entity $entity, array $options = [])
+//    {
+//        $this->deleteCacheFile();
+//
+//        parent::afterSave($entity, $options);
+//    }
+//
+//    protected function beforeRemove(Entity $entity, array $options = [])
+//    {
+//        $this->validateSystemHandler($entity);
+//
+//        parent::beforeRemove($entity, $options);
+//    }
+//
+//    protected function afterRemove(Entity $entity, array $options = [])
+//    {
+//        $this->deleteCacheFile();
+//
+//        parent::afterRemove($entity, $options);
+//    }
+//
+//    public function validateSystemHandler(Entity $entity): void
+//    {
+//        if (!empty($entity->get('hash'))) {
+//            throw new BadRequest(
+//                sprintf($this->getLanguage()->translate('systemHandler', 'exceptions', 'UiHandler'), $entity->get('name'))
+//            );
+//        }
+//    }
+//
+//    public function deleteCacheFile(): void
+//    {
+//        if (empty($this->getMemoryStorage()->get('importJobId'))) {
+//            $file = self::CACHE_FILE;
+//            if (file_exists($file)) {
+//                unlink($file);
+//            }
+//
+//            $this->getConfig()->remove('cacheTimestamp');
+//            $this->getConfig()->save();
+//
+//            DataManager::pushPublicData('dataTimestamp', (new \DateTime())->getTimestamp());
+//        }
+//    }
 }
