d965ab95fc3b62846e7f41ad21be15b1aac30443
Roman Ratsun <roman.ratsun@atrocore.com>
2024-10-17 12:49:53 +0000

    .

--- app/Atro/Repositories/UiHandler.php
+++ app/Atro/Repositories/UiHandler.php
@@ -13,70 +13,81 @@ declare(strict_types=1);
 
 namespace Atro\Repositories;
 
+use Atro\Core\KeyValueStorages\StorageInterface;
 use Atro\Core\Templates\Repositories\ReferenceData;
 use Espo\Core\DataManager;
 use Atro\Core\Exceptions\BadRequest;
+use Atro\Core\Utils\Language;
 use Espo\ORM\Entity;
 
 class UiHandler extends ReferenceData
 {
-    public const CACHE_FILE = DataManager::CACHE_DIR_PATH . '/ui_handler.json';
+    protected function beforeSave(Entity $entity, array $options = [])
+    {
+        if (!$entity->isNew()) {
+            foreach (['type', 'isActive', 'entityType', 'fields', 'conditionsType', 'conditions'] as $field) {
+                if ($entity->isAttributeChanged($field)) {
+                    $this->validateSystemHandler($entity);
+                }
+            }
+        }
 
-//    protected function beforeSave(Entity $entity, array $options = [])
-//    {
-//        if (!$entity->isNew()) {
-//            foreach (['type', 'isActive', 'entityType', 'fields', 'conditionsType', 'conditions'] as $field) {
-//                if ($entity->isAttributeChanged($field)) {
-//                    $this->validateSystemHandler($entity);
-//                }
-//            }
-//        }
-//
-//        parent::beforeSave($entity, $options);
-//    }
-//
-//    protected function afterSave(Entity $entity, array $options = [])
-//    {
-//        $this->deleteCacheFile();
-//
-//        parent::afterSave($entity, $options);
-//    }
-//
-//    protected function beforeRemove(Entity $entity, array $options = [])
-//    {
-//        $this->validateSystemHandler($entity);
-//
-//        parent::beforeRemove($entity, $options);
-//    }
-//
-//    protected function afterRemove(Entity $entity, array $options = [])
-//    {
-//        $this->deleteCacheFile();
-//
-//        parent::afterRemove($entity, $options);
-//    }
-//
-//    public function validateSystemHandler(Entity $entity): void
-//    {
-//        if (!empty($entity->get('hash'))) {
-//            throw new BadRequest(
-//                sprintf($this->getLanguage()->translate('systemHandler', 'exceptions', 'UiHandler'), $entity->get('name'))
-//            );
-//        }
-//    }
-//
-//    public function deleteCacheFile(): void
-//    {
-//        if (empty($this->getMemoryStorage()->get('importJobId'))) {
-//            $file = self::CACHE_FILE;
-//            if (file_exists($file)) {
-//                unlink($file);
-//            }
-//
-//            $this->getConfig()->remove('cacheTimestamp');
-//            $this->getConfig()->save();
-//
-//            DataManager::pushPublicData('dataTimestamp', (new \DateTime())->getTimestamp());
-//        }
-//    }
+        parent::beforeSave($entity, $options);
+    }
+
+    protected function afterSave(Entity $entity, array $options = [])
+    {
+        $this->refreshCache();
+
+        parent::afterSave($entity, $options);
+    }
+
+    protected function beforeRemove(Entity $entity, array $options = [])
+    {
+        $this->validateSystemHandler($entity);
+
+        parent::beforeRemove($entity, $options);
+    }
+
+    protected function afterRemove(Entity $entity, array $options = [])
+    {
+        $this->refreshCache();
+
+        parent::afterRemove($entity, $options);
+    }
+
+    public function validateSystemHandler(Entity $entity): void
+    {
+        if (!empty($entity->get('system'))) {
+            throw new BadRequest(sprintf($this->getLanguage()->translate('systemHandler', 'exceptions', 'UiHandler'), $entity->get('name')));
+        }
+    }
+
+    public function refreshCache(): void
+    {
+        if (empty($this->getMemoryStorage()->get('importJobId'))) {
+            $this->getConfig()->remove('cacheTimestamp');
+            $this->getConfig()->save();
+
+            DataManager::pushPublicData('dataTimestamp', (new \DateTime())->getTimestamp());
+        }
+    }
+
+    protected function getMemoryStorage(): StorageInterface
+    {
+        return $this->getInjection('memoryStorage');
+    }
+
+    protected function getLanguage(): Language
+    {
+        return $this->getInjection('language');
+    }
+
+    protected function init()
+    {
+        parent::init();
+
+        $this->addDependency('memoryStorage');
+        $this->addDependency('language');
+    }
 }
