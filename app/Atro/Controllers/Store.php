1820e90c68b9057662a8343ad223e44cececb122
Roman Ratsun <roman.ratsun@atrocore.com>
2024-10-22 06:23:41 +0000

    .

---
+++ app/Atro/Controllers/Store.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\Controllers;
+
+use Atro\Core\Exceptions\BadRequest;
+use Atro\Core\Templates\Controllers\ReferenceData;
+
+class Store extends ReferenceData
+{
+    public function actionCreate($params, $data, $request)
+    {
+        throw new BadRequest();
+    }
+
+    public function actionDelete($params, $data, $request)
+    {
+        throw new BadRequest();
+    }
+}
