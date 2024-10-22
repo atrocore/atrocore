7b826593b017e73540d36134ddb6ddbe37610517
Roman Ratsun <roman.ratsun@atrocore.com>
2024-10-22 13:54:42 +0000

    .

--- client/modules/treo-core/src/views/composer/list.js
+++ client/modules/treo-core/src/views/composer/list.js
@@ -146,7 +146,7 @@ Espo.define('treo-core:views/composer/list', 'views/list',
                     this.createView('listStore', 'views/record/list', {
                         collection: collection,
                         el: `${this.options.el} .list-container.modules-store`,
-                        layoutName: 'list',
+                        layoutName: 'listSmall',
                         searchManager: false,
                         selectable: false,
                         checkboxes: false,
