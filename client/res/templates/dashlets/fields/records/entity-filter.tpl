2371ecb44dcba448a9ce6b029352f067a3f2ea18
Roman Ratsun <roman.ratsun@atrocore.com>
2024-11-07 15:42:25 +0000

    .

---
+++ client/res/templates/dashlets/fields/records/entity-filter.tpl
@@ -0,0 +1,5 @@
+<div class="row">
+    <div class="col-12">
+        <div class="search-container">{{{search}}}</div>
+    </div>
+</div>
--- client/src/views/dashlets/fields/records/entity-filter.js
+++ client/src/views/dashlets/fields/records/entity-filter.js
@@ -8,56 +8,19 @@
  * @license    GPLv3 (https://www.gnu.org/licenses/)
  */
 
-Espo.define('views/dashlets/fields/records/entity-filter', ['views/record/panels/bottom', 'search-manager'],
-    (Dep, SearchManager) => Dep.extend({
+Espo.define('views/dashlets/fields/records/entity-filter', 'views/fields/base',
+    Dep => Dep.extend({
 
-        template: 'export:export-feed/simple-type-components/record/panels/simple-type-entity-filter',
-
-        entitySearchView: 'export:views/export-feed/simple-type-components/record/entity-search',
-
-        productSearchView: 'export:views/export-feed/simple-type-components/record/product-search',
-
-        searchManager: null,
-
-        collection: null,
-
-        setup() {
-            Dep.prototype.setup.call(this);
-
-            this.scope = this.model.get('entity');
-
-            console.log('1')
-
-            let data = _.extend({}, this.model.get('data'));
-            if (typeof data.whereScope === 'undefined' || data.whereScope !== this.scope) {
-                data = _.extend(data, {
-                    where: [],
-                    whereData: {},
-                    whereScope: this.scope,
-                });
-                this.model.set({data: data});
-            }
-        },
+        editTemplate: 'dashlets/fields/records/entity-filter',
 
         setupSearchPanel() {
-            this.getCollectionFactory().create(this.scope, collection => {
-                this.collection = collection;
-                this.searchManager = new SearchManager(this.collection, `exportSimpleType`, null, this.getDateTime(), (this.model.get('data') || {}).whereData || [], true);
-
-                let searchView = this.entitySearchView;
-                if (this.scope === 'Product' && this.getMetadata().get('scopes.Product.module') === 'Pim') {
-                    searchView = this.productSearchView;
-                }
-
-                this.createView('search', searchView, {
-                    collection: this.collection,
+            this.getModelFactory().create(this.model.get('entityType'), model => {
+                model.set('data', this.model.get('entityFilter'));
+                this.createView('search', 'views/dashlets/panels/search', {
                     el: `${this.options.el} .search-container`,
-                    searchManager: this.searchManager,
-                    scope: this.scope,
-                    entityType: this.model.name,
-                    viewMode: 'list',
-                    hiddenBoolFilterList: this.getMetadata().get(`clientDefs.${this.scope}.hiddenBoolFilterList`) || [],
-                    feedModel: this.model,
+                    model: model,
+                    mainModel: this.model,
+                    mode: 'edit'
                 }, view => {
                     view.render();
                 });
@@ -67,14 +30,12 @@ Espo.define('views/dashlets/fields/records/entity-filter', ['views/record/panels
         afterRender() {
             Dep.prototype.afterRender.call(this);
 
-            console.log('q111')
+            this.$el.parent().show();
+            if (!this.model.get('entityType')) {
+                this.$el.parent().hide();
+            }
 
-            // this.$el.parent().show();
-            // if (!this.model.get('entity') || this.model.get('hasMultipleSheets')) {
-            //     this.$el.parent().hide();
-            // }
-            //
-            // this.setupSearchPanel()
+            this.setupSearchPanel()
         },
 
     })
