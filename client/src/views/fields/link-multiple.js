c44b5416adc9328d95609dde061b7043621e4dbe
Roman Zablodskyi <rz@atrocore.com>
2024-11-12 15:37:16 +0200

    Fixed autcoplete search filter

--- client/src/views/fields/link-multiple.js
+++ client/src/views/fields/link-multiple.js
@@ -335,7 +335,7 @@ Espo.define('views/fields/link-multiple', 'views/fields/base', function (Dep) {
                 let foreignDefs = this.getMetadata().get(['entityDefs', this.foreignScope, 'fields']);
 
                 if (foreignDefs && typeof foreignDefs === 'object' && foreignDefs.name) {
-                    where.push({type: 'contains', attribute: 'name', value: q});
+                    where.push({type: 'like', attribute: 'name', value: this.prepareAutocompleteQueryText(q)});
                 }
             }
 
@@ -351,6 +351,18 @@ Espo.define('views/fields/link-multiple', 'views/fields/base', function (Dep) {
             }
 
             return url;
+        },
+
+        prepareAutocompleteQueryText(q) {
+            if (q.includes('*')) {
+                q = q.replace(/\*/g, '%');
+            }
+
+            if (!q.includes('%')) {
+                q += '%';
+            }
+
+            return q;
         },
 
         getAutocompleteAdditionalWhereConditions: function () {
