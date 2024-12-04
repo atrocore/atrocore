745192cc3f981901d838457434ac6f7b2af7e030
Roman Ratsun <roman.ratsun@atrocore.com>
2024-12-04 15:56:22 +0000

    .

--- app/Atro/Console/Cron.php
+++ app/Atro/Console/Cron.php
@@ -126,6 +126,60 @@ class Cron extends AbstractConsole
         $this->runCronManager();
     }
 
+    protected function cleanupEntities(): void
+    {
+        // if ($this->getConfig()->get('notificationsMaxDays') !== 0) {
+        //            $this->createJob('Delete Notifications', '20 1 * * 0', 'Notification', 'deleteOld');
+        //        }
+        //        if ($this->getConfig()->get('queueItemsMaxDays') !== 0) {
+        //            $this->createJob('Delete Queue Items', '42 1 * * 0', 'QueueItem', 'deleteOld');
+        //        }
+        //        if ($this->getConfig()->get('jobsMaxDays') !== 0) {
+        //            $this->createJob('Delete Jobs', '0 2 * * 0', 'Job', 'deleteOld');
+        //        }
+        //        if ($this->getConfig()->get('authLogsMaxDays') !== 0) {
+        //            $this->createJob('Delete Auth Logs', '40 2 * * 0', 'AuthLogRecord', 'deleteOld');
+        //        }
+        //        if ($this->getConfig()->get('actionHistoryMaxDays') !== 0) {
+        //            $this->createJob('Delete Action History Records', '50 2 * * 0', 'ActionHistoryRecord', 'deleteOld');
+        //        }
+        //        if ($this->getConfig()->get('deletedItemsMaxDays') !== 0) {
+        //            $this->createJob('Remove Deleted Items', '20 3 * * 0', 'App', 'cleanupDeleted');
+        //        }
+        //        if ($this->getConfig()->get('cleanDbSchema') !== false) {
+        //            $this->createJob('Clean DB Schema', '50 3 * * 0', 'App', 'cleanDbSchema');
+        //        }
+        //        if ($this->getConfig()->get('cleanEntityTeam') !== false) {
+        //            $this->createJob('Clean Entity Team', '0 4 * * 0', 'App', 'cleanupEntityTeam');
+        //        }
+
+        // $cronExpression = \Cron\CronExpression::factory($scheduling);
+        //        $nextDate = $cronExpression->getNextRunDate()->format('Y-m-d H:i:s');
+        //
+        //        $existingJob = $this->getEntityManager()->getRepository('Job')
+        //            ->where([
+        //                'serviceName' => $serviceName,
+        //                'methodName'  => $methodName,
+        //                'executeTime' => $nextDate,
+        //            ])
+        //            ->findOne();
+        //
+        //        if (!empty($existingJob)) {
+        //            return;
+        //        }
+        //
+        //        $jobEntity = $this->getEntityManager()->getEntity('Job');
+        //        $jobEntity->set([
+        //            'name'        => $name,
+        //            'status'      => 'Pending',
+        //            'serviceName' => $serviceName,
+        //            'methodName'  => $methodName,
+        //            'executeTime' => $nextDate
+        //        ]);
+        //        $this->getEntityManager()->saveEntity($jobEntity);
+
+    }
+
     /**
      * Run cron manager
      */
@@ -133,6 +187,10 @@ class Cron extends AbstractConsole
     {
         $auth = new \Espo\Core\Utils\Auth($this->getContainer());
         $auth->useNoAuth();
+
+        if ((new \DateTime())->format('H:i') === '22:22') {
+            $this->cleanupEntities();
+        }
 
         $scheduledJobs = $this->getEntityManager()->getRepository('ScheduledJob')
             ->where(['isActive' => true])
