1bfa6e679075d90835b59d438b8b0a7c998a251d
Roman Ratsun <roman.ratsun@atrocore.com>
2024-12-09 12:51:35 +0000

    .

--- app/Atro/Core/Templates/Repositories/Base.php
+++ app/Atro/Core/Templates/Repositories/Base.php
@@ -18,21 +18,31 @@ use Doctrine\DBAL\ParameterType;
 
 class Base extends RDB
 {
-    protected int $cleanupDays = 60;
-
     public function hasDeletedRecordsToCleanup(): bool
     {
         if (empty($this->seed)) {
             return false;
         }
 
+        if (!empty($this->getMetadata()->get(['scopes', $this->entityName, 'autoDeleteAfterDays']))) {
+            return true;
+        }
+
+        $cleanupDays = $this->getMetadata()->get(['scopes', $this->entityName, 'cleanupDeletedAfterDays']) ?? 60;
+
+        $tableName = $this->getEntityManager()->getMapper()->toDb($this->entityName);
+
         $qb = $this->getConnection()->createQueryBuilder()
-            ->select('*')
-            ->from($this->getConnection()->quoteIdentifier($this->getEntityManager()->getMapper()->toDb($this->entityName)))
+            ->select('id')
+            ->from($this->getConnection()->quoteIdentifier($tableName))
             ->where('deleted=:true')
             ->setParameter('true', true, ParameterType::BOOLEAN);
 
-        $date = (new \DateTime())->modify("-{$this->cleanupDays} days")->format('Y-m-d H:i:s');
+        $date = new \DateTime();
+        if ($cleanupDays > 0) {
+            $date->modify("-{$cleanupDays} days");
+        }
+        $date = $date->format('Y-m-d H:i:s');
 
         if ($this->seed->hasField('modifiedAt')) {
             if ($this->seed->hasField('createdAt')) {
@@ -55,8 +65,46 @@ class Base extends RDB
             return;
         }
 
+        $autoDeleteAfterDays = $this->getMetadata()->get(['scopes', $this->entityName, 'autoDeleteAfterDays']);
+
+        if (!empty($autoDeleteAfterDays) && $autoDeleteAfterDays > 0) {
+            $date = (new \DateTime())->modify("-$autoDeleteAfterDays days");
+            while (true) {
+                $toDelete = [];
+                if ($this->seed->hasField('modifiedAt')) {
+                    $toDelete = $this
+                        ->where(['modifiedAt<' => $date->format('Y-m-d H:i:s')])
+                        ->limit(0, 2000)
+                        ->order('modifiedAt')
+                        ->find();
+                } elseif ($this->seed->hasField('createdAt')) {
+                    $toDelete = $this
+                        ->where(['createdAt<' => $date->format('Y-m-d H:i:s')])
+                        ->limit(0, 2000)
+                        ->order('createdAt')
+                        ->find();
+                }
+                if (empty($toDelete[0])) {
+                    break;
+                }
+                foreach ($toDelete as $entity) {
+                    $this->getEntityManager()->removeEntity($entity);
+                }
+            }
+        }
+
+        $cleanupDays = $this->getMetadata()->get(['scopes', $this->entityName, 'cleanupDeletedAfterDays']) ?? 60;
+
+        $date = new \DateTime();
+        if ($cleanupDays > 0) {
+            $date->modify("-{$cleanupDays} days");
+        }
+        $date = $date->format('Y-m-d H:i:s');
+
+        $tableName = $this->getEntityManager()->getMapper()->toDb($this->entityName);
+
         $qb = $this->getConnection()->createQueryBuilder()
-            ->delete($this->getConnection()->quoteIdentifier($this->getEntityManager()->getMapper()->toDb($this->entityName)))
+            ->delete($this->getConnection()->quoteIdentifier($tableName))
             ->where('deleted=:true')
             ->setParameter('true', true, ParameterType::BOOLEAN);
 
@@ -66,7 +114,10 @@ class Base extends RDB
             } else {
                 $qb->andWhere('modified_at<:date OR modified_at IS NULL');
             }
-            $qb->setParameter('date', (new \DateTime())->modify("-{$this->cleanupDays} days")->format('Y-m-d H:i:s'));
+            $qb->setParameter('date', $date);
+        } elseif ($this->seed->hasField('createdAt')) {
+            $qb->andWhere('created_at<:date OR created_at IS NULL');
+            $qb->setParameter('date', $date);
         }
 
         $qb->executeQuery();
