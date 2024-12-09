dd24d4b08c1dddaf7dd09ac49cc539852a940f1d
Kevin Teguia <kt@atrocore.com>
2024-12-09 13:31:06 +0100

    fix migration name

--- app/Atro/Migrations/V1Dot11Dot59.php
+++ app/Atro/Migrations/V1Dot11Dot59.php
@@ -16,11 +16,11 @@ use Atro\Core\Migration\Base;
 use Atro\Core\Templates\Repositories\ReferenceData;
 use Atro\Core\Utils\Util;
 
-class V1Dot11Dot58 extends Base
+class V1Dot11Dot59 extends Base
 {
     public function getMigrationDateTime(): ?\DateTime
     {
-        return new \DateTime('2024-12-05 14:00:00');
+        return new \DateTime('2024-12-09 14:00:00');
     }
 
     public function up(): void
