5ea37b7f07e684008f7f879a2ad02c794d541067
Roman Ratsun <roman.ratsun@atrocore.com>
2024-12-09 13:07:22 +0000

    .

--- app/Atro/Jobs/CleanupEntity.php
+++ app/Atro/Jobs/CleanupEntity.php
@@ -29,44 +29,5 @@ class CleanupEntity extends AbstractJob implements JobInterface
         } catch (\Throwable $e) {
             $GLOBALS['log']->error("Cleanup Entity failed for $entityName: {$e->getMessage()}");
         }
-
-        //        if ($this->getConfig()->get('queueItemsMaxDays') !== 0) {
-        //            $this->createJob('Delete Queue Items', '42 1 * * 0', 'QueueItem', 'deleteOld');
-        //        }
-        //        if ($this->getConfig()->get('authLogsMaxDays') !== 0) {
-        //            $this->createJob('Delete Auth Logs', '40 2 * * 0', 'AuthLogRecord', 'deleteOld');
-        //        }
-
-        //        if ($this->getConfig()->get('deletedItemsMaxDays') !== 0) {
-        //            $this->createJob('Remove Deleted Items', '20 3 * * 0', 'App', 'cleanupDeleted');
-        //        }
-        //        if ($this->getConfig()->get('cleanEntityTeam') !== false) {
-        //            $this->createJob('Clean Entity Team', '0 4 * * 0', 'App', 'cleanupEntityTeam');
-        //        }
-
-        // $cronExpression = \Cron\CronExpression::factory($scheduling);
-        //        $nextDate = $cronExpression->getNextRunDate()->format('Y-m-d H:i:s');
-        //
-        //        $existingJob = $this->getEntityManager()->getRepository('Job')
-        //            ->where([
-        //                'serviceName' => $serviceName,
-        //                'methodName'  => $methodName,
-        //                'executeTime' => $nextDate,
-        //            ])
-        //            ->findOne();
-        //
-        //        if (!empty($existingJob)) {
-        //            return;
-        //        }
-        //
-        //        $jobEntity = $this->getEntityManager()->getEntity('Job');
-        //        $jobEntity->set([
-        //            'name'        => $name,
-        //            'status'      => 'Pending',
-        //            'serviceName' => $serviceName,
-        //            'methodName'  => $methodName,
-        //            'executeTime' => $nextDate
-        //        ]);
-        //        $this->getEntityManager()->saveEntity($jobEntity);
     }
 }
