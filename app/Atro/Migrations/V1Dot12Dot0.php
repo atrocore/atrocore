6e304ff8660688fa497eff0c68953e756a25f2fa
Roman Ratsun <roman.ratsun@atrocore.com>
2024-12-10 14:39:32 +0000

    .

--- app/Atro/Migrations/V1Dot12Dot0.php
+++ app/Atro/Migrations/V1Dot12Dot0.php
@@ -20,58 +20,62 @@ class V1Dot12Dot0 extends Base
 {
     public function getMigrationDateTime(): ?\DateTime
     {
-        return new \DateTime('2024-12-04 18:00:00');
+        return new \DateTime('2024-12-10 18:00:00');
     }
 
     public function up(): void
     {
-//        file_put_contents(Cron::DAEMON_KILLER, '1');
-//
-//        $this->getConfig()->set('maxConcurrentWorkers', $this->getConfig()->get('queueManagerWorkersCount', 6));
-//        $this->getConfig()->save();
+        file_put_contents(Cron::DAEMON_KILLER, '1');
+
+        $this->getConfig()->set('maxConcurrentWorkers', $this->getConfig()->get('queueManagerWorkersCount', 6));
+        $this->getConfig()->save();
 
         if ($this->isPgSQL()) {
-            // ALTER TABLE job ADD priority DOUBLE PRECISION DEFAULT '100';
-            // ALTER TABLE job ADD type VARCHAR(255) DEFAULT NULL;
-            // ALTER TABLE job ADD payload TEXT DEFAULT NULL;
-            // COMMENT ON COLUMN job.payload IS '(DC2Type:jsonObject)'
-
-            // ALTER TABLE job ADD owner_user_id VARCHAR(36) DEFAULT NULL;
-            // ALTER TABLE job ADD assigned_user_id VARCHAR(36) DEFAULT NULL;
-            // CREATE INDEX IDX_JOB_OWNER_USER_ID ON job (owner_user_id, deleted);
-            // CREATE INDEX IDX_JOB_ASSIGNED_USER_ID ON job (assigned_user_id, deleted)
-
-            // ALTER TABLE scheduled_job ADD type VARCHAR(255) DEFAULT NULL
+            $this->exec("ALTER TABLE job ADD priority DOUBLE PRECISION DEFAULT '100'");
+            $this->exec("ALTER TABLE job ADD type VARCHAR(255) DEFAULT NULL");
+            $this->exec("ALTER TABLE job ADD payload TEXT DEFAULT NULL;COMMENT ON COLUMN job.payload IS '(DC2Type:jsonObject)'");
+            $this->exec("ALTER TABLE job ADD owner_user_id VARCHAR(36) DEFAULT NULL");
+            $this->exec("ALTER TABLE job ADD assigned_user_id VARCHAR(36) DEFAULT NULL");
+            $this->exec("CREATE INDEX IDX_JOB_OWNER_USER_ID ON job (owner_user_id, deleted)");
+            $this->exec("CREATE INDEX IDX_JOB_ASSIGNED_USER_ID ON job (assigned_user_id, deleted)");
+            $this->exec("ALTER TABLE scheduled_job ADD type VARCHAR(255) DEFAULT NULL");
+            $this->exec("ALTER TABLE job ADD message TEXT DEFAULT NULL");
+            $this->exec("ALTER TABLE job ADD started_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL");
+            $this->exec("ALTER TABLE job ADD ended_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL");
         } else {
-
+            $this->exec("DROP INDEX IDX_JOB_QUEUE_ITEM_ID ON job");
+            $this->exec("ALTER TABLE job ADD message LONGTEXT DEFAULT NULL, ADD started_at DATETIME DEFAULT NULL, ADD ended_at DATETIME DEFAULT NULL, ADD priority DOUBLE PRECISION DEFAULT '100', ADD type VARCHAR(255) DEFAULT NULL, ADD assigned_user_id VARCHAR(36) DEFAULT NULL, CHANGE data payload LONGTEXT DEFAULT NULL COMMENT '(DC2Type:jsonObject)', CHANGE queue_item_id owner_user_id VARCHAR(36) DEFAULT NULL");
+            $this->exec("CREATE INDEX IDX_JOB_OWNER_USER_ID ON job (owner_user_id, deleted)");
+            $this->exec("CREATE INDEX IDX_JOB_ASSIGNED_USER_ID ON job (assigned_user_id, deleted)");
+            $this->exec("ALTER TABLE scheduled_job CHANGE scheduling scheduling VARCHAR(255) DEFAULT '0 2 * * *', CHANGE status type VARCHAR(255) DEFAULT NULL, CHANGE is_internal is_active TINYINT(1) DEFAULT '0' NOT NULL");
         }
 
-//        $this->getConnection()->createQueryBuilder()
-//            ->update('scheduled_job')
-//            ->set('type', 'job')
-//            ->where('deleted=:false')
-//            ->setParameter('false', false, ParameterType::BOOLEAN)
-//            ->executeQuery();
+        $this->getConnection()->createQueryBuilder()
+            ->update('scheduled_job')
+            ->set('type', 'job')
+            ->where('deleted=:false')
+            ->setParameter('false', false, ParameterType::BOOLEAN)
+            ->executeQuery();
 
-//        $this->getConnection()->createQueryBuilder()
-//            ->update('scheduled_job')
-//            ->set('is_active', ':true')
-//            ->where('deleted=:false')
-//            ->andWhere('status=:active')
-//            ->setParameter('true', true, ParameterType::BOOLEAN)
-//            ->setParameter('false', false, ParameterType::BOOLEAN)
-//            ->setParameter('active', 'Active')
-//            ->executeQuery();
+        $this->getConnection()->createQueryBuilder()
+            ->update('scheduled_job')
+            ->set('is_active', ':true')
+            ->where('deleted=:false')
+            ->andWhere('status=:active')
+            ->setParameter('true', true, ParameterType::BOOLEAN)
+            ->setParameter('false', false, ParameterType::BOOLEAN)
+            ->setParameter('active', 'Active')
+            ->executeQuery();
 
-//        $this->getConnection()->createQueryBuilder()
-//            ->delete($this->getConnection()->quoteIdentifier('job'))
-//            ->where('status=:pending OR status=:running OR deleted=:true')
-//            ->setParameter('pending', 'Pending')
-//            ->setParameter('running', 'Running')
-//            ->setParameter('true', true, ParameterType::BOOLEAN)
-//            ->executeQuery();
+        $this->getConnection()->createQueryBuilder()
+            ->delete($this->getConnection()->quoteIdentifier('job'))
+            ->where('status=:pending OR status=:running OR deleted=:true')
+            ->setParameter('pending', 'Pending')
+            ->setParameter('running', 'Running')
+            ->setParameter('true', true, ParameterType::BOOLEAN)
+            ->executeQuery();
 
-//        $this->updateComposer('atrocore/core', '^1.12.0');
+        $this->updateComposer('atrocore/core', '^1.12.0');
     }
 
     public function down(): void
