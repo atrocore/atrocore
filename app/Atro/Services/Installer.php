f61c5277d8bafd0fcfaca25da18238dfede6b1cd
Roman Zablodskyi <rz@atrocore.com>
2024-12-12 09:31:37 +0200

    Added default HTML sanitizer

--- app/Atro/Services/Installer.php
+++ app/Atro/Services/Installer.php
@@ -16,6 +16,7 @@ namespace Atro\Services;
 use Atro\Core\Templates\Services\HasContainer;
 use Atro\Console\AbstractConsole;
 use Atro\Core\ModuleManager\Manager;
+use Atro\Migrations\V1Dot12Dot1;
 use Atro\ORM\DB\RDB\Mapper;
 use Atro\Core\Utils\Language;
 use Atro\Core\Utils\Util;
@@ -749,6 +750,8 @@ class Installer extends HasContainer
             }
         }
         @file_put_contents(ReferenceData::DIR_PATH . DIRECTORY_SEPARATOR . 'EmailTemplate.json', json_encode($emailTemplates));
+
+        @file_put_contents(ReferenceData::DIR_PATH . DIRECTORY_SEPARATOR . 'HtmlSanitizer.json', json_encode([V1Dot12Dot1::getDefaultHtmlSanitizer()]));
 
         $this->createDefaultLayoutProfile();
 
