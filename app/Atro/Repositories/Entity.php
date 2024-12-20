dcac88723cbb4bd879f991bd405b2d0cfdffb7d3
Roman Ratsun <roman.ratsun@atrocore.com>
2024-12-20 13:25:41 +0000

    .

--- app/Atro/Repositories/Entity.php
+++ app/Atro/Repositories/Entity.php
@@ -38,18 +38,17 @@ class Entity extends ReferenceData
             }
 
             $items[] = array_merge($row, [
-                'id'               => $code,
-                'code'             => $code,
-                'name'             => $this->getLanguage()->translate($code, 'scopeNames'),
-                'namePlural'       => $this->getLanguage()->translate($code, 'scopeNamesPlural'),
-                'iconClass'        => $this->getMetadata()->get(['clientDefs', $code, 'iconClass']),
-                'kanbanViewMode'   => $this->getMetadata()->get(['clientDefs', $code, 'kanbanViewMode']),
-                'color'            => $this->getMetadata()->get(['clientDefs', $code, 'color']),
-                'sortBy'           => $this->getMetadata()->get(['entityDefs', $code, 'collection', 'sortBy']),
-                'sortDirection'    => $this->getMetadata()
-                    ->get(['entityDefs', $code, 'collection', 'asc']) ? 'asc' : 'desc',
-                'textFilterFields' => $this->getMetadata()
-                    ->get(['entityDefs', $code, 'collection', 'textFilterFields']),
+                'id'                    => $code,
+                'code'                  => $code,
+                'name'                  => $this->getLanguage()->translate($code, 'scopeNames'),
+                'namePlural'            => $this->getLanguage()->translate($code, 'scopeNamesPlural'),
+                'iconClass'             => $this->getMetadata()->get(['clientDefs', $code, 'iconClass']),
+                'kanbanViewMode'        => $this->getMetadata()->get(['clientDefs', $code, 'kanbanViewMode']),
+                'clearDeletedAfterDays' => $this->getMetadata()->get(['scopes', $code, 'clearDeletedAfterDays'], 60),
+                'color'                 => $this->getMetadata()->get(['clientDefs', $code, 'color']),
+                'sortBy'                => $this->getMetadata()->get(['entityDefs', $code, 'collection', 'sortBy']),
+                'sortDirection'         => $this->getMetadata()->get(['entityDefs', $code, 'collection', 'asc']) ? 'asc' : 'desc',
+                'textFilterFields'      => $this->getMetadata()->get(['entityDefs', $code, 'collection', 'textFilterFields']),
             ]);
         }
 
