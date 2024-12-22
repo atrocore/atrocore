77b1ac265223f60725431fb71f0c2d63115b0e49
yurdm <yd@atrocore.com>
2024-12-23 01:44:46 +0200

    Add new twig functions

---
+++ app/Atro/TwigFunction/ParseMarkdown.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\TwigFunction;
+
+use Atro\Core\Twig\AbstractTwigFunction;
+
+class ParseMarkdown extends AbstractTwigFunction
+{
+    public function run(string $value): ?string
+    {
+        if (!$value) {
+            return null;
+        }
+
+        return (new \Parsedown())->parse($value);
+    }
+}
