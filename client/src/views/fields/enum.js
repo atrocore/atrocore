26adc668c73bdf9068fec18306be51e6f90d18c1
Roman Ratsun <roman.ratsun@atrocore.com>
2024-12-27 10:49:17 +0000

    .

--- client/src/views/fields/enum.js
+++ client/src/views/fields/enum.js
@@ -84,6 +84,14 @@ Espo.define('views/fields/enum', ['views/fields/base', 'lib!Selectize'], functio
             Dep.prototype.setup.call(this);
 
             if (!this.params.options) {
+                this.params.options = this.model.getFieldParam(this.name, 'optionsIds');
+            }
+
+            if (!this.params.options) {
+                this.params.options = this.model.getFieldParam(this.name, 'options');
+            }
+
+            if (!this.params.options) {
                 var methodName = 'get' + Espo.Utils.upperCaseFirst(this.name) + 'Options';
                 if (typeof this.model[methodName] == 'function') {
                     this.params.options = this.model[methodName].call(this.model);
