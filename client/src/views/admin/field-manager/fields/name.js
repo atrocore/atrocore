77592a573c49b6d5973ce073b2aaa687c7c6dccf
Roman Ratsun <roman.ratsun@atrocore.com>
2025-01-08 09:39:18 +0000

    .

---
+++ client/src/views/admin/field-manager/fields/name.js
@@ -0,0 +1,26 @@
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+Espo.define('views/admin/field-manager/fields/name', 'views/fields/varchar', Dep => {
+
+    return Dep.extend({
+
+        setup() {
+            Dep.prototype.setup.call(this);
+
+            this.listenTo(this.model, 'change:code', () => {
+                if (!this.model.get('name')) {
+                    this.model.set('name', this.model.get('code'));
+                }
+            });
+        },
+
+    });
+});
