b36fef33d2c7bd6651fbe9868f31e191d5a0f92e
Kevin Teguia <kt@atrocore.com>
2025-01-15 11:41:37 +0100

    .

--- app/Atro/Migrations/V1Dot12Dot12.php
+++ app/Atro/Migrations/V1Dot12Dot12.php
@@ -61,6 +61,9 @@ class V1Dot12Dot12 extends Base
                 $oldConfig = $config->get('customStylesheetsList', []);
                 if(!empty($oldConfig[$theme])) {
                     foreach ($styles[$key] as $param => $_) {
+                        if(in_array($param, ['customStylesheetPath', 'customHeadCodePath'])) {
+                            continue;
+                        }
                         if(!empty($oldConfig[$theme][$param])) {
                             $styles[$key][$param] = $oldConfig[$theme][$param];
                         }
