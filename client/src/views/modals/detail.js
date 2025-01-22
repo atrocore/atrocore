5623b16e9f8fe474bc07b2747ac8ca26e845a759
yurdm <yd@atrocore.com>
2025-01-23 00:51:09 +0200

    Display status icons on modal window

--- client/src/views/modals/detail.js
+++ client/src/views/modals/detail.js
@@ -270,6 +270,12 @@ Espo.define('views/modals/detail', 'views/modal', function (Dep) {
             if (!this.navigateButtonsDisabled) {
                 this.controlNavigationButtons();
             }
+
+            if ((this.options.htmlStatusIcons || []).length > 0) {
+                const iconsContainer = $('<div class="icons-container pull-right"></div>');
+                this.options.htmlStatusIcons.forEach(icon => iconsContainer.append(icon));
+                this.$el.find('.modal-body').prepend(iconsContainer);
+            }
         },
 
         controlNavigationButtons: function () {
@@ -381,7 +387,8 @@ Espo.define('views/modals/detail', 'views/modal', function (Dep) {
             this.createView('quickEdit', viewName, {
                 scope: this.scope,
                 id: this.id,
-                fullFormDisabled: this.fullFormDisabled
+                fullFormDisabled: this.fullFormDisabled,
+                htmlStatusIcons: this.options.htmlStatusIcons
             }, function (view) {
                 view.once('after:render', function () {
                     Espo.Ui.notify(false);
