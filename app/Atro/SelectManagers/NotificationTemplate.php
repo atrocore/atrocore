4a27331df731a03d7ef68e202565200825026509
Roman Zablodskyi <rz@atrocore.com>
2025-01-23 16:38:18 +0200

    .

--- app/Atro/SelectManagers/NotificationTemplate.php
+++ app/Atro/SelectManagers/NotificationTemplate.php
@@ -13,40 +13,8 @@ declare(strict_types=1);
 
 namespace Atro\SelectManagers;
 
-use Doctrine\DBAL\ParameterType;
 use Espo\Core\SelectManagers\Base;
 
 class NotificationTemplate extends Base
 {
-    protected function boolFilterTransportType(array &$result)
-    {
-        if (!empty($type = $this->getBoolFilterParameter('transportType'))) {
-            $connection = $this->getEntityManager()->getConnection();
-
-            $list = $connection
-                ->createQueryBuilder()
-                ->select('nr.id, nr.data')
-                ->from($connection->quoteIdentifier('notification_rule'), 'nr')
-                ->where("nr.data LIKE :condition")
-                ->andWhere("nr.deleted = :false")
-                ->setParameter('condition', '%"' . $type . 'Active":true%')
-                ->setParameter('false', false, ParameterType::BOOLEAN)
-                ->fetchAllAssociative();
-
-            $ids = [];
-            $field = $type . 'TemplateId';
-
-            foreach ($list as $item) {
-                $data = @json_decode($item['data'], true);
-
-                if (!empty($data['field'][$field])) {
-                    $ids[] = $data['field'][$field];
-                }
-            }
-
-            $result['whereClause'][] = [
-                'id' => array_unique($ids)
-            ];
-        }
-    }
 }
