af69bcc00483a3fe14ffeb078608f7e55cdc7321
Kevin Teguia <kt@atrocore.com>
2025-01-31 16:24:35 +0100

    .

--- app/Atro/Listeners/SettingsController.php
+++ app/Atro/Listeners/SettingsController.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
 
 namespace Atro\Listeners;
 
