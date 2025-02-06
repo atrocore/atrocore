dc092d5b46f2c25b569bf89c134e34d831968d15
Roman Ratsun <roman.ratsun@atrocore.com>
2025-02-06 08:34:29 +0000

    .

--- app/Atro/Migrations/V1Dot13Dot11.php
+++ app/Atro/Migrations/V1Dot13Dot11.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace Atro\Migrations;
 
 use Atro\Core\Migration\Base;
+use Atro\Core\Utils\Util;
 
 class V1Dot13Dot11 extends Base
 {
@@ -24,9 +25,83 @@ class V1Dot13Dot11 extends Base
 
     public function up(): void
     {
-        if($this->isPgSQL()) {
+//        $vendorPath = 'vendor/atrocore';
+        $vendorPath = 'src';
 
-        }else{
+        $scopes = [];
+
+        foreach (scandir($vendorPath) as $module) {
+            if (in_array($module, ['.', '..'])) {
+                continue;
+            }
+
+            $scopesDir = $module === 'atrocore' ? "$vendorPath/$module/app/Atro/Resources/metadata/scopes" : "$vendorPath/$module/app/Resources/metadata/scopes";
+            if (!is_dir($scopesDir)) {
+                continue;
+            }
+
+            foreach (scandir($scopesDir) as $scopeFile) {
+                if (in_array($scopeFile, ['.', '..'])) {
+                    continue;
+                }
+
+                $scopeDefs = json_decode(file_get_contents("$scopesDir/$scopeFile"), true);
+
+                if (empty($scopeDefs['type'])) {
+                    continue;
+                }
+
+                if (array_key_exists('stream', $scopeDefs) && !array_key_exists('streamDisabled', $scopeDefs)) {
+                    $scopeDefs['streamDisabled'] = empty($scopeDefs['stream']);
+                }
+
+                if (!empty($scopeDefs['streamDisabled']) || !empty($scopeDefs['notStorable'])) {
+                    continue;
+                }
+
+                $scopes[] = str_replace('.json', '', $scopeFile);
+            }
+        }
+
+        $customPath = 'data/metadata/scopes';
+        if (is_dir($customPath)) {
+            foreach (scandir($customPath) as $scopeFile) {
+                if (in_array($scopeFile, ['.', '..'])) {
+                    continue;
+                }
+
+                $scopeDefs = json_decode(file_get_contents("$customPath/$scopeFile"), true);
+
+                if (array_key_exists('stream', $scopeDefs) && !array_key_exists('streamDisabled', $scopeDefs)) {
+                    $scopeDefs['streamDisabled'] = empty($scopeDefs['stream']);
+                }
+
+                if (!empty($scopeDefs['streamDisabled']) || !empty($scopeDefs['notStorable'])) {
+                    continue;
+                }
+
+                $scopes[] = str_replace('.json', '', $scopeFile);
+            }
+        }
+
+        $tables = [];
+        foreach (array_unique($scopes) as $scope) {
+            $tables[] = Util::toUnderScore(lcfirst($scope));
+        }
+
+        if ($this->isPgSQL()) {
+            foreach ($tables as $table) {
+                $uppercased = strtoupper($table);
+                $this->exec("CREATE TABLE user_followed_{$table} (id VARCHAR(36) NOT NULL, deleted BOOLEAN DEFAULT 'false', created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, modified_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, created_by_id VARCHAR(36) DEFAULT NULL, modified_by_id VARCHAR(36) DEFAULT NULL, user_id VARCHAR(36) DEFAULT NULL, {$table}_id VARCHAR(36) DEFAULT NULL, PRIMARY KEY(id))");
+                $this->exec("CREATE UNIQUE INDEX IDX_USER_FOLLOWED_{$uppercased}_UNIQUE_RELATION ON user_followed_{$table} (deleted, user_id, {$table}_id)");
+                $this->exec("CREATE INDEX IDX_USER_FOLLOWED_{$uppercased}_CREATED_BY_ID ON user_followed_{$table} (created_by_id, deleted)");
+                $this->exec("CREATE INDEX IDX_USER_FOLLOWED_{$uppercased}_MODIFIED_BY_ID ON user_followed_{$table} (modified_by_id, deleted)");
+                $this->exec("CREATE INDEX IDX_USER_FOLLOWED_{$uppercased}_USER_ID ON user_followed_{$table} (user_id, deleted)");
+                $this->exec("CREATE INDEX IDX_USER_FOLLOWED_{$uppercased}_{$uppercased}_ID ON user_followed_{$table} ({$table}_id, deleted)");
+                $this->exec("CREATE INDEX IDX_USER_FOLLOWED_{$uppercased}_CREATED_AT ON user_followed_{$table} (created_at, deleted)");
+                $this->exec("CREATE INDEX IDX_USER_FOLLOWED_{$uppercased}_MODIFIED_AT ON user_followed_{$table} (modified_at, deleted)");
+            }
+        } else {
 
         }
     }
