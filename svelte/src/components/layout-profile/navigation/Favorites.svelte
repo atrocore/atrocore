60eea8f4394c9644fccee6d2e23199a635ed697f
yurdm <yd@atrocore.com>
2025-02-06 15:34:19 +0200

    .

--- svelte/src/components/layout-profile/navigation/Favorites.svelte
+++ svelte/src/components/layout-profile/navigation/Favorites.svelte
@@ -1,13 +1,3 @@
-<!--
-  - AtroCore Software
-  -
-  - This source file is available under GNU General Public License version 3 (GPLv3).
-  - Full copyright and license information is available in LICENSE.txt, located in the root directory.
-  -
-  - @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
-  - @license    GPLv3 (https://www.gnu.org/licenses/)
-  -->
-
 <script lang="ts">
 
     import RowsLayout from './RowsLayout.svelte';
