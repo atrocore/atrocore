b39cbdb1b1fdcd481fa9fe977772301de9c2ac38
Kevin Teguia <kt@atrocore.com>
2025-02-06 09:25:09 +0100

    Add  addMenu

--- app/Atro/Core/ModuleManager/AfterInstallAfterDelete.php
+++ app/Atro/Core/ModuleManager/AfterInstallAfterDelete.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 namespace Atro\Core\ModuleManager;
 
 use Atro\Core\Container;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ParameterType;
 
 /**
  * Class AfterInstallAfterDelete
@@ -49,6 +51,62 @@ class AfterInstallAfterDelete
     {
     }
 
+    protected  function addMenuItems(array $menuItems): void
+    {
+        /** @var Connection $connection */
+        $connection = $this->getContainer()->get('connection');
+
+        $defaultLayout = $connection->createQueryBuilder()
+            ->select('id, navigation')
+            ->from('layout_profile')
+            ->where('is_default = :true')
+            ->andWhere('deleted = :false')
+            ->setParameter('true', true, ParameterType::BOOLEAN)
+            ->setParameter('false', false, ParameterType::BOOLEAN)
+            ->fetchAssociative();
+
+        if(empty($defaultLayout)) {
+            return;
+        }
+
+        $navigation = @json_decode($defaultLayout['navigation'], true) ?? [];
+
+        $tabList = [];
+        foreach ($navigation as $item) {
+            if(is_array($item) && !empty($item['items'])) {
+                foreach ($item['items'] as $subItem) {
+                    $tabList[] = $subItem;
+                }
+            }else{
+                $tabList[] = $item;
+            }
+        }
+
+        foreach ($menuItems as $item) {
+            if(is_array($item) && !empty($item['items'])) {
+                $subItems = [];
+                foreach ($item['items'] as $subItem) {
+                    if(!in_array($subItem, $tabList)) {
+                        $subItems[] = $subItem;
+                    }
+                }
+                if(!empty($subItems)) {
+                    $item['items'] = $subItems;
+                    $navigation[] = $item;
+                }
+            }else if(!in_array($item, $tabList)) {
+                $navigation[] = $item;
+            }
+        }
+
+        $connection->createQueryBuilder()
+            ->update('layout_profile')
+            ->set('navigation', ':navigation')
+            ->where('id =:id')
+            ->setParameter('navigation', json_encode($navigation))
+            ->setParameter('id', $defaultLayout['id'])
+            ->executeStatement();
+    }
     /**
      * @return Container
      */
