3163b89a1d81f83113630f3a52d93b19f9c344e5
Roman Ratsun <roman.ratsun@atrocore.com>
2025-02-24 13:23:44 +0000

    .

--- client/src/views/user-profile/record/detail.js
+++ client/src/views/user-profile/record/detail.js
@@ -15,8 +15,6 @@ Espo.define('views/user-profile/record/detail', 'views/record/detail', Dep => {
         setup() {
             Dep.prototype.setup.call(this);
 
-            this.dropdownItemList = [];
-
             this.buttonEditList = [
                 {
                     name: 'save',
@@ -30,7 +28,88 @@ Espo.define('views/user-profile/record/detail', 'views/record/detail', Dep => {
                     edit: true
                 }
             ];
-        }
+
+            this.buttonList = [
+                {
+                    name: 'access',
+                    label: 'Access',
+                    style: 'default'
+                }
+            ];
+
+            this.dropdownItemList = [];
+
+            if (!this.getConfig().get('resetPasswordViaEmailOnly', false)) {
+                this.dropdownItemList.push({
+                    name: 'changePassword',
+                    label: this.translate('Change Password', 'labels', 'User'),
+                    style: 'default'
+                });
+            }
+
+            this.dropdownItemList.push({
+                name: 'resetPassword',
+                label: this.translate('Reset Password', 'labels', 'User'),
+                style: 'default'
+            });
+        },
+
+        actionChangePassword() {
+            this.notify('Loading...');
+
+            this.createView('changePassword', 'views/modals/change-password', {
+                userId: this.model.id
+            }, view => {
+                view.render();
+                this.notify(false);
+
+                this.listenToOnce(view, 'changed', () => {
+                    if (this.model.id === this.getUser().id) {
+                        setTimeout(() => {
+                            this.getBaseController().logout();
+                        }, 2000);
+                    }
+                });
+
+            });
+        },
+
+        actionResetPassword() {
+            $.ajax({
+                url: 'User/action/resetPassword',
+                type: 'POST',
+                data: JSON.stringify({
+                    userId: this.model.id
+                })
+            }).done(() => {
+                Espo.Ui.success(this.translate('uniqueLinkHasBeenSent', 'messages', 'User'));
+                setTimeout(() => {
+                    if (this.model.id === this.getUser().id) {
+                        this.getBaseController().logout();
+                    }
+                }, 2000);
+            });
+        },
+
+        actionAccess() {
+            this.notify('Loading...');
+
+            $.ajax({
+                url: 'User/action/acl',
+                type: 'GET',
+                data: {
+                    id: this.model.id,
+                }
+            }).done(aclData => {
+                this.createView('access', 'views/user/modals/access', {
+                    aclData: aclData,
+                    model: this.model,
+                }, view => {
+                    this.notify(false);
+                    view.render();
+                });
+            });
+        },
 
     });
 });
