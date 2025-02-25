0c7ae87f1a61ec50374e329282080ced99fd23b9
Roman Zablodskyi <rz@atrocore.com>
2025-02-25 09:25:09 +0200

    Fixed entity field params saving

--- app/Atro/Repositories/EntityField.php
+++ app/Atro/Repositories/EntityField.php
@@ -229,7 +229,7 @@ class EntityField extends ReferenceData
             throw new BadRequest("Middle Table Name cannot be changed.");
         }
 
-        $loadedData = json_decode(json_encode($this->getMetadata()->loadData(true)), true);
+        $loadedData = json_decode(json_encode($this->getMetadata()->loadData()), true);
 
         $entity->set('isCustom', true);
         $this->updateField($entity, $loadedData);
@@ -352,7 +352,7 @@ class EntityField extends ReferenceData
                 continue;
             }
 
-            $loadedVal = $loadedData['entityDefs'][$entity->get('code')][$field] ?? null;
+            $loadedVal = $loadedData['entityDefs'][$entity->get('entityId')]['fields'][$entity->get('code')][$field] ?? null;
             if ($fieldType === 'bool') {
                 $loadedVal = !empty($loadedVal);
             }
