5bc935e4cdcc780b129080c12203cf752df15def
Kevin Teguia <kevin.teguia@atrocore.com>
2025-03-12 09:20:17 +0100

    clear silently

---
+++ client/src/views/locale/record/detail.js
@@ -0,0 +1,26 @@
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+Espo.define('views/locale/record/detail', 'views/record/detail', Dep => {
+
+    return Dep.extend({
+
+        setup() {
+            Dep.prototype.setup.call(this);
+
+            this.listenTo(this.model, 'after:save', () => {
+                setTimeout(() => {
+                    this.showReloadPageMessage()
+                }, 2000);
+            });
+        }
+    });
+});
+
