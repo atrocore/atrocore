db053602827481403de6b9f70ed0f6b11a692b10
raoul <dzoukouraoul@gmail.com>
2025-03-21 13:19:15 +0100

    .

--- app/Atro/Core/LayoutManager.php
+++ app/Atro/Core/LayoutManager.php
@@ -162,7 +162,7 @@ class LayoutManager
     }
 
 
-    public function saveUserPreference(string $scope, string $viewType, ?string $relatedScope = null, ?string $layoutProfileId = null): bool
+    public function saveUserPreference(string $scope, string $viewType, ?string $relatedEntity = null, ?string $relatedLink = null, ?string $layoutProfileId = null): bool
     {
         /* @var $repository RDB */
         $repository = $this->getEntityManager()->getRepository('UserEntityLayout');
@@ -171,7 +171,8 @@ class LayoutManager
                 'userId'        => $this->getUser()->id,
                 'entity'        => $scope,
                 'viewType'      => $viewType,
-                'relatedEntity' => empty($relatedScope) ? null : $relatedScope
+                'relatedEntity' => empty($relatedEntity) ? null : $relatedEntity,
+                'relatedLink'   => empty($relatedLink) ? null : $relatedLink
             ])
             ->findOne();
 
@@ -185,8 +186,11 @@ class LayoutManager
                 $record->set('userId', $this->getUser()->id);
                 $record->set('entity', $scope);
                 $record->set('viewType', $viewType);
-                if (!empty($relatedScope)) {
-                    $record->set('relatedEntity', $relatedScope);
+                if (!empty($relatedEntity)) {
+                    $record->set('relatedEntity', $relatedEntity);
+                }
+                if (!empty($relatedLink)) {
+                    $record->set('relatedLink', $relatedLink);
                 }
             }
             $record->set('layoutProfileId', $layoutProfileId);
