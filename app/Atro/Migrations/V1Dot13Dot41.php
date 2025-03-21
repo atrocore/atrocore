9844b87870736b936690fba42994ec4aa378ce12
raoul <dzoukouraoul@gmail.com>
2025-03-21 12:09:19 +0100

    .

---
+++ app/Atro/Migrations/V1Dot13Dot41.php
@@ -0,0 +1,136 @@
+<?php
+/*
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\Migrations;
+
+use Atro\Core\Migration\Base;
+use Atro\Core\Templates\Repositories\ReferenceData;
+use Atro\Core\Utils\Util;
+
+class V1Dot13Dot41 extends Base
+{
+    public function getMigrationDateTime(): ?\DateTime
+    {
+        return new \DateTime('2025-03-21 15:00:00');
+    }
+
+    public function up(): void
+    {
+        if ($this->isPgSQL()) {
+            $this->execute("DROP INDEX idx_layout_layout_profile;");
+            $this->execute("DROP INDEX idx_user_entity_layout_unique;");
+            $this->execute("ALTER TABLE user_entity_layout ADD hash VARCHAR(255) DEFAULT NULL;");
+        } else {
+
+        }
+
+        // calculate hash
+        foreach (['layout', 'user_entity_layout'] as $table) {
+            $limit = 2000;
+            $offset = 0;
+            while (true) {
+                $records = $this->getConnection()->createQueryBuilder()
+                    ->from($table)
+                    ->select('*')
+                    ->setMaxResults($limit)
+                    ->setFirstResult($offset)
+                    ->fetchAllAssociative();
+
+                if (empty($records)) {
+                    break;
+                }
+                $offset = $offset + $limit;
+
+                foreach ($records as $record) {
+                    $this->getConnection()->createQueryBuilder()
+                        ->update($table)
+                        ->set('hash', ':hash')
+                        ->where('id = :id')
+                        ->setParameter('id', $record['id'])
+                        ->setParameter('hash', $this->generateHash($record, $table === 'user_entity_layout'))
+                        ->executeStatement();
+                }
+            }
+
+            // remove all values with same hash
+
+            $duplicates = $this->getConnection()
+                ->createQueryBuilder()
+                ->from($table)
+                ->select("hash")
+                ->groupBy("hash")
+                ->having('count(*)>1')
+                ->fetchAllAssociative();
+
+
+            foreach ($duplicates as $duplicate) {
+                $records = $this->getConnection()
+                    ->createQueryBuilder()
+                    ->from($table)
+                    ->select("*")
+                    ->where('hash=:hash')
+                    ->setParameter("hash", $duplicate["hash"])
+                    ->fetchAllAssociative();
+
+                foreach ($records as $index => $record) {
+                    if ($index === 0) {
+                        continue;
+                    }
+                    $this->getConnection()
+                        ->createQueryBuilder()
+                        ->delete($table)
+                        ->where('id = :id')
+                        ->setParameter('id', $record['id'])
+                        ->executeStatement();
+                }
+            }
+        }
+
+        if ($this->isPgSQL()) {
+
+        } else {
+
+        }
+    }
+
+    protected function generateHash($record, $forUser = false): string
+    {
+        $fields = [
+            "layout_profile_id",
+            "entity",
+            "related_entity",
+            "related_link",
+            "view_type"
+        ];
+        if ($forUser) {
+            $fields[] = "user_id";
+        }
+        $text = join("\n", array_map(function ($field) use ($record) {
+            return empty($record[$field]) ? "" : $record[$field];
+        }, $fields));
+        return md5('atrocore_salt' . $text);
+    }
+
+    /**
+     * @param string $sql
+     */
+    protected function execute(string $sql)
+    {
+        try {
+            $this->getPDO()->exec($sql);
+        } catch (\Throwable $e) {
+            // ignore all
+        }
+    }
+
+}
