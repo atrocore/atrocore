50b481177991f857d4ab44d4f5913978bf1a4707
Roman Ratsun <roman.ratsun@atrocore.com>
2025-04-02 11:58:23 +0000

    .

--- client/src/views/record/detail.js
+++ client/src/views/record/detail.js
@@ -1951,18 +1951,21 @@ Espo.define('views/record/detail', ['views/record/base', 'view-record-helper'], 
             if (this.layoutName === 'detail' && this.getMetadata().get(`scopes.${this.model.name}.hasAttribute`) && this.getAcl().check(this.model.name, 'read')) {
                 let layoutRows = [];
                 let layoutRow = [];
-                (this.model.get('attributeValues') || []).forEach(item => {
-                    this.model.defs['fields'][item.name] = item;
-                    layoutRow.push({
-                        name: item.name,
-                        customLabel: item.label,
-                        fullWidth: ['text', 'markdown', 'wysiwyg', 'script'].includes(item.type)
-                    });
-                    if (layoutRow[0]['fullWidth'] || layoutRow[1]) {
-                        layoutRows.push(layoutRow);
-                        layoutRow = [];
-                    }
-                })
+
+                if (!this.model.isNew()) {
+                    (this.model.get('attributeValues') || []).forEach(item => {
+                        this.model.defs['fields'][item.name] = item;
+                        layoutRow.push({
+                            name: item.name,
+                            customLabel: item.label,
+                            fullWidth: ['text', 'markdown', 'wysiwyg', 'script'].includes(item.type)
+                        });
+                        if (layoutRow[0]['fullWidth'] || layoutRow[1]) {
+                            layoutRows.push(layoutRow);
+                            layoutRow = [];
+                        }
+                    })
+                }
 
                 if (layoutRow.length > 0) {
                     layoutRow.push(false);
