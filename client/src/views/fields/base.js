e9843811f68f9927c92e8342f83e4ad4282db26d
Roman Ratsun <roman.ratsun@atrocore.com>
2025-04-04 07:21:54 +0000

    .

--- client/src/views/fields/base.js
+++ client/src/views/fields/base.js
@@ -85,12 +85,8 @@ Espo.define('views/fields/base', 'view', function (Dep) {
         defaultFilterValue: null,
 
         translate: function (name, category, scope) {
-            if (category === 'fields' && scope === this.model.name) {
-                let preparedAttributeName = name.replace(/^unitAttr_/, "attr_");
-                let attributeLabel = this.model.getFieldParam(preparedAttributeName, 'label');
-                if (attributeLabel) {
-                    return attributeLabel;
-                }
+            if (category === 'fields' && scope === this.model.name && this.model.get('attributesDefs') && this.model.get('attributesDefs')[name]) {
+                return this.model.get('attributesDefs')[name].label;
             }
 
             return Dep.prototype.translate.call(this, name, category, scope);
