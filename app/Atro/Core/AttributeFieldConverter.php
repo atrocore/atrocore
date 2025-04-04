05b5725ecbc760e46fde18db0d02f56e360a87eb
Roman Ratsun <roman.ratsun@atrocore.com>
2025-04-04 07:07:16 +0000

    .

--- app/Atro/Core/AttributeFieldConverter.php
+++ app/Atro/Core/AttributeFieldConverter.php
@@ -12,6 +12,7 @@
 namespace Atro\Core;
 
 use Atro\Core\AttributeFieldTypes\AttributeFieldTypeInterface;
+use Atro\Core\Exceptions\Error;
 use Atro\Core\Utils\Config;
 use Atro\Core\Utils\Metadata;
 use Atro\Core\Utils\Util;
@@ -70,171 +71,24 @@ class AttributeFieldConverter
             $id = $row['av_id'];
             $name = "attr_{$id}";
 
-            $className = "\\Atro\\Core\\AttributeFieldTypes\\" . ucfirst($row['type']) . "Type";
-            if (!class_exists($className)) {
-                $className = "\\Atro\\Core\\AttributeFieldTypes\\VarcharType";
-            }
-
-            if (is_a($className, AttributeFieldTypeInterface::class, true)) {
-                $this->container->get($className)->convert($entity, $id, $name, $row, $attributesDefs);
-            }
-
-//            $attributeRow = [
-//                'id'                            => $id,
-//                'attributeId'                   => $row['id'],
-//                'name'                          => $name,
-//                'label'                         => $row['name'],
-//                'type'                          => $row['type'],
-//                'trim'                          => !empty($row['trim']),
-//                'required'                      => !empty($row['is_required']),
-//                'notNull'                       => !empty($row['not_null']),
-//                'useDisabledTextareaInViewMode' => !empty($row['use_disabled_textarea_in_view_mode']),
-//                'amountOfDigitsAfterComma'      => $row['amount_of_digits_after_comma'] ?? null,
-//                'prohibitedEmptyValue'          => !empty($row['prohibited_empty_value']),
-//                'extensibleEnumId'              => $row['extensible_enum_id'] ?? null
-//            ];
-//
-//            $attributeData = @json_decode($row['data'], true)['field'] ?? null;
-//
-//            if (!empty($attributeData['entityType'])) {
-//                $attributeRow['entity'] = $attributeData['entityType'];
-//            }
-//
-//            if (!empty($attributeData['maxLength'])) {
-//                $attributeRow['maxLength'] = $attributeData['maxLength'];
-//            }
-//
-//            if (!empty($attributeData['countBytesInsteadOfCharacters'])) {
-//                $attributeRow['countBytesInsteadOfCharacters'] = $attributeData['countBytesInsteadOfCharacters'];
-//            }
-//
-//            if (isset($attributeData['min'])) {
-//                $attributeRow['min'] = $attributeData['min'];
-//            }
-//
-//            if (isset($attributeData['max'])) {
-//                $attributeRow['max'] = $attributeData['max'];
-//            }
-//
-//            if (isset($row['measure_id'])) {
-//                $attributeRow['measureId'] = $row['measure_id'];
-//                $attributeRow['view'] = "views/fields/unit-{$row['type']}";
-//            }
-//
-//            $dropdownTypes = $this->metadata->get(['app', 'attributeDropdownTypes'], []);
-//            if (!empty($row['dropdown']) && isset($dropdownTypes[$item['type']])) {
-//                $attributeRow['view'] = $dropdownTypes[$row['type']];
-//            }
-//
-//            if ($row['type'] === 'rangeInt') {
-//                $attributeRow['view'] = 'views/fields/range-int';
-//            }
-//
-//            if ($row['type'] === 'rangeFloat') {
-//                $attributeRow['view'] = 'views/fields/range-float';
-//            }
-//
-//            $entity->set('attributeValues', $attributeValues);
-
-//            switch ($row['type']) {
-////                case 'rangeInt':
-////                    $entity->fields[$name . 'From'] = [
-////                        'type'             => 'int',
-////                        'name'             => $name,
-////                        'attributeValueId' => $id,
-////                        'attributeId'      => $row['id'],
-////                        'attributeName'    => $row['name'],
-////                        'attributeType'    => $row['type'],
-////                        'column'           => 'int_value',
-////                        'required'         => !empty($row['is_required'])
-////                    ];
-////                    $entity->set($name . 'From', $row[$entity->fields[$name . 'From']['column']] ?? null);
-////
-////                    $entity->fields[$name . 'To'] = [
-////                        'type'             => 'int',
-////                        'name'             => $name,
-////                        'attributeValueId' => $id,
-////                        'attributeId'      => $row['id'],
-////                        'attributeName'    => $row['name'],
-////                        'attributeType'    => $row['type'],
-////                        'column'           => 'int_value1',
-////                        'required'         => !empty($row['is_required'])
-////                    ];
-////                    $entity->set($name . 'To', $row[$entity->fields[$name . 'To']['column']] ?? null);
-////
-////                    $entity->fields[$name . 'UnitId'] = [
-////                        'type'             => 'varchar',
-////                        'name'             => $name,
-////                        'attributeValueId' => $id,
-////                        'attributeId'      => $row['id'],
-////                        'attributeName'    => $row['name'],
-////                        'attributeType'    => $row['type'],
-////                        'column'           => 'reference_value',
-////                        'required'         => !empty($row['is_required'])
-////                    ];
-////                    $entity->set($name . 'UnitId', $row[$entity->fields[$name . 'UnitId']['column']] ?? null);
-////
-////                    $entity->fields[$name . 'UnitName'] = [
-////                        'type'        => 'varchar',
-////                        'notStorable' => true
-////                    ];
-////
-////                    if (isset($row['measure_id'])) {
-////                        $entity->entityDefs['fields'][$name]['measureId'] = $row['measure_id'];
-////                        $entity->entityDefs['fields'][$name]['view'] = "views/fields/unit-int'";
-////                        $entity->entityDefs['fields'][$name . 'Unit'] = [
-////                            "type"        => "link",
-////                            'label'       => "{$row['name']} (Unit)",
-////                            "view"        => "views/fields/unit-link",
-////                            "measureId"   => $row['measure_id'],
-////                            "entity"      => 'Unit',
-////                            "unitIdField" => true,
-////                            "mainField"   => $name,
-////                            'required'    => !empty($row['is_required'])
-////                        ];
-////                    }
-////                    break;
-////                case 'rangeFloat':
-////                    $entity->fields[$name . 'From'] = [
-////                        'type'             => 'float',
-////                        'name'             => $name,
-////                        'attributeValueId' => $id,
-////                        'attributeId'      => $row['id'],
-////                        'attributeName'    => $row['name'],
-////                        'attributeType'    => $row['type'],
-////                        'column'           => 'float_value',
-////                        'required'         => !empty($row['is_required'])
-////                    ];
-////                    $entity->set($name . 'From', $row[$entity->fields[$name . 'From']['column']] ?? null);
-////
-////                    $entity->fields[$name . 'To'] = [
-////                        'type'             => 'float',
-////                        'name'             => $name,
-////                        'attributeValueId' => $id,
-////                        'attributeId'      => $row['id'],
-////                        'attributeName'    => $row['name'],
-////                        'attributeType'    => $row['type'],
-////                        'column'           => 'float_value1',
-////                        'required'         => !empty($row['is_required'])
-////                    ];
-////                    $entity->set($name . 'To', $row[$entity->fields[$name . 'To']['column']] ?? null);
-////
-////                    $entity->fields[$name . 'UnitId'] = [
-////                        'type'             => 'varchar',
-////                        'name'             => $name,
-////                        'attributeValueId' => $id,
-////                        'attributeId'      => $row['id'],
-////                        'attributeName'    => $row['name'],
-////                        'attributeType'    => $row['type'],
-////                        'column'           => 'reference_value',
-////                        'required'         => !empty($row['is_required'])
-////                    ];
-////                    $entity->set($name . 'UnitId', $row[$entity->fields[$name . 'UnitId']['column']] ?? null);
-////                    break;
-//            }
+            $this->getFieldType($row['type'])->convert($entity, $id, $name, $row, $attributesDefs);
         }
 
         $entity->set('attributesDefs', $attributesDefs);
         $entity->setAsFetched();
+    }
+
+    protected function getFieldType(string $type): AttributeFieldTypeInterface
+    {
+        $className = "\\Atro\\Core\\AttributeFieldTypes\\" . ucfirst($type) . "Type";
+        if (!class_exists($className)) {
+            $className = "\\Atro\\Core\\AttributeFieldTypes\\VarcharType";
+        }
+
+        if (!is_a($className, AttributeFieldTypeInterface::class, true)) {
+            throw new Error("No such attribute field type '$type'.");
+        }
+
+        return $this->container->get($className);
     }
 }
---
+++ app/Atro/Core/AttributeFieldTypes/RangeFloatType.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Atro\Core\AttributeFieldTypes;
+
+use Atro\Core\Container;
+use Atro\Core\Utils\Language;
+use Espo\ORM\IEntity;
+
+class RangeFloatType extends RangeIntType
+{
+    protected string $type = 'float';
+}
