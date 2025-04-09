83349993809534f2c5f5487c685f51022acda31c
raoul <dzoukouraoul@gmail.com>
2025-04-09 16:21:37 +0100

    .

--- client/src/views/record/list.js
+++ client/src/views/record/list.js
@@ -2073,7 +2073,7 @@ Espo.define('views/record/list', 'view', function (Dep) {
 
                 var item = {
                     name: this.listLayout[i].name,
-                    sortable: !(this.listLayout[i].notSortable || false),
+                    sortable: !(this.listLayout[i].notSortable || false) && !this.options.disableSorting,
                     width: width,
                     align: ('align' in this.listLayout[i]) ? this.listLayout[i].align : false,
                 };
