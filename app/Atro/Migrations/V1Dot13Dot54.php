370f3f5cf7bc7b96f19495db3da16141b4eac469
raoul <dzoukouraoul@gmail.com>
2025-04-22 16:34:29 +0100

    .

--- app/Atro/Migrations/V1Dot13Dot54.php
+++ app/Atro/Migrations/V1Dot13Dot54.php
@@ -17,6 +17,11 @@ use Atro\Core\Migration\Base;
 
 class V1Dot13Dot54 extends Base
 {
+    public function getMigrationDateTime(): ?\DateTime
+    {
+        return new \DateTime('2025-04-22 17:00:00');
+    }
+
     public function up(): void
     {
         if ($this->isPgSQL()) {
