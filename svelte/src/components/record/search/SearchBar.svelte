c2facd42db9cdeda1e2bba05829719ec4ac8c3b0
Kevin Teguia <kevin.teguia@atrocore.com>
2025-04-22 12:49:05 +0100

    update search position

--- svelte/src/components/record/search/SearchBar.svelte
+++ svelte/src/components/record/search/SearchBar.svelte
@@ -99,10 +99,6 @@
         width: 250px;
     }
 
-    .search-container .search input {
-        width: 150px;
-    }
-
     .search-row .input-group {
         border: 1px solid #eee;
         border-radius: 5px;
