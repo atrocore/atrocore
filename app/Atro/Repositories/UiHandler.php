330bab32ce37b914eb0b84719c798bfa76176317
Kevin Teguia <kevin.teguia@atrocore.com>
2025-05-12 23:13:49 +0100

    .

--- app/Atro/Repositories/UiHandler.php
+++ app/Atro/Repositories/UiHandler.php
@@ -19,9 +19,103 @@ use Atro\Core\DataManager;
 use Atro\Core\Exceptions\BadRequest;
 use Atro\Core\Utils\Language;
 use Espo\ORM\Entity;
+use Espo\ORM\EntityCollection;
 
 class UiHandler extends ReferenceData
 {
+    public function find(array $params = [])
+    {
+        $items = $this->getAllItems($params);
+        $items = array_values($items);
+
+        // filter by name
+        foreach ($params['whereClause'] ?? [] as $row) {
+            if (!empty($row['name*'])) {
+                $search = str_replace('%', '', $row['name*']);
+                $items = array_filter($items, function ($item) use ($search) {
+                    return isset($item['name']) && preg_match('/^' . preg_quote($search, '/') . '/i', $item['name']);
+                });
+            }
+        }
+
+        // text filter
+        if (!empty($params['whereClause'][0]['OR'])) {
+            $filtered = [];
+            $optionNames = [];
+            foreach ($params['whereClause'][0]['OR'] as $k => $v) {
+                $field = str_replace('*', '', $k);
+                $search = str_replace('%', '', $v);
+
+                foreach ($items as $item) {
+                    if ($field === 'fields') {
+                        $value = is_string($item[$field]) ? @json_decode($item[$field], true) : $item[$field];
+                        if (!isset($filtered[$item['code']]) && is_array($value) && !empty($value)) {
+                            $targetFieldsTranslated = array_map(
+                                fn($field) => $this->translate($field, 'fields', $item['entityType'] ?? ''),
+                                $value
+                            );
+                            if (strpos(join(', ', $targetFieldsTranslated), $search) !== false) {
+                                $filtered[$item['code']] = $item;
+                            }
+                        }
+                    }else if ($field === 'type') {
+                        if (!isset($filtered[$item['code']]) && is_string($item[$field])) {
+                            if(!empty($optionNames[$item[$field]])) {
+                                $optionName = $optionNames[$item[$field]];
+                            }else{
+                                foreach ($this->getMetadata()->get(['app', 'extensibleEnumOptions'], []) as $option) {
+                                    $optionName = null;
+                                    if(!empty($option['id']) && $option['id'] === $item[$field]) {
+                                        $optionNames[$option['id']] = $optionName = $option['name'];
+                                        break;
+                                    }
+                                }
+                            }
+
+                            if (!empty($optionName) && strpos($optionName, $search) !== false) {
+                                $filtered[$item['code']] = $item;
+                            }
+                        }
+                    }else if (!isset($filtered[$item['code']]) && is_string($item[$field]) && strpos($item[$field], $search) !== false) {
+                        $filtered[$item['code']] = $item;
+                    }
+                }
+            }
+
+            $items = array_values($filtered);
+        }
+
+
+
+        // sort data
+        if (!empty($params['orderBy'])) {
+            usort($items, function ($a, $b) use ($params) {
+                $field = $params['orderBy'];
+                if (strtolower($params['order']) === 'desc') {
+                    return $b[$field] <=> $a[$field];
+                } else {
+                    return $a[$field] <=> $b[$field];
+                }
+            });
+        }
+
+        // limit data
+        if (isset($params['limit']) && isset($params['offset'])) {
+            $prepared = [];
+            foreach ($items as $k => $item) {
+                if ($k >= $params['offset'] && count($prepared) < $params['limit']) {
+                    $prepared[] = $item;
+                }
+            }
+            $items = $prepared;
+        }
+
+        $collection = new EntityCollection($items, $this->entityName, $this->entityFactory);
+        $collection->setAsFetched();
+
+        return $collection;
+    }
+
     protected function beforeSave(Entity $entity, array $options = [])
     {
         if (!$entity->isNew()) {
