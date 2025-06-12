0715d4732ea1f3430e453fcac00857f248cb0220
Roman Ratsun <roman.ratsun@atrocore.com>
2025-06-12 11:36:34 +0000

    .

---
+++ client/src/views/role-scope/fields/edit-action.js
@@ -0,0 +1,36 @@
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+Espo.define('views/role-scope/fields/edit-action', 'views/fields/enum', Dep => {
+
+    return Dep.extend({
+
+        setup() {
+            Dep.prototype.setup.call(this);
+
+            this.listenTo(this.model, 'change:name', () => {
+                this.reRender();
+            });
+        },
+
+        afterRender() {
+            Dep.prototype.afterRender.call(this);
+
+            const aclActionList = this.getMetadata().get(`scopes.${this.model.get('name')}.aclActionList`) || ['edit'];
+
+            this.$el.parent().hide();
+            if (aclActionList.includes('edit')) {
+                this.$el.parent().show();
+            }
+        },
+
+    });
+});
+
