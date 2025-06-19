ead7ee684380425965b3092e5e750a68bcfb6504
Kevin Teguia <kevin.teguia@atrocore.com>
2025-06-19 08:45:59 +0100

    avoid case entity does not exit anymore

--- app/Atro/Repositories/SavedSearch.php
+++ app/Atro/Repositories/SavedSearch.php
@@ -29,6 +29,9 @@ class SavedSearch extends Base
         if ($cachedData === null) {
             $cachedData = [];
             foreach ($this->find() as $entity) {
+                if(!$this->getMetadata()->get(['scopes', $entity->get('entityType')])) {
+                    continue;
+                }
                 $this->cleanDeletedFieldsFromFilterData($entity);
                 if(!empty($entity->get('data'))) {
                     $cachedData[] = $entity->toArray();
