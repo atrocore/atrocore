e0c2dda0e56b30e8550adc9a9a3bbe2a5aec10fe
Roman Ratsun <roman.ratsun@atrocore.com>
2025-07-15 06:36:02 +0000

    .

--- client/modules/treo-core/src/views/composer/record/row-actions/store.js
+++ client/modules/treo-core/src/views/composer/record/row-actions/store.js
@@ -11,6 +11,25 @@
 Espo.define('treo-core:views/composer/record/row-actions/store', 'views/record/row-actions/default',
     Dep => Dep.extend({
 
+        setup() {
+            Dep.prototype.setup.call(this);
+
+            const locales = this.getConfig().get('locales');
+            let localeId = this.getUser()?.user?.localeId;
+            if (!localeId || !locales[localeId]) {
+                localeId = this.getConfig().get('locale')
+                if (!locales[localeId]) {
+                    localeId = 'main'
+                }
+            }
+
+            const locale = locales[localeId]
+            if (locale) {
+                this.thousandSeparator = locale['thousandSeparator'] || '';
+                this.decimalMark = locale['decimalMark']
+            }
+        },
+
         afterRender() {
             if (this.model.get('inStoreLink')) {
                 this.model.set('status', 'buyable');
@@ -20,8 +39,8 @@ Espo.define('treo-core:views/composer/record/row-actions/store', 'views/record/r
                 let storeLink = this.model.get('inStoreLink') || 'https://store.atrocore.com';
                 storeLink += '?instanceId=' + this.getConfig().get('appId');
 
-                const titleStr = this.model.get('purchasePrice') ? `${this.translate('Purchase price:')} €${this.model.get('purchasePrice')}` : this.translate('Сlick to purchase!');
-                const rentalPriceStr = this.model.get('rentalPrice') ? `€${this.model.get('rentalPrice')}/mo` : this.translate('Purchase');
+                const titleStr = this.model.get('purchasePrice') ? `${this.translate('Purchase price:')} €${this.formatNumber(this.model.get('purchasePrice'))}` : this.translate('Click to purchase!');
+                const rentalPriceStr = this.model.get('rentalPrice') ? `€${this.formatNumber(this.model.get('rentalPrice'))}/mo` : this.translate('Purchase');
 
                 this.$el.html(`<a role="button" href="${storeLink}" target="_blank" title="${titleStr}" class="btn btn-sm btn-default" style="padding:4px 8px;border-radius:4px"><i class="ph ph-wallet"></i> <span>${rentalPriceStr}</span></a>`);
             } else if (this.model.get('status') === 'available') {
@@ -33,6 +52,16 @@ Espo.define('treo-core:views/composer/record/row-actions/store', 'views/record/r
 
         getActionList() {
             return [];
+        },
+
+        formatNumber(value) {
+            let parts = value.toString().split(".");
+
+            if (this.mode !== 'edit') {
+                parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, this.thousandSeparator);
+            }
+
+            return parts.join(this.decimalMark);
         },
 
     })
