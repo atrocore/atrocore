72148162c5bdda9b996bb01907c0f08b1565f4e1
Roman Ratsun <roman.ratsun@atrocore.com>
2025-07-16 13:48:32 +0000

    .

---
+++ app/Atro/Migrations/V2Dot0Dot13.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\Migrations;
+
+use Atro\Core\Migration\Base;
+
+class V2Dot0Dot13 extends Base
+{
+    public function getMigrationDateTime(): ?\DateTime
+    {
+        return new \DateTime('2025-07-17 15:00:00');
+    }
+
+    public function up(): void
+    {
+        // create attribute_panel
+    }
+
+    protected function exec(string $sql): void
+    {
+        try {
+            $this->getPDO()->exec($sql);
+        } catch (\Throwable $e) {
+        }
+    }
+}
