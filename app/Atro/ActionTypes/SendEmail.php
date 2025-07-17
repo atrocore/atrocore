3fedec09b950d2c7e690a8bd95d71ed920113792
Kevin Teguia <kevin.teguia@atrocore.com>
2025-07-17 12:21:49 +0100

    fix licence

--- app/Atro/ActionTypes/SendEmail.php
+++ app/Atro/ActionTypes/SendEmail.php
@@ -1,21 +1,12 @@
 <?php
-/*
- * This file is part of premium software, which is NOT free.
- * Copyright (c) AtroCore GmbH.
+/**
+ * AtroCore Software
  *
- * This Software is the property of AtroCore GmbH and is
- * protected by copyright law - it is NOT Freeware and can be used only in one
- * project under a proprietary license, which is delivered along with this program.
- * If not, see <https://atropim.com/eula> or <https://atrodam.com/eula>.
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
  *
- * This Software is distributed as is, with LIMITED WARRANTY AND LIABILITY.
- * Any unauthorised use of this Software without a valid license is
- * a violation of the License Agreement.
- *
- * According to the terms of the license you shall not resell, sublicense,
- * rent, lease, distribute or otherwise transfer rights or usage of this
- * Software or its derivatives. You may modify the code of this Software
- * for your own needs, if source code is provided.
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
  */
 
 declare(strict_types=1);
