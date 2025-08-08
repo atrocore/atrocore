62afcabae7a22a0564d24145d6c1e6dc3c1628ac
yurdm <yd@atrocore.com>
2025-08-08 13:32:46 +0300

    .

--- client/src/views/fields/bool.js
+++ client/src/views/fields/bool.js
@@ -30,7 +30,7 @@
  * and "AtroCore" word.
  */
 
-Espo.define('views/fields/bool', 'views/fields/base', function (Dep) {
+Espo.define('views/fields/bool', ['views/fields/base', 'lib!Selectize'], function (Dep) {
 
     return Dep.extend({
 
