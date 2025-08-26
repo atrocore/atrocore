396dc8df94eb454ee40313ccb30aace7559e1c4c
Kevin Teguia <kevin.teguia@atrocore.com>
2025-08-26 15:36:59 +0100

    .

--- client/src/views/action/record/panels/entity-filter-result.js
+++ client/src/views/action/record/panels/entity-filter-result.js
@@ -17,7 +17,7 @@
  * for your own needs, if source code is provided.
  */
 
-Espo.define('workflows:views/action/record/panels/entity-filter-result', ['views/record/panels/relationship','views/search/search-filter-opener'],
+Espo.define('views/action/record/panels/entity-filter-result', ['views/record/panels/relationship','views/search/search-filter-opener'],
     (Dep, SearchFilterOpener) => Dep.extend({
 
         rowActionsView: 'views/record/row-actions/relationship-view-only',
