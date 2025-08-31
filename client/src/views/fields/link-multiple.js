abf8c06c7a79a5217fe0798103c4127aa86194de
Kevin Teguia <kevin.teguia@atrocore.com>
2025-08-31 11:58:10 +0100

    .

--- client/src/views/fields/link-multiple.js
+++ client/src/views/fields/link-multiple.js
@@ -833,7 +833,7 @@ Espo.define('views/fields/link-multiple', ['views/fields/base', 'views/fields/co
                             rule.$el.find(`input[name="${inputName}"]`).trigger('change');
                         });
 
-                        this.listenTo(view, 'clear-subquery', () => {g
+                        this.listenTo(view, 'clear-subquery', () => {
                             this.filterValue = rule.value;
                             if (rule.value !== null && rule.length === 0) {
                                 this.filterValue = null;
