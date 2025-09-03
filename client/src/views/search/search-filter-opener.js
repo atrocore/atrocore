66322ef69d020eaafe421a602705ea9f0c66401d
Kevin Teguia <kevin.teguia@atrocore.com>
2025-09-03 10:34:37 +0100

    .

--- client/src/views/search/search-filter-opener.js
+++ client/src/views/search/search-filter-opener.js
@@ -144,7 +144,7 @@ Espo.define('views/search/search-filter-opener', ['view', 'search-manager'], fun
             this.createView('dialog', 'views/search/modals/select-filter-search', {
                 scope: foreignScope,
                 filters: filters,
-                disabledUnsetSearch: searchManager.isFilterSet(),
+                disabledUnsetSearch: !searchManager.isFilterSet(),
                 additionalBoolFilterList: additionalBoolFilterList,
                 boolFilterData: boolFilterData,
             }, (dialog) => {
