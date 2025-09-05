d069d7cc71cfeab4f0d76015ba5d0598bbe6130d
Roman Ratsun <roman.ratsun@atrocore.com>
2025-09-05 11:42:16 +0000

    .

--- client/src/model.js
+++ client/src/model.js
@@ -306,7 +306,7 @@ Espo.define('model', [], function () {
 
         fetchDynamicActions: function (type, display) {
             this.lastDynamicActionXhr = $.ajax({
-                url: 'Action/action/dynamicActions?scope=' + this.name + 'type=' + type + (this.id ? '&id=' + this.id : '') + (display ? `&display=${display}` : ''),
+                url: 'Action/action/dynamicActions?scope=' + this.name + '&type=' + type + (this.id ? '&id=' + this.id : '') + (display ? `&display=${display}` : ''),
                 type: 'GET'
             })
                 .then(resp => {
