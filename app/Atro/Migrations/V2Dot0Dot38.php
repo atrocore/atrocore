0b7398a5fe079862cf40aedcd3b574033ca4267d
Roman Zablodskyi <rz@atrocore.com>
2025-09-09 17:41:49 +0300

    .

--- app/Atro/Migrations/V2Dot0Dot38.php
+++ app/Atro/Migrations/V2Dot0Dot38.php
@@ -19,7 +19,7 @@ class V2Dot0Dot38 extends Base
 {
     public function getMigrationDateTime(): ?\DateTime
     {
-        return new \DateTime('2025-09-05 14:00:00');
+        return new \DateTime('2025-09-05 09:00:00');
     }
 
     public function up(): void
