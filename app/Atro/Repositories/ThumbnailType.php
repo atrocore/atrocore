92b5f8b47ebe29e07a6feae0ec180aa2795e9ddb
Roman Ratsun <roman.ratsun@atrocore.com>
2025-09-22 12:52:43 +0000

    .

--- app/Atro/Repositories/ThumbnailType.php
+++ app/Atro/Repositories/ThumbnailType.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Atro\Repositories;
 
+use Atro\Core\Exceptions\BadRequest;
 use Atro\Core\Templates\Repositories\ReferenceData;
 use Espo\ORM\Entity;
 
@@ -26,7 +27,7 @@ class ThumbnailType extends ReferenceData
             $items[] = [
                 "id"     => $type,
                 "code"   => $type,
-                "name"   => $this->getLanguage()->translateOption($type, 'previewSize', 'EntityField'),
+                "name"   => $this->translate($type, 'thumbnailTypes'),
                 "width"  => $data["size"][0] ?? null,
                 "height" => $data["size"][1] ?? null,
             ];
@@ -37,16 +38,76 @@ class ThumbnailType extends ReferenceData
 
     public function insertEntity(Entity $entity): bool
     {
+        if (!preg_match('/^[A-Za-z0-9]*$/', $entity->get('code'))) {
+            throw new BadRequest("Code is invalid.");
+        }
+
+        if ($this->getMetadata()->get("app.thumbnailTypes.{$entity->get('code')}")) {
+            throw new BadRequest("Thumbnail type '{$entity->get('code')}' is already exists.");
+        }
+
+        $entity->id = $entity->get('code');
+
+        $this->getMetadata()->set('app', 'thumbnailTypes', [
+            $entity->get('code') => [
+                'size' => [
+                    $entity->get('width'),
+                    $entity->get('height'),
+                ],
+            ],
+        ]);
+
+        $this->getMetadata()->save();
+
+        $this->getLanguage()->set('Global', 'thumbnailTypes', $entity->get('code'), $entity->get('name'));
+        $this->getLanguage()->save();
+
+        if ($this->getLanguage()->getLanguage() !== $this->getBaseLanguage()->getLanguage()) {
+            $this->getBaseLanguage()->set('Global', 'thumbnailTypes', $entity->get('code'), $entity->get('name'));
+            $this->getBaseLanguage()->save();
+        }
+
         return true;
     }
 
     public function updateEntity(Entity $entity): bool
     {
+        $this->getMetadata()->set('app', 'thumbnailTypes', [
+            $entity->get('code') => [
+                'size' => [
+                    $entity->get('width'),
+                    $entity->get('height'),
+                ],
+            ],
+        ]);
+
+        $this->getMetadata()->save();
+
+        if ($entity->isAttributeChanged('name')) {
+            $this->getLanguage()->set('Global', 'thumbnailTypes', $entity->get('code'), $entity->get('name'));
+            $this->getLanguage()->save();
+        }
+
         return true;
     }
 
     public function deleteEntity(Entity $entity): bool
     {
+        $this->getMetadata()->delete('app', 'thumbnailTypes', [$entity->get('code')]);
+        $this->getMetadata()->save();
+
         return true;
+    }
+
+    protected function init()
+    {
+        parent::init();
+
+        $this->addDependency('baseLanguage');
+    }
+
+    protected function getBaseLanguage(): \Atro\Core\Utils\Language
+    {
+        return $this->getInjection('baseLanguage');
     }
 }
