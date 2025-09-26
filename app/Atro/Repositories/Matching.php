32f4098e3972be729e2a6565f7518b7bae676051
Roman Ratsun <roman.ratsun@atrocore.com>
2025-09-26 14:49:30 +0000

    .

---
+++ app/Atro/Repositories/Matching.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\Repositories;
+
+use Atro\Core\Templates\Repositories\ReferenceData;
+use Espo\ORM\Entity as OrmEntity;
+use Espo\ORM\EntityCollection;
+
+class Matching extends ReferenceData
+{
+    public function findRelated(OrmEntity $entity, string $link, array $selectParams): EntityCollection
+    {
+        if ($link === 'matchingRules') {
+            $selectParams['whereClause'] = [['matchingId=' => $entity->get('id')]];
+
+            return $this->getEntityManager()->getRepository('MatchingRule')->find($selectParams);
+        }
+
+        return parent::findRelated($entity, $link, $selectParams);
+    }
+
+    public function countRelated(OrmEntity $entity, string $relationName, array $params = []): int
+    {
+        if ($relationName === 'matchingRules') {
+            $params['offset'] = 0;
+            $params['limit'] = \PHP_INT_MAX;
+
+            return count($this->findRelated($entity, $relationName, $params));
+        }
+
+        return parent::countRelated($entity, $relationName, $params);
+    }
+}
