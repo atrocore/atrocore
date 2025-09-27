971021922bc27ffa90d7a53825be0c32bcd3d0d0
Roman Zablodskyi <rz@atrocore.com>
2025-09-27 20:49:50 +0300

    Added validation for 'Has Associate' entity value

---
+++ client/src/views/entity/fields/has-associate.js
@@ -0,0 +1,24 @@
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+Espo.define('views/entity/fields/has-associate', 'views/fields/bool',
+    Dep => Dep.extend({
+
+        afterRender() {
+            Dep.prototype.afterRender.call(this);
+
+            this.listenTo(this.model, 'change:type', () => {
+                if (!['Base', 'Hierarchy'].includes(this.model.get('type'))) {
+                    this.model.set(this.name, false);
+                }
+            });
+        }
+    })
+);
