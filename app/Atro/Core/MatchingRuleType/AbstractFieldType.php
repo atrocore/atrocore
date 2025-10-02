ee0492a11bea2650da4247da49d7a8d37bb6a315
Roman Ratsun <roman.ratsun@atrocore.com>
2025-10-02 12:45:20 +0000

    .

---
+++ app/Atro/Core/MatchingRuleType/AbstractFieldType.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Atro\Core\MatchingRuleType;
+
+use Atro\Core\Container;
+
+class Like implements MatchingRuleTypeInterface
+{
+    private Container $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+}
