e65c23d734f2aa0c3c441d33903827c161974173
Roman Ratsun <roman.ratsun@atrocore.com>
2025-10-20 13:56:04 +0000

    .

---
+++ app/Atro/Repositories/AuthLogRecord.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Atro\Repositories;
+
+use Atro\Core\Templates\Repositories\Log;
+
+class AuthLogRecord extends Log
+{
+    protected $processFieldsAfterSaveDisabled = true;
+
+    protected $processFieldsBeforeSaveDisabled = true;
+
+    protected $processFieldsAfterRemoveDisabled = true;
+
+    protected bool $cacheable = false;
+}
