fdb9f15880a06d05c938d2766559783c6c1dd245
Roman Ratsun <roman.ratsun@atrocore.com>
2025-10-21 07:55:34 +0000

    .

--- app/Atro/Core/Templates/Repositories/Log.php
+++ app/Atro/Core/Templates/Repositories/Log.php
@@ -14,6 +14,58 @@ declare(strict_types=1);
 
 namespace Atro\Core\Templates\Repositories;
 
+use Atro\Services\Record;
+
 class Log extends Base
 {
+    public function hasDeletedRecordsToClear(): bool
+    {
+        if (empty($this->seed)) {
+            return false;
+        }
+
+        if (!empty($this->getMetadata()->get(['scopes', $this->entityName, 'autoDeleteAfterDays']))) {
+            return true;
+        }
+
+        return parent::hasDeletedRecordsToClear();
+    }
+
+    public function clearDeletedRecords(): void
+    {
+        if (empty($this->seed)) {
+            return;
+        }
+
+        $autoDeleteAfterDays = $this->getMetadata()->get(['scopes', $this->entityName, 'autoDeleteAfterDays']);
+
+        if (!empty($autoDeleteAfterDays) && $autoDeleteAfterDays > 0) {
+            $date = (new \DateTime())->modify("-$autoDeleteAfterDays days");
+
+            // delete using massActions
+            /** @var Record $service */
+            $service = $this->getEntityManager()->getContainer()->get('serviceFactory')->create($this->entityName);
+            $where = [];
+
+            if ($this->seed->hasField('modifiedAt')) {
+                $where[] = [
+                    'attribute' => 'modifiedAt',
+                    'type'      => 'before',
+                    'value'     => $date->format('Y-m-d H:i:s'),
+                ];
+            } elseif ($this->seed->hasField('createdAt')) {
+                $where[] = [
+                    'attribute' => 'createdAt',
+                    'type'      => 'before',
+                    'value'     => $date->format('Y-m-d H:i:s'),
+                ];
+            }
+
+            if (!empty($where)) {
+                $service->massRemove(['where' => $where]);
+            }
+        }
+
+        parent::clearDeletedRecords();
+    }
 }
