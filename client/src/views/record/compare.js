be49e88a800469eeec3ab0072c6b6f78334783c6
Kevin Teguia <kevin.teguia@atrocore.com>
2025-11-04 15:39:46 +0100

    .

--- client/src/views/record/compare.js
+++ client/src/views/record/compare.js
@@ -1024,7 +1024,7 @@ Espo.define('views/record/compare', 'view', function (Dep) {
         loadModels(selectionId) {
             let models = [];
             return new Promise((initialResolve, reject) => {
-                this.ajaxGetRequest(`selection/${selectionId}/selectionRecords?select=name,entityType,entityId,entity&collectionOnly=true&sortBy=id&asc=false`, {async: false})
+                this.ajaxGetRequest(`selection/${selectionId}/selectionRecords?select=name,entityType,entityId,entity&collectionOnly=true&sortBy=id&asc=true`, {async: false})
                     .then(result => {
                         let entityByScope = {};
                         let order = 0;
