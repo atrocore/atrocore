1271a5a02a2d00717ab17ddf8a34a7ad3bf93e8f
Roman Ratsun <roman.ratsun@atrocore.com>
2025-11-07 09:29:58 +0000

    .

--- app/Atro/Migrations/V2Dot1Dot25.php
+++ app/Atro/Migrations/V2Dot1Dot25.php
@@ -12,7 +12,9 @@
 namespace Atro\Migrations;
 
 use Atro\Core\Migration\Base;
+use Atro\Core\Utils\Metadata;
 use Atro\Core\Utils\Util;
+use Espo\ORM\EntityManager;
 
 class V2Dot1Dot25 extends Base
 {
@@ -23,7 +25,13 @@ class V2Dot1Dot25 extends Base
 
     public function up(): void
     {
-        $metadata = (new \Atro\Core\Application())->getContainer()->get('metadata');
+        $container = (new \Atro\Core\Application())->getContainer();
+
+        /** @var EntityManager $em */
+        $em = $container->get('entityManager');
+
+        /** @var Metadata $metadata */
+        $metadata = $container->get('metadata');
 
         foreach ($metadata->get('scopes') ?? [] as $scope => $defs) {
             if (empty($defs['type'])) {
@@ -37,6 +45,25 @@ class V2Dot1Dot25 extends Base
                     $this->exec("COMMENT ON COLUMN $tableName.routes IS '(DC2Type:jsonArray)'");
                 } else {
                     $this->exec("ALTER TABLE $tableName ADD routes LONGTEXT DEFAULT NULL COMMENT '(DC2Type:jsonArray)'");
+                }
+
+                while (true) {
+                    $res = $this->getConnection()->createQueryBuilder()
+                        ->select('t.*')
+                        ->from($tableName, 't')
+                        ->leftJoin('t', Util::toUnderScore(lcfirst($scope)).'_hierarchy', 'h', 't.id=h.entity_id')
+                        ->where('h.id IS NULL AND t.routes IS NULL')
+                        ->setFirstResult(0)
+                        ->setMaxResults(20000)
+                        ->fetchAllAssociative();
+
+                    if (empty($res)) {
+                        break;
+                    }
+
+                    foreach ($res as $row) {
+                        $em->getRepository($scope)->buildRoutes($row['id']);
+                    }
                 }
             }
         }
