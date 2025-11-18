7301667491d4516d7b6391fe373ad8121cada5ec
Roman Ratsun <roman.ratsun@atrocore.com>
2025-11-18 10:09:26 +0000

    .

---
+++ app/Atro/Migrations/V2Dot1Dot28.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Atro\Migrations;
+
+use Atro\Core\Migration\Base;
+use Atro\Core\Utils\Metadata;
+use Atro\Core\Utils\Util;
+use Espo\ORM\EntityManager;
+
+class V2Dot1Dot28 extends Base
+{
+    public function getMigrationDateTime(): ?\DateTime
+    {
+        return new \DateTime('2025-11-18 18:00:00');
+    }
+
+    public function up(): void
+    {
+        if ($this->isPgSQL()) {
+            $this->exec("ALTER TABLE matched_record ADD manually_added BOOLEAN DEFAULT 'false' NOT NULL");
+            $this->exec("ALTER TABLE matched_record ADD golden_record BOOLEAN DEFAULT 'false' NOT NULL");
+        } else {
+            $this->exec("ALTER TABLE matched_record ADD manually_added TINYINT(1) DEFAULT '0' NOT NULL");
+            $this->exec("ALTER TABLE matched_record ADD golden_record TINYINT(1) DEFAULT '0' NOT NULL");
+        }
+    }
+
+    protected function exec(string $sql): void
+    {
+        try {
+            $this->getPDO()->exec($sql);
+        } catch (\Throwable $e) {
+        }
+    }
+}
