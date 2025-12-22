96f29283405d53c78b6b8e8cd5059b5de601e4b8
Roman Zablodskyi <rz@atrocore.com>
2025-12-22 16:22:25 +0200

    Changed Entity duplication

--- app/Atro/Services/Entity.php
+++ app/Atro/Services/Entity.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Atro\Services;
 
+use Atro\Core\DataManager;
 use Atro\Core\Exceptions\Error;
 use Atro\Core\Templates\Services\ReferenceData;
 use Espo\ORM\Entity as OrmEntity;
@@ -68,6 +69,80 @@ class Entity extends ReferenceData
 
     }
 
+    protected function duplicateFields($entity, $duplicatingEntity): void
+    {
+        $fieldsDefs = $linksDefs = [];
+
+        $needToUpdate = false;
+
+        foreach ($this->getMetadata()->get(['entityDefs', $duplicatingEntity->get('code'), 'fields'], []) as $field => $defs) {
+            if ($field == 'id' || empty($defs['isCustom'])) {
+                continue;
+            }
+
+            $fieldsDefs[$field] = $defs;
+
+            if (in_array($defs['type'], ['link', 'linkMultiple'])) {
+                $linkDefs = $this->getMetadata()->get(['entityDefs', $duplicatingEntity->get('code'), 'links', $field], []);
+
+                if (!empty($linkDefs['foreign']) && !empty($linkDefs['entity'])) {
+                    $foreign = $linkDefs['foreign'];
+                    $duplicateForeign = lcfirst($entity->get('code')) . 's';
+
+                    $linkDefs['foreign'] = $duplicateForeign;
+
+                    $foreignDefs = $this->getMetadata()->get(['entityDefs', $linkDefs['entity']], []);
+
+                    if (!empty($foreignDefs['fields'][$foreign])) {
+                        $this->getMetadata()->set('entityDefs', $linkDefs['entity'], [
+                            'fields' => [
+                                $duplicateForeign => $foreignDefs['fields'][$foreign]
+                            ]
+                        ]);
+                    }
+
+                    if (!empty($foreignDefs['links'][$foreign])) {
+                        $foreignLinkDefs = $foreignDefs['links'][$foreign];
+
+                        if (!empty($foreignLinkDefs['entity'])) {
+                            $foreignLinkDefs['entity'] = $entity->get('code');
+                        }
+
+                        if (!empty($foreignLinkDefs['relationName'])) {
+                            if (str_starts_with($foreignLinkDefs['relationName'], lcfirst($duplicatingEntity->get('code')))) {
+                                $foreignLinkDefs['relationName'] = str_replace(lcfirst($duplicatingEntity->get('code')), lcfirst($entity->get('code')), $foreignLinkDefs['relationName']);
+                            } elseif (str_ends_with($foreignLinkDefs['relationName'], ucfirst($duplicatingEntity->get('code')))) {
+                                $foreignLinkDefs['relationName'] = str_replace(ucfirst($duplicatingEntity->get('code')), ucfirst($entity->get('code')), $foreignLinkDefs['relationName']);
+                            }
+
+                            $linkDefs['relationName'] = $foreignLinkDefs['relationName'];
+                        }
+
+                        $this->getMetadata()->set('entityDefs', $linkDefs['entity'], [
+                            'links' => [
+                                $duplicateForeign => $foreignLinkDefs
+                            ]
+                        ]);
+                    }
+
+                    $linksDefs[$field] = $linkDefs;
+                }
+            }
+
+            $needToUpdate = true;
+        }
+
+        if ($needToUpdate) {
+            $this->getMetadata()->set('entityDefs', $entity->get('code'), ['fields' => $fieldsDefs]);
+            if (!empty($linkDefs)) {
+                $this->getMetadata()->set('entityDefs', $entity->get('code'), ['links' => $linksDefs]);
+            }
+            $this->getMetadata()->save();
+
+            $this->getDataManager()->rebuild();
+        }
+    }
+
     protected function getFieldsThatConflict(OrmEntity $entity, \stdClass $data): array
     {
         return [];
@@ -78,5 +153,10 @@ class Entity extends ReferenceData
         parent::init();
 
         $this->addDependency('dataManager');
+    }
+
+    protected function getDataManager(): DataManager
+    {
+        return $this->getInjection('dataManager');
     }
 }
