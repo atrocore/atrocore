ab1a1dc8a171753aad53793981dc273a4703fa90
roman.ratsun <roman.ratsun@atrocore.com>
2026-01-13 11:20:02 +0200

    .

---
+++ app/Atro/Jobs/CreateClusters.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\Jobs;
+
+use Atro\Entities\Job;
+
+class CreateClusters extends AbstractJob implements JobInterface
+{
+    public function run(Job $job): void
+    {
+        $exists = $this->getEntityManager()->getRepository('Job')
+            ->where([
+                'id!='   => $job->id,
+                'type'   => 'CreateClusters',
+                'status' => 'Running'
+            ])
+            ->findOne();
+
+        if (!empty($exists)) {
+            return;
+        }
+
+        echo '<pre>';
+        print_r('123');
+        die();
+    }
+}
