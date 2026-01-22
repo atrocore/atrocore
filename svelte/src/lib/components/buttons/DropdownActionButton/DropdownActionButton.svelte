5d6c8095db864f22ef1cef49a8e789d088e803b6
Kevin Teguia <kevin.teguia@atrocore.com>
2026-01-22 08:44:45 +0100

    .

--- svelte/src/lib/components/buttons/DropdownActionButton/DropdownActionButton.svelte
+++ svelte/src/lib/components/buttons/DropdownActionButton/DropdownActionButton.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import {createEventDispatcher} from "svelte";
-    import ActionButton from "../ActionButton/ActionButton.svelte";
+    import ActionButton from "$lib/components/buttons/ActionButton/ActionButton.svelte";
     import { Language } from "$lib/core/language"
-    import type { ActionParams } from "../ActionButton/types/button-style";
+    import DropdownActionParams from "./types/params";
     import { getToggleClasses } from "./utils/dropdown-utils";
 
-    export let params: ActionParams;
+    export let params: DropdownActionParams;
     export let className: string = '';
 
     const dispatch = createEventDispatcher();
@@ -23,31 +23,27 @@
 </script>
 
 {#if !params.hidden}
-    {#if params.dropdownItems && params.dropdownItems.length > 0}
-        <div class="btn-group">
-            <ActionButton {params} {className} on:execute />
-
-            <button class={toggleClasses} data-toggle="dropdown">
-                <i class="ph ph-caret-down"></i>
-            </button>
-
-            <ul class="dropdown-menu pull-left filter-list">
-                {#each params.dropdownItems as subItem}
-                    <li data-name={subItem.name}>
-                        <a
-                                href="javascript:"
-                                data-name={subItem.name}
-                                data-id={subItem.id}
-                                data-action={subItem.action}
-                                on:click={handleClick}
-                        >
-                            {Language.translate(subItem.label ?? subItem.name ?? '')}
-                        </a>
-                    </li>
-                {/each}
-            </ul>
-        </div>
-    {:else}
+    <div class="btn-group">
         <ActionButton {params} {className} on:execute />
-    {/if}
+
+        <button class={toggleClasses} data-toggle="dropdown">
+            <i class="ph ph-caret-down"></i>
+        </button>
+
+        <ul class="dropdown-menu pull-left filter-list">
+            {#each params.dropdownItems as subItem}
+                <li data-name={subItem.name}>
+                    <a
+                            href="javascript:"
+                            data-name={subItem.name}
+                            data-id={subItem.id}
+                            data-action={subItem.action}
+                            on:click={handleClick}
+                    >
+                        {Language.translate(subItem.label ?? subItem.name ?? '')}
+                    </a>
+                </li>
+            {/each}
+        </ul>
+    </div>
 {/if}
