239f37b74e9ef255829ddff7e07e39a861d560a5
Kevin Teguia <kevin.teguia@atrocore.com>
2026-02-27 15:34:50 +0100

    .

--- app/Atro/NotificationTransport/SystemTransport.php
+++ app/Atro/NotificationTransport/SystemTransport.php
@@ -20,18 +20,27 @@ class SystemTransport extends AbstractNotificationTransport
 {
     public function send(User $user, Entity $template, array $params): void
     {
+
         $notification = $this->getEntityManager()->getEntity('Notification');
-        $notification->set('type', 'SystemNotification');
-        $notification->set('userId', $user->get('id'));
-        $notification->set('relatedId', $params['entity']->get('id'));
-        $notification->set('relatedType', $params['entity']->getEntityType());
 
-        $this->addEntitiesAdditionalData($params, $this->getLanguage()->getLanguage(), true);
+        if(!empty($params['message'])) {
+            $notification->set('type', 'Message');
+            $notification->set('message', $params['message']);
+            $notification->set('userId', $user->get('id'));
+        }else{
+            $notification->set('type', 'SystemNotification');
+            $notification->set('userId', $user->get('id'));
+            $notification->set('relatedId', $params['entity']->get('id'));
+            $notification->set('relatedType', $params['entity']->getEntityType());
 
-        $notification->set('data', [
-            "notificationTemplateId" => $template->get('id'),
-            "notificationParams" => $params,
-        ]);
+            $this->addEntitiesAdditionalData($params, $this->getLanguage()->getLanguage(), true);
+
+            $notification->set('data', [
+                "notificationTemplateId" => $template->get('id'),
+                "notificationParams" => $params,
+            ]);
+        }
+
 
         $this->getEntityManager()->saveEntity($notification);
     }
