3749d9ab2bed72dc411f2473769baa91b6ece283
roman.ratsun <roman.ratsun@atrocore.com>
2026-03-05 11:00:23 +0200

    .

--- client/src/views/fields/text.js
+++ client/src/views/fields/text.js
@@ -36,52 +36,19 @@ Espo.define('views/fields/text', 'views/fields/base', function (Dep) {
 
         type: 'text',
 
-        listTemplate: 'fields/text/list',
-
-        detailTemplate: 'fields/text/detail',
-
-        editTemplate: 'fields/text/edit',
-
-        searchTemplate: 'fields/text/search',
-
-        detailMaxLength: 400,
-
-        detailMaxNewLineCount: 10,
-
-        seeMoreText: false,
-
-        rowsMin: 2,
-
-        rowsMax: 10,
-
-        seeMoreDisabled: false,
-
-        useDisabledTextareaInViewMode: false,
+        _template: '',
 
         defaultFilterValue: '',
 
         searchTypeList: ['contains', 'startsWith', 'equals', 'endsWith', 'like', 'notContains', 'notLike', 'isEmpty', 'isNotEmpty'],
 
-        events: {
-            'click a[data-action="seeMoreText"]': function (e) {
-                this.seeMoreText = true;
-                this.reRender();
-            },
-            'keyup textarea.with-text-length': function (e) {
-                this.updateTextCounter();
-            },
-        },
+        svelteComponent: null,
 
         setup: function () {
             Dep.prototype.setup.call(this);
 
-            this.params.rowsMin = this.params.rowsMin || this.rowsMin;
-            this.params.rowsMax = this.params.rowsMax || this.rowsMax;
-            this.detailMaxLength = this.params.lengthOfCut || this.detailMaxLength;
-            this.seeMoreDisabled = this.seeMoreDisabled || this.params.seeMoreDisabled;
-            this.useDisabledTextareaInViewMode = this.options.useDisabledTextareaInViewMode || this.params.useDisabledTextareaInViewMode || this.useDisabledTextareaInViewMode;
-            this.autoHeightDisabled = this.options.autoHeightDisabled || this.params.autoHeightDisabled || this.autoHeightDisabled;
-
+            if (!this.params.rowsMin) this.params.rowsMin = 2;
+            if (!this.params.rowsMax) this.params.rowsMax = 10;
             if (this.params.rowsMax < this.params.rowsMin) {
                 this.params.rowsMin = this.params.rowsMax;
             }
@@ -89,232 +56,56 @@ Espo.define('views/fields/text', 'views/fields/base', function (Dep) {
             this.setScriptDefaultValue();
         },
 
-        setupSearch: function () {
-            this.events = _.extend({
-                'change select.search-type': function (e) {
-                    var type = $(e.currentTarget).val();
-                    this.handleSearchType(type);
-                },
-            }, this.events || {});
-        },
-
-        data: function () {
-            var data = Dep.prototype.data.call(this);
-            if (
-                this.model.get(this.name) !== null
-                &&
-                this.model.get(this.name) !== ''
-                &&
-                this.model.has(this.name)
-            ) {
-                data.isNotEmpty = true;
-            }
-            if (this.mode === 'search') {
-                if (typeof this.searchParams.value === 'string') {
-                    this.searchData.value = this.searchParams.value;
-                }
-            }
-            if (this.mode === 'edit') {
-                if (this.autoHeightDisabled) {
-                    data.rows = this.params.rowsMax;
-                } else {
-                    data.rows = this.params.rowsMin;
-                }
-            }
-
-            if (this.mode === 'detail' && this.useDisabledTextareaInViewMode) {
-                data.rows = this.params.rowsMin;
-            }
-
-            data.valueIsSet = this.model.has(this.name);
-            data.useDisabledTextareaInViewMode = this.useDisabledTextareaInViewMode;
-
-            return data;
-        },
-
-        handleSearchType: function (type) {
-            if (~['isEmpty', 'isNotEmpty'].indexOf(type)) {
-                this.$el.find('input.main-element').addClass('hidden');
-            } else {
-                this.$el.find('input.main-element').removeClass('hidden');
-            }
-        },
-
-        getValueForDisplay: function () {
-            var text = this.model.get(this.name);
-
-            if (text && ((this.mode == 'detail' && !this.useDisabledTextareaInViewMode) || this.mode == 'list') && !this.seeMoreText && !this.seeMoreDisabled) {
-                text = text.toString();
-
-                var maxLength = this.detailMaxLength;
-
-                var isCut = false;
-
-                if (text.length > this.detailMaxLength) {
-                    text = text.substr(0, this.detailMaxLength);
-                    isCut = true;
-                }
-
-                var nlCount = (text.match(/\n/g) || []).length;
-                if (nlCount > this.detailMaxNewLineCount) {
-                    var a = text.split('\n').slice(0, this.detailMaxNewLineCount);
-                    text = a.join('\n');
-                    isCut = true;
-                }
-
-                if (isCut) {
-                    text += ' ...\n[#see-more-text]';
-                }
-            }
-            return text || '';
-        },
-
-        controlTextareaHeight: function (lastHeight) {
-            var scrollHeight = this.$element.prop('scrollHeight');
-            var clientHeight = this.$element.prop('clientHeight');
-
-            if (typeof lastHeight === 'undefined' && clientHeight === 0) {
-                setTimeout(this.controlTextareaHeight.bind(this), 10);
-                return;
-            }
-
-            if (clientHeight === lastHeight) return;
-
-            if (scrollHeight > clientHeight + 1) {
-                var rows = this.$element.prop('rows');
-
-                if (this.params.rowsMax && rows >= this.params.rowsMax) return;
-
-                this.$element.attr('rows', rows + 1);
-                this.controlTextareaHeight(clientHeight);
-            }
-            if (this.$element.val().length === 0) {
-                this.$element.attr('rows', this.params.rowsMin);
-            }
-        },
-
-        updateTextCounter() {
-            let maxLength = this.params.maxLength;
-            let countBytesInsteadOfCharacters = this.params.countBytesInsteadOfCharacters;
-            if (!maxLength) {
-                return;
-            }
-
-            let $textarea = this.$el.find('textarea');
-
-            let text = this.$el.find('textarea').val();
-            let textLength = this.getRealLength(text, countBytesInsteadOfCharacters);
-
-            let $el = this.$el.find('.text-length-counter .current-length');
-
-            $el.html(textLength);
-
-            $textarea.css('border-color', '');
-            $el.css('color', '');
-            if (maxLength < textLength) {
-                $textarea.css('border-color', 'red');
-                $el.css('color', 'red');
-            }
-        },
-
-        getRealLength(text, countBytesInsteadOfCharacters) {
-            if (countBytesInsteadOfCharacters) {
-                return encodeURI(text).split(/%..|./).length - 1;
-            } else {
-                return (text ? text.toString().length : 0);
-            }
-        },
-
         afterRender: function () {
             Dep.prototype.afterRender.call(this);
-            if (this.mode == 'edit') {
-                var text = this.getValueForDisplay();
-                if (text) {
-                    this.$element.val(text);
-                }
-                this.updateTextCounter();
-            }
-            if (this.mode == 'search') {
-                var type = this.$el.find('select.search-type').val();
-                this.handleSearchType(type);
-            }
 
-            if (this.mode === 'edit' && !this.autoHeightDisabled) {
-                this.controlTextareaHeight();
-                this.$element.on('input', function () {
-                    this.controlTextareaHeight();
-                }.bind(this));
-            }
+            const target = this.$el[0];
+            if (!target) return;
 
-            if (['detail', 'list'].includes(this.mode) && !this.autoHeightDisabled) {
-                this.$element = this.$el.find('[name="' + this.name + '"]');
-                this.controlTextareaHeight();
+            this.removeSvelteComponent();
+
+            const searchType = this.getSearchType();
+
+            this.svelteComponent = new Svelte.TextField({
+                target,
+                props: {
+                    name: this.name,
+                    value: this.model.get(this.name) ?? '',
+                    mode: this.mode,
+                    scope: this.model.name,
+                    params: this.params || {},
+                    searchType: searchType || 'startsWith',
+                    searchValue: (this.mode === 'search' && typeof this.searchParams.value === 'string')
+                        ? this.searchParams.value
+                        : '',
+                },
+            });
+
+            this.svelteComponent.$on('change', (event) => {
+                const { name, value } = event.detail;
+                this.model.set(name, value);
+            });
+        },
+
+        removeSvelteComponent: function () {
+            if (this.svelteComponent) {
+                try {
+                    this.svelteComponent.$destroy();
+                } catch (e) {}
+                this.svelteComponent = null;
             }
         },
 
         fetch: function () {
-            var data = {};
-            data[this.name] = this.$element ? this.$element.val() : null;
-            if (data[this.name] === '') data[this.name] = null;
-            return data;
+            if (this.svelteComponent) {
+                return this.svelteComponent.fetch();
+            }
+            return { [this.name]: null };
         },
 
         fetchSearch: function () {
-
-            var type = this.$el.find('[name="' + this.name + '-type"]').val() || 'startsWith';
-
-            var data;
-
-            if (~['isEmpty', 'isNotEmpty'].indexOf(type)) {
-                if (type == 'isEmpty') {
-                    data = {
-                        type: 'or',
-                        value: [
-                            {
-                                type: 'isNull',
-                                field: this.name,
-                            },
-                            {
-                                type: 'equals',
-                                field: this.name,
-                                value: ''
-                            }
-                        ],
-                        data: {
-                            type: type
-                        }
-                    }
-                } else {
-                    data = {
-                        type: 'and',
-                        value: [
-                            {
-                                type: 'notEquals',
-                                field: this.name,
-                                value: ''
-                            },
-                            {
-                                type: 'isNotNull',
-                                field: this.name,
-                                value: null
-                            }
-                        ],
-                        data: {
-                            type: type
-                        }
-                    }
-                }
-                return data;
-            } else {
-                var value = this.$element.val().toString().trim();
-                value = value.trim();
-                if (value) {
-                    data = {
-                        value: value,
-                        type: type
-                    }
-                    return data;
-                }
+            if (this.svelteComponent) {
+                return this.svelteComponent.fetchSearch();
             }
             return false;
         },
@@ -326,7 +117,6 @@ Espo.define('views/fields/text', 'views/fields/base', function (Dep) {
         inlineEditFocusing() {
             requestAnimationFrame(() => {
                 const $textarea = this.$el.find('textarea');
-
                 $textarea.focus();
                 const val = $textarea.val();
                 $textarea[0].setSelectionRange(val.length, val.length);
@@ -351,7 +141,7 @@ Espo.define('views/fields/text', 'views/fields/base', function (Dep) {
                 valueGetter: this.filterValueGetter.bind(this),
                 validation: {
                     callback: function (value, rule) {
-                        if(value === null || typeof value !== 'string') {
+                        if (value === null || typeof value !== 'string') {
                             return 'bad string';
                         }
                         return true;
@@ -360,6 +150,9 @@ Espo.define('views/fields/text', 'views/fields/base', function (Dep) {
             };
         },
 
+        remove: function (dontEmpty) {
+            this.removeSvelteComponent();
+            Dep.prototype.remove.call(this, dontEmpty);
+        },
     });
 });
-
