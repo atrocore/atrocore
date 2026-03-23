df2ab241847a68da11049a7e8e076ee56a7699a7
Kevin Teguia <kevin.teguia@atrocore.com>
2026-03-23 12:14:38 +0100

    feat: add bulk-move note rendering to cluster-note.js

--- client/src/views/stream/notes/cluster-note.js
+++ client/src/views/stream/notes/cluster-note.js
@@ -17,12 +17,21 @@ Espo.define('views/stream/notes/cluster-note', 'views/stream/note', function (De
         init: function () {
             var type   = this.model.get('type') || '';
             var action = (this.model.get('data') || {}).action;
+            var data   = this.model.get('data') || {};
 
             var prefix = type.replace('Activity', '');
             prefix = prefix.charAt(0).toLowerCase() + prefix.slice(1);
 
             if (action) {
                 this.messageName = prefix + action.charAt(0).toUpperCase() + action.slice(1);
+
+                // Apply Staging/Master suffix before parent appends 'This'
+                if (data.stagingRecords !== undefined || data.masterRecords !== undefined) {
+                    var hasSt = data.stagingRecords && data.stagingRecords.length > 0;
+                    var hasMa = data.masterRecords  && data.masterRecords.length  > 0;
+                    if (hasSt && !hasMa) { this.messageName += 'Staging'; }
+                    if (!hasSt && hasMa) { this.messageName += 'Master';  }
+                }
             }
 
             Dep.prototype.init.call(this);
@@ -33,6 +42,37 @@ Espo.define('views/stream/notes/cluster-note', 'views/stream/note', function (De
             var relatedType = data.relatedType || this.model.get('relatedType');
             var relatedId   = data.relatedId   || this.model.get('relatedId');
             var relatedName = this.model.get('relatedName');
+
+            // Bulk-move note (movedToCluster / movedFromCluster)
+            if (data.stagingRecords !== undefined || data.masterRecords !== undefined) {
+                var hasSt = data.stagingRecords && data.stagingRecords.length > 0;
+                var hasMa = data.masterRecords  && data.masterRecords.length  > 0;
+                if (!hasSt && !hasMa) { return; }
+
+                this.messageData['stagingRecords'] = (data.stagingRecords || []).map(function (r) {
+                    var label      = Handlebars.Utils.escapeExpression(r.name || r.id);
+                    var entityName = Handlebars.Utils.escapeExpression(r.entityName || '');
+                    var id         = Handlebars.Utils.escapeExpression(r.id || '');
+                    return '<a href="#' + entityName + '/view/' + id + '">' + label + '</a>';
+                }).join(', ');
+
+                this.messageData['masterRecords'] = (data.masterRecords || []).map(function (r) {
+                    var label      = Handlebars.Utils.escapeExpression(r.name || r.id);
+                    var entityName = Handlebars.Utils.escapeExpression(r.entityName || '');
+                    var id         = Handlebars.Utils.escapeExpression(r.id || '');
+                    return '<a href="#' + entityName + '/view/' + id + '">' + label + '</a>';
+                }).join(', ');
+
+                var clusterLabel = Handlebars.Utils.escapeExpression(
+                    data.clusterNumber != null ? String(data.clusterNumber) : (data.clusterId || '')
+                );
+                this.messageData['clusterLink'] = data.clusterId
+                    ? '<a href="#Cluster/view/' + data.clusterId + '">' + clusterLabel + '</a>'
+                    : clusterLabel;
+
+                this.createMessage();
+                return;
+            }
 
             if (relatedType && relatedId) {
                 var label = Handlebars.Utils.escapeExpression(relatedName || relatedId);
