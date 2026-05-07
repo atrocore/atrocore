4eb099c606742aa65614376f0e004af16e201fd5
roman.ratsun <roman.ratsun@atrocore.com>
2026-05-07 11:14:38 +0300

    Update `listKanban` route paths in handler and client: Simplify endpoint URL structure.

--- app/Atro/Core/EntityTypeHandlers/ListKanbanHandler.php
+++ app/Atro/Core/EntityTypeHandlers/ListKanbanHandler.php
@@ -23,7 +23,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 #[Route(
-    path: '/{entityName}/action/listKanban',
+    path: '/{entityName}/listKanban',
     methods: [
         'GET',
     ],
