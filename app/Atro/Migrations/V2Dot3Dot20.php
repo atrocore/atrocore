adf255fb85d0d359c1dcbd5faf0cd287a6ae6427
Roman Ratsun <33658723+rratsun@users.noreply.github.com>
2026-09-08 09:24:23 +0300

    Update and rename V2Dot3Dot19.php to V2Dot3Dot20.php

--- app/Atro/Migrations/V2Dot3Dot20.php
+++ app/Atro/Migrations/V2Dot3Dot20.php
@@ -13,7 +13,7 @@ namespace Atro\Migrations;
 
 use Atro\Core\Migration\Base;
 
-class V2Dot3Dot19 extends Base
+class V2Dot3Dot20 extends Base
 {
     public function getMigrationDateTime(): ?\DateTime
     {
