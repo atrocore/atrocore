ebfaa08b5e7c4145c215e57bf3d894530c940edd
Roman Ratsun <roman.ratsun@atrocore.com>
2026-09-15 17:27:56 +0300

    Refactor unique index handling: remove outdated client views and metadata definitions, introduce service-based ACL logic for `EntityUniqueIndex`.

---
+++ app/Atro/Services/EntityUniqueIndex.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Atro\Services;
+
+use Atro\Core\Templates\Services\ReferenceData;
+use Espo\ORM\Entity;
+
+class EntityUniqueIndex extends ReferenceData
+{
+    public function putAclMeta(Entity $entity): void
+    {
+        parent::putAclMeta($entity);
+
+        // indexes coming from the module code are read-only
+        if (empty($entity->get('isCustom'))) {
+            $entity->setMetaPermission('edit', false);
+            $entity->setMetaPermission('delete', false);
+        }
+    }
+}
