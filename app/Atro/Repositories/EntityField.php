a5e41fab214b0237005f6f5f2f241950c11daae6
Roman Ratsun <roman.ratsun@atrocore.com>
2026-09-15 17:22:02 +0300

    Add support for managing unique indexes: introduce new entity, update field definitions, add validations, adjust repositories, translations, and UI components.

--- app/Atro/Repositories/EntityField.php
+++ app/Atro/Repositories/EntityField.php
@@ -672,7 +672,32 @@ class EntityField extends ReferenceData
             throw new Forbidden();
         }
 
+        $this->validateFieldIsNotUsedInUniqueIndex((string)$entity->get('entityId'), (string)$entity->get('code'));
+
+        // a link field is removed together with its foreign field
+        if (!empty($entity->get('foreignEntityId')) && !empty($entity->get('foreignCode'))) {
+            $this->validateFieldIsNotUsedInUniqueIndex((string)$entity->get('foreignEntityId'), (string)$entity->get('foreignCode'));
+        }
+
         parent::beforeRemove($entity, $options);
+    }
+
+    protected function validateFieldIsNotUsedInUniqueIndex(string $entityName, string $field): void
+    {
+        if (empty($entityName) || empty($field)) {
+            return;
+        }
+
+        $indexes = $this->getEntityManager()->getRepository('EntityUniqueIndex')->findIndexesByField($entityName, $field);
+        if (empty($indexes)) {
+            return;
+        }
+
+        throw new BadRequest(sprintf(
+            $this->getLanguage()->translate('fieldIsUsedInUniqueIndex', 'exceptions', 'EntityField'),
+            $this->translateLabel($field, 'fields', $entityName),
+            implode(', ', $indexes)
+        ));
     }
 
     public function insertEntity(OrmEntity $entity): bool
---
+++ client/src/views/entity-unique-index/fields/fields.js
@@ -0,0 +1,65 @@
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+Espo.define('views/entity-unique-index/fields/fields', 'views/fields/multi-enum', Dep => {
+
+    return Dep.extend({
+
+        setup: function () {
+            Dep.prototype.setup.call(this);
+
+            this.prepareOptions();
+
+            this.listenTo(this.model, 'change:entityId', () => {
+                this.prepareOptions();
+                this.reRender();
+            });
+        },
+
+        prepareOptions: function () {
+            const scope = this.model.get('entityId');
+
+            this.params.options = [];
+            this.translatedOptions = {};
+
+            if (!scope) {
+                return;
+            }
+
+            const fieldDefs = this.getMetadata().get(['entityDefs', scope, 'fields']) || {};
+
+            this.params.options = Object.keys(fieldDefs).filter(field => this.isFieldAvailable(scope, field, fieldDefs[field]));
+
+            this.params.options.forEach(field => {
+                this.translatedOptions[field] = this.translate(field, 'fields', scope);
+            });
+        },
+
+        /**
+         * Only simple field types having their own column in the table can be a part of the unique index.
+         */
+        isFieldAvailable: function (scope, field, fieldDefs) {
+            if (!fieldDefs || !fieldDefs.type) {
+                return false;
+            }
+
+            if (field === 'deleted') {
+                return false;
+            }
+
+            if (fieldDefs.notStorable || fieldDefs.disabled || fieldDefs.emHidden) {
+                return false;
+            }
+
+            return !!this.getMetadata().get(['fields', fieldDefs.type, 'uniqueIndexAllowed']);
+        }
+
+    });
+});
